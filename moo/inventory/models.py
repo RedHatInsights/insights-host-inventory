252d953a3851a3d9775e19ee69ec08185278f97f
Jesse Jaggars <jjaggars@redhat.com>
2018-04-08 15:33:21 -0500

    trying out hstore for tags

--- moo/inventory/models.py
+++ moo/inventory/models.py
@@ -1,29 +1,25 @@
 import uuid
 from django.db import models
-from django.contrib.postgres.fields import JSONField
-
-
-class Tag(models.Model):
-    name = models.CharField(max_length=200)
-    value = models.CharField(max_length=200, null=True)
-
-    def __str__(self):
-        return "{}={}".format(self.name, self.value)
+from django.contrib.postgres.fields import JSONField, HStoreField
 
 
 class Entity(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    account_number = models.CharField(max_length=10)
     facts = JSONField(null=True)
     display_name = models.CharField(max_length=200)
     created_on = models.DateTimeField(auto_now_add=True)
     modified_on = models.DateTimeField(auto_now=True)
-    tags = models.ManyToManyField(Tag)
+    tags = HStoreField(null=True)
     relationships = models.ManyToManyField(
         "self",
         through="EntityRelationship",
         through_fields=("from_entity", "to_entity"),
         symmetrical=False,
     )
+
+    class Meta:
+        indexes = [models.Index(fields=["account_number"])]
 
     def __str__(self):
         return self.display_name
