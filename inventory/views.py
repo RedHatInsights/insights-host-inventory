f6e43025ada6fe1bcd516ca05b8bffee2bcddbbf
Jesse Jaggars <jjaggars@redhat.com>
2018-04-12 14:01:04 -0500

    adding initial migration

--- inventory/views.py
+++ inventory/views.py
@@ -13,11 +13,12 @@ class EntityViewSet(viewsets.ModelViewSet):
     queryset = Entity.objects.all()
     serializer_class = EntitySerializer
     filter_backends = (filters.SearchFilter,)
-    search_fields = ('account_number', 'display_name')
+    search_fields = ("account_number", "display_name")
 
     def get_queryset(self):
         account_number = self.kwargs.get("account_number")
         if account_number:
             return self.queryset.filter(account_number=account_number)
+
         else:
             return self.queryset
