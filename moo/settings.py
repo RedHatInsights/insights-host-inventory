fefabcdf7c1c4b342dfffd66d6a5fa7c342a716b
Jesse Jaggars <jjaggars@redhat.com>
2018-04-12 12:13:21 -0400

    adding os routes to allowed_hosts

--- moo/settings.py
+++ moo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "bww4f_v05jj*$to3%k7i)ky0^=+radtq+n@e&g!997u+-iz%2x"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.insights.openshiftapps.com']
 
 
 # Application definition
