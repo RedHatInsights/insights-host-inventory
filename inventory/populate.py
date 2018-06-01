071ad7cbe41662d7c31ec41e6ff362ac31d8fcb8
Kyle Lape <klape@redhat.com>
2018-06-01 15:55:27 -0500

    Adding basic unit tests based on RequestsClient

---
+++ inventory/populate.py
@@ -0,0 +1,34 @@
+import random
+
+import os
+import django
+from django.db.transaction import atomic
+
+products = (["Red Hat Enterprise Linux"] * 75) + ["Openstack", "Red Hat Virtualization"]
+versions = ["7.0", "7.1", "7.2", "7.3", "7.4", "7.5"]
+accts = ["%07d" % n for n in range(1, 99)]
+
+
+@atomic
+def populate(count=100):
+    from inventory.models import Entity
+
+    def make_facts():
+        return [
+            dict(namespace="default", name="hostname", value=e.display_name.replace("ent", "host")),
+            dict(namespace="default", name="product", value=random.choice(products)),
+            dict(namespace="default", name="version", value=random.choice(versions))
+        ]
+
+    for x in range(count):
+        e = Entity.objects.create(
+            account_number=random.choice(accts), display_name="ent_%d" % x
+        )
+        e.facts = make_facts()
+        e.save()
+
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "moo.settings")
+    django.setup()
+    populate()
--- inventory/tests.py
+++ inventory/tests.py
@@ -1,3 +1,39 @@
-from django.test import TestCase
+from rest_framework.test import APIRequestFactory, RequestsClient
+from django.test import TransactionTestCase
 
-# Create your tests here.
+from inventory.views import EntityViewSet
+from inventory.populate import populate
+
+TEST_DATA = {"account_number": "1", "display_name": "hi"}
+
+
+class RequestsTest(TransactionTestCase):
+
+    BASE_URL = "http://localhost"
+
+    def setUp(self):
+        self.client = RequestsClient()
+        # self.factory = APIRequestFactory()
+        # self.view = EntityViewSet.as_view({"get": "list", "post": "create"})
+
+    def test_create(self):
+        response = self.client.post(self.BASE_URL + "/entities", json=TEST_DATA)
+        # request = self.factory.post("/entities", TEST_DATA, format="json")
+        # response = self.view(request)
+        assert response.status_code == 201, response.status_code
+        saved_id = response.json()["entity"]["id"]
+        response = self.client.get(self.BASE_URL + "/entities/%d" % saved_id)
+        data = response.json()["entity"]
+        assert response.status_code == 200, response.status_code
+        assert data["account_number"] == TEST_DATA["account_number"]
+        assert data["display_name"] == TEST_DATA["display_name"]
+
+    def test_fetch(self):
+        count = 100
+        populate(count)
+        response = self.client.get(self.BASE_URL + "/entities")
+        # request = self.factory.get("/entities")
+        # response = self.view(request)
+        data = response.json()
+        assert response.status_code == 200, response.status_code
+        assert len(data["entities"]) == count, len(data["entities"])
