990806edecae0c07cf9e2ee0b58d2ce9c1f2e9dc
Kyle Lape <klape@redhat.com>
2018-07-10 11:10:46 -0500

    Configure basic logging

--- insights/wsgi.py
+++ insights/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
+import logging
 
 from django.core.wsgi import get_wsgi_application
 
+logging.basicConfig()
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "insights.settings")
 
 application = get_wsgi_application()
