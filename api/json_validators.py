9b4cf65ead5528ddc705ff6a394855a7aa1db258
Derek Horton <dehort@redhat.com>
2019-01-15 22:15:17 -0600

    Quick and dirty (for now) addition of more custom format checkers Issue #92

--- api/json_validators.py
+++ api/json_validators.py
@@ -1,4 +1,5 @@
 import uuid
+import validators
 
 from jsonschema import draft4_format_checker
 
@@ -16,3 +17,20 @@ def verify_uuid_format(uuid_str):
     except:
         return False
     return True
+
+
+@draft4_format_checker.checks('ip_address')
+def verify_ip_address_format(ip_address):
+    if(validators.ip_address.ipv4(ip_address) or
+            validators.ip_address.ipv6(ip_address)):
+        return True
+    else:
+        return False
+
+
+@draft4_format_checker.checks('mac_address')
+def verify_mac_address_format(mac_address):
+    if validators.mac_address(mac_address):
+        return True
+    else:
+        return False
