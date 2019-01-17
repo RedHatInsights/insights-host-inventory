b70162936d2ac2126fcf710d4a361046b77d4595
Štěpán Tomsa <stomsa@redhat.com>
2019-01-17 16:03:00 +0100

    Remove unused imports
    Removed some unused imports, remnants of templates and refactorings.

--- manage.py
+++ manage.py
@@ -2,9 +2,6 @@ import os
 from flask_script import Manager  # class for handling a set of commands
 from flask_migrate import Migrate, MigrateCommand
 from app import db, create_app
-from app import models
-
-# import models
 
 
 app = create_app(config_name=os.getenv('APP_SETTINGS'))
