209da2c95061e8dd4080e318ccdfde5c1ceb735a
Derek Horton <dehort@redhat.com>
2019-03-27 11:14:34 -0500

    Fixed the validators to disallow None as a valid input

--- app/validators.py
+++ app/validators.py
@@ -6,10 +6,8 @@ from jsonschema import draft4_format_checker
 
 @draft4_format_checker.checks('uuid')
 def verify_uuid_format(uuid_str):
-    if uuid_str is None:
-        # None/null checking is handled by Connexion before this format check
-        # For now, allow UUID fields to be None at this level
-        return True
+    if not uuid_str:
+        return False
 
     try:
         return uuid.UUID(uuid_str) is not None
@@ -20,16 +18,15 @@ def verify_uuid_format(uuid_str):
 
 @draft4_format_checker.checks('ip_address')
 def verify_ip_address_format(ip_address):
-    if ip_address is None:
-        return True
+    if not ip_address:
+        return False
 
     return validators.ipv4(ip_address) or validators.ipv6(ip_address)
 
 
 @draft4_format_checker.checks('mac_address')
 def verify_mac_address_format(mac_address):
-    if mac_address is None:
-        return True
+    if not mac_address:
+        return False
 
     return validators.mac_address(mac_address)
-
