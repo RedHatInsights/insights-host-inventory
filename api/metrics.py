7461cc5d628a7b436994897d2e64e833e1effc06
Jesse Jaggars <jjaggars@redhat.com>
2019-04-23 11:13:37 -0500

    adding metrics for system profile processing
    Signed-off-by: Jesse Jaggars <jjaggars@redhat.com>

--- api/metrics.py
+++ api/metrics.py
@@ -7,7 +7,7 @@ host_dedup_processing_time = Summary("inventory_dedup_processing_seconds",
 new_host_commit_processing_time = Summary("inventory_new_host_commit_seconds",
                                           "Time spent committing a new host to the database")
 update_host_commit_processing_time = Summary("inventory_update_host_commit_seconds",
-                                          "Time spent committing a update host to the database")
+                                             "Time spent committing a update host to the database")
 api_request_count = Counter("inventory_request_count",
                             "The total amount of API requests")
 create_host_count = Counter("inventory_create_host_count",
@@ -16,3 +16,11 @@ update_host_count = Counter("inventory_update_host_count",
                             "The total amount of hosts updated")
 login_failure_count = Counter("inventory_login_failure_count",
                               "The total amount of failed login attempts")
+system_profile_deserialization_time = Summary("system_profile_deserialization_time",
+                                              "Time spent deserializing system profile documents")
+system_profile_commit_processing_time = Summary("system_profile_commit_processing_time",
+                                                "Time spent committing an update to a system profile to the database")
+system_profile_commit_count = Counter("system_profile_commit_count",
+                                      "Count of successful system profile commits to the database")
+system_profile_failure_count = Counter("system_profile_failure_count",
+                                       "Count of failures to commit the system profile to the database")
