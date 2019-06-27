85dec55193a820bfba2fba2cd40ef729c9a9b4f3
Derek Horton <dehort@redhat.com>
2019-06-26 23:33:45 -0500

    Do not start the tasks subsystem unless explicitly asked to

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ import logging
 from app import create_app
 
 config_name = os.getenv('APP_SETTINGS', "development")
-application = create_app(config_name)
+application = create_app(config_name, start_tasks=True)
 listen_port = os.getenv('LISTEN_PORT', 8080)
 
 if __name__ == '__main__':
