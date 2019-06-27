b0e2aa6478ad8157992d8a1fca5b70c36699fd82
Jason Stephens <48523896+Jason-RH@users.noreply.github.com>
2019-06-27 14:49:09 -0400

    Updating kafka topic name (#321)

--- app/config.py
+++ app/config.py
@@ -29,7 +29,7 @@ class Config:
 
         self.api_urls = [self.api_url_path_prefix, self.legacy_api_url_path_prefix]
 
-        self.host_ingress_topic = os.environ.get("KAFKA_HOST_INGRESS_TOPIC", "platform.host-ingress")
+        self.host_ingress_topic = os.environ.get("KAFKA_HOST_INGRESS_TOPIC", "platform.inventory.host-ingress")
         self.host_ingress_consumer_group = os.environ.get("KAFKA_HOST_INGRESS_GROUP", "inventory-mq")
         self.system_profile_topic = os.environ.get("KAFKA_TOPIC", "platform.system-profile")
         self.consumer_group = os.environ.get("KAFKA_GROUP", "inventory")
