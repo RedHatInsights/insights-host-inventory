8b6ded9d549e3477b693f2fa0e62ea5d34f08be6
Jason Stephens <jassteph@redhat.com>
2019-07-08 11:07:25 -0400

    Renamed mq_parser to app/queue/ingress.py. Removed error handling from handle_message()

--- app/queue/ingress.py
+++ app/queue/ingress.py
@@ -1,9 +1,8 @@
 from marshmallow import Schema, fields, ValidationError
 from app.logging import get_logger
-from app.exceptions import ValidationException
 
 
-logger = get_logger("mq_parser")
+logger = get_logger(__name__)
 
 
 class OperationSchema(Schema):
@@ -16,8 +15,8 @@ def parse_operation_message(message):
     try:
         parsed_operation = OperationSchema(strict=True).load(message).data
     except ValidationError as e:
-        # logger.error("Input validation error while parsing operation message")  # logger.error is used to avoid printing out ValidationError traceback
-        raise ValidationException(e) from None
+        logger.error("Input validation error while parsing operation message", extra={"operation": message})  # logger.error is used to avoid printing out the same traceback twice
+        raise
     except Exception as e:
         logger.exception("Error parsing operation message", extra={"operation": message})
         raise
