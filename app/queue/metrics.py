552e1d076aba9d4ba046f22ebbcfbf47acdb8400
Jason Stephens <jassteph@redhat.com>
2019-07-19 09:38:04 -0400

    Adding new metrics

--- app/queue/metrics.py
+++ app/queue/metrics.py
@@ -4,6 +4,9 @@ ingress_message_parsing_time = Summary("inventory_ingress_message_parsing_second
                                                 "Time spent parsing a message from the ingress queue")
 ingress_message_parsing_failure = Counter("inventory_ingress_message_parsing_failures",
                                                 "Total amount of failures parsing ingress messages")
-add_host_failure = Counter("inventory_add_host_failures", "Total amount of failures adding hosts")
+add_host_success = Counter("inventory_ingress_add_host_successes", "Total amount of successfully added hosts")
+add_host_failure = Counter("inventory_ingress_add_host_failures", "Total amount of failures adding hosts")
 ingress_message_handler_success = Counter("inventory_ingress_message_handler_successes",
                                                 "Total amount of successfully handled messages from the ingress queue")
+ingress_message_handler_failure = Counter("inventory_ingress_message_handler_failures",
+                                                "Total amount of failures handling messages from the ingress queue")
