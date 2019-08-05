5038067a754f7de45746baaeec273b0188b6ae7a
Alec Cohan <alcohan@redhat.com>
2019-08-05 14:16:28 -0400

    More small changes to try and setup the local dev enviroment

--- app/events.py
+++ app/events.py
@@ -12,6 +12,7 @@ class HostEvent(Schema):
     id = fields.UUID()
     timestamp = fields.DateTime(format="iso8601")
     type = fields.Str()
+    '''
     insights_id = fields.Str(validate=verify_uuid_format)
     rhel_machine_id = fields.Str(validate=verify_uuid_format)
     subscription_manager_id = fields.Str(validate=verify_uuid_format)
@@ -21,9 +22,10 @@ class HostEvent(Schema):
     ip_addresses = fields.List(fields.Str(validate=validate.Length(min=1, max=255)))
     mac_addresses = fields.List(fields.Str(validate=validate.Length(min=1, max=255)))
     external_id = fields.Str(validate=validate.Length(min=1, max=500))
+    '''
 
 
 def delete(id):
-    return HostEvent(strict=True).dumps({"id": id, "timestamp": datetime.utcnow(), "type": "delete", "insights_id": id.canonical_facts.insights_id, "rhel_machine_id": id.canonical_facts.rhel_machine_id,
+    return HostEvent(strict=True).dumps({"id": id, "timestamp": datetime.utcnow(), "type": "delete"}).data, ''' "insights_id": id.canonical_facts.insights_id, "rhel_machine_id": id.canonical_facts.rhel_machine_id,
                                         "subscription_manager_id": id.canonical_facts.subscription_manager_id, "satellite_id": id.canonical_facts.satellite_id, 
-                                        "fqdn": id.canonical_facts.fqdn, "bios_uuid": id.canonical_facts.bios_uuid, "external_id": id.canonical_facts.external_id}).data
+                                        "fqdn": id.canonical_facts.fqdn, "bios_uuid": id.canonical_facts.bios_uuid, "external_id": id.canonical_facts.external_id}).data '''
