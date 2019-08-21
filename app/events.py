355ba890da6cea01a06e14dd220c3c17e5f1a958
Alec Cohan <alcohan@redhat.com>
2019-08-21 09:22:26 -0400

    Added validate back in to match create/update schema

--- app/events.py
+++ app/events.py
@@ -3,8 +3,10 @@ from datetime import datetime
 
 from marshmallow import fields
 from marshmallow import Schema
+from marshmallow import validate
 
 from app.models import CanonicalFacts
+from app.validators import verify_uuid_format
 
 logger = logging.getLogger(__name__)
 
@@ -13,16 +15,16 @@ class HostEvent(Schema):
     id = fields.UUID()
     timestamp = fields.DateTime(format="iso8601")
     type = fields.Str()
-    account = fields.Str()
-    insights_id = fields.Str()
-    rhel_machine_id = fields.Str()
-    subscription_manager_id = fields.Str()
-    satellite_id = fields.Str()
-    fqdn = fields.Str()
-    bios_uuid = fields.Str()
-    ip_addresses = fields.List(fields.Str())
-    mac_addresses = fields.List(fields.Str())
-    external_id = fields.Str()
+    account = fields.Str(required=True, validate=validate.Length(min=1, max=10))
+    insights_id = fields.Str(validate=verify_uuid_format)
+    rhel_machine_id = fields.Str(validate=verify_uuid_format)
+    subscription_manager_id = fields.Str(validate=verify_uuid_format)
+    satellite_id = fields.Str(validate=verify_uuid_format)
+    fqdn = fields.Str(validate=validate.Length(min=1, max=255))
+    bios_uuid = fields.Str(validate=verify_uuid_format)
+    ip_addresses = fields.List(fields.Str(validate=validate.Length(min=1, max=255)))
+    mac_addresses = fields.List(fields.Str(validate=validate.Length(min=1, max=255)))
+    external_id = fields.Str(validate=validate.Length(min=1, max=500))
 
 
 def delete(host):
