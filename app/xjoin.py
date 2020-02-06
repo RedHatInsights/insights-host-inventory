954e146eab43e82e363d82ecd92a2cccfc711d90
Ryan Felton <rfelton@redhat.com>
2020-02-06 09:56:23 -0500

    Switched abort to always report 500 status

--- app/xjoin.py
+++ app/xjoin.py
@@ -29,7 +29,7 @@ def graphql_query(query_string, variables):
     response = post(url_, json=payload, headers=_forwarded_headers())
     status = response.status_code
     if status != 200:
-        logger.debug("xjoin-search returned status: %s", status)
+        logger.debug("xjoin-search returned status: %s", 500)
         abort(status, "xjoin-search error")
 
     logger.debug("QUERY: response %s", response.text)
