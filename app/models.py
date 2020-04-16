b481edee0df7f9fa3ea01feb845239aafa86cc8a
Ryan Felton <rfelton@redhat.com>
2020-04-16 17:17:49 -0400

    fix?

--- app/models.py
+++ app/models.py
@@ -108,7 +108,9 @@ class Host(db.Model):
     def update(self, input_host, update_system_profile=False):
         self.update_canonical_facts(input_host.canonical_facts)
 
-        self.update_display_name(input_host.display_name)
+        # TODO: Address this. Hotfix satellite 6.7 issue wher display name is set to fqdn when it
+        if input_host.reporter == "puptoo":
+            self.update_display_name(input_host.display_name)
 
         self._update_ansible_host(input_host.ansible_host)
 
