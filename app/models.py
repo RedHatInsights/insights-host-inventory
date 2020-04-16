6cb7cab22b50c811172ce7a9569be3979aedf5a6
Shaun Dunning <sdunning@redhat.com>
2020-04-16 18:22:36 -0400

    small bug fix

--- app/models.py
+++ app/models.py
@@ -110,7 +110,7 @@ class Host(db.Model):
         self.update_canonical_facts(input_host.canonical_facts)
 
         # TODO: Address this. Hotfix satellite 6.7 issue wher display name is set to fqdn when it
-        if hasattr(input_host, "reporter") or input_host.reporter == "puptoo":
+        if not hasattr(input_host, "reporter") or input_host.reporter == "puptoo":
             self.update_display_name(input_host.display_name)
 
         self._update_ansible_host(input_host.ansible_host)
