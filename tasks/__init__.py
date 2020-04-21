0fd0285f2503452538810c20d7a08408b6c57fa5
Štěpán Tomsa <stomsa@redhat.com>
2020-04-21 11:59:45 +0200

    Remove System Profile consumer
    System profile consumer is no longer in use for some time. Removed the
    task. Removed along with it everything that was used solely by the
    consumer: message handler, metrics, configuration.

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,16 +1,6 @@
-import json
-from threading import Thread
-
-from kafka import KafkaConsumer
 from kafka import KafkaProducer
 
-from api import metrics
-from app import db
 from app.logging import get_logger
-from app.logging import threadctx
-from app.models import Host
-from app.models import SystemProfileSchema
-from lib.db import session_guard
 
 logger = get_logger(__name__)
 
@@ -19,7 +9,7 @@ producer = None
 cfg = None
 
 
-def init_tasks(config, flask_app=None):
+def init_tasks(config):
     global cfg
     global producer
 
@@ -27,8 +17,6 @@ def init_tasks(config, flask_app=None):
 
     if config.kafka_enabled:
         producer = _init_event_producer(config)
-    if flask_app:
-        _init_system_profile_consumer(config, flask_app)
 
 
 def _init_event_producer(config):
@@ -49,51 +37,3 @@ def flush():
     logger.info("Event messages flush%s", suffix)
     if producer:
         producer.flush()
-
-
-@metrics.system_profile_commit_processing_time.time()
-def msg_handler(parsed):
-    id_ = parsed["id"]
-    threadctx.request_id = parsed["request_id"]
-    if not id_:
-        logger.error("ID is null, something went wrong.")
-        return
-
-    with session_guard(db.session):
-        host = Host.query.get(id_)
-        if host is None:
-            logger.error("Host with id [%s] not found!", id_)
-            return
-        logger.info("Processing message id=%s request_id=%s", parsed["id"], parsed["request_id"])
-        profile = SystemProfileSchema(strict=True).load(parsed["system_profile"]).data
-        host._update_system_profile(profile)
-
-
-def _init_system_profile_consumer(config, flask_app, handler=msg_handler, consumer=None):
-
-    if not config.kafka_enabled:
-        logger.info("System profile consumer has been disabled")
-        return
-
-    logger.info("Starting system profile queue consumer.")
-
-    if consumer is None:
-        consumer = KafkaConsumer(
-            config.system_profile_topic, group_id=config.consumer_group, bootstrap_servers=config.bootstrap_servers
-        )
-
-    def _f():
-        with flask_app.app_context():
-            while True:
-                for msg in consumer:
-                    try:
-                        with metrics.system_profile_deserialization_time.time():
-                            data = json.loads(msg.value)
-                        handler(data)
-                        metrics.system_profile_commit_count.inc()
-                    except Exception:
-                        logger.exception("uncaught exception in handler, moving on.")
-                        metrics.system_profile_failure_count.inc()
-
-    t = Thread(target=_f, daemon=True)
-    t.start()
