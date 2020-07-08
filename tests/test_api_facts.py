07d47485c7ad0e4b0db117a1c554fbe578eb3d96
Rodrigo <contato@rodrigoantunes.com.br>
2020-07-08 13:32:15 -0300

    RHCLOUD-6424 Tests refactor
    * Move test files to a test directory and split big files into small ones
    
    * Remove IDE files
    
    * Added a new fixture to create/drop a test database
    
    * Small fix
    
    * Reformat the code
    
    * Added markers for the host reaper tests
    
    * test_api_auth unittest to pytest conversion
    
    * Fix flake8 validations
    
    * Implemented review suggestions
    
    * Always create a new test database to avoid unexpected data
    
    * Lint
    
    * Linting
    
    * Remove test_api file
    
    * Linting
    
    * Consolidate CamelCase in class names
    
    * Consolidate relative imports
    
    Somewhere there were relative imports, somewhere there were absolute
    ones. Consolidated to absolute.
    
    * Consolidate tags tests name
    
    Consolidated tags tests to “tags” instead of “tag” or “tagging”.
    
    * Consolidate executability
    
    Added shebangs and executable permisions to unittest test suites.
    Those have if name main clauses and can be run directly as Python
    module.
    
    * Rename some test cases
    
    * System culling is called only culling in the codebase.
    * Simplified host dedup logic to just deduplication.
    
    * Decouple culling tests
    
    Extracted some culling related functionality to a new module. Like
    this, tests dont import from other test suites.
    
    * Rename tests cases
    
    Renamed test cases named after HTTP verbs.
    
    - The PUT tests was testing more various facts functionalities than
      just the PUT method.
    - Other test cases in fact tests various host related functionality.
      Renamed to reflect that.
    
    * Rename test_db_model
    
    The test_db_model test suite deals not only with database model, but
    with other things from app.models. Renamed.
    
    * Rename inconsistent hosts get tests
    
    There was one test case in the hosts GET test suite that had much
    different name than the others. Renamed for consistency.
    
    * Extract common delete test case to a new module
    
    To avoid cross importing from different test suites, created a new
    test_delete_utils module to hold the common DeleteHostsBaseTestCase.
    
    * Remove shebang from pytest file
    
    test_api_auth has been converted from unittest to pytest, so it is no
    longer directly executable. Thus the shebang line ought not to be
    there.
    
    * Remove manual context push/pop
    
    Flask application app_context context manager does the context
    pushing/popping automatically. There is no need to do that manually.
    
    Co-authored-by: Rodrigo Antunes <rantunes@redhat.com>
    Co-authored-by: Štěpán Tomsa <stomsa@redhat.com>
    Co-authored-by: Jozef Hartinger <jozefhartinger@gmail.com>

---
+++ tests/test_api_facts.py
@@ -0,0 +1,201 @@
+#!/usr/bin/env python
+from itertools import chain
+from unittest import main
+
+from app.utils import HostWrapper
+from tests.test_api_utils import generate_uuid
+from tests.test_api_utils import HOST_URL
+from tests.test_api_utils import PreCreatedHostsBaseTestCase
+from tests.test_culling_utils import HostStalenessBaseTestCase
+
+
+class FactsTestCase(PreCreatedHostsBaseTestCase):
+    def _valid_fact_doc(self):
+        return {"newfact1": "newvalue1", "newfact2": "newvalue2"}
+
+    def _build_facts_url(self, host_list, namespace):
+        if type(host_list) == list:
+            url_host_id_list = self._build_host_id_list_for_url(host_list)
+        else:
+            url_host_id_list = str(host_list)
+        return HOST_URL + "/" + url_host_id_list + "/facts/" + namespace
+
+    def _basic_fact_test(self, input_facts, expected_facts, replace_facts):
+
+        host_list = self.added_hosts
+
+        # This test assumes the set of facts are the same across
+        # the hosts in the host_list
+
+        target_namespace = host_list[0].facts[0]["namespace"]
+
+        url_host_id_list = self._build_host_id_list_for_url(host_list)
+
+        patch_url = self._build_facts_url(host_list, target_namespace)
+
+        if replace_facts:
+            response = self.put(patch_url, input_facts, 200)
+        else:
+            response = self.patch(patch_url, input_facts, 200)
+
+        response = self.get(f"{HOST_URL}/{url_host_id_list}", 200)
+
+        self.assertEqual(len(response["results"]), len(host_list))
+
+        for response_host in response["results"]:
+            host_to_verify = HostWrapper(response_host)
+
+            self.assertEqual(host_to_verify.facts[0]["facts"], expected_facts)
+
+            self.assertEqual(host_to_verify.facts[0]["namespace"], target_namespace)
+
+    def test_add_facts_without_fact_dict(self):
+        patch_url = self._build_facts_url(1, "ns1")
+        response = self.patch(patch_url, None, 400)
+        self.assertEqual(response["detail"], "Request body is not valid JSON")
+
+    def test_add_facts_to_multiple_hosts(self):
+        facts_to_add = self._valid_fact_doc()
+
+        host_list = self.added_hosts
+
+        # This test assumes the set of facts are the same across
+        # the hosts in the host_list
+
+        expected_facts = {**host_list[0].facts[0]["facts"], **facts_to_add}
+
+        self._basic_fact_test(facts_to_add, expected_facts, False)
+
+    def test_replace_and_add_facts_to_multiple_hosts_with_branch_id(self):
+        facts_to_add = self._valid_fact_doc()
+
+        host_list = self.added_hosts
+
+        target_namespace = host_list[0].facts[0]["namespace"]
+
+        url_host_id_list = self._build_host_id_list_for_url(host_list)
+
+        patch_url = HOST_URL + "/" + url_host_id_list + "/facts/" + target_namespace + "?" + "branch_id=1234"
+
+        # Add facts
+        self.patch(patch_url, facts_to_add, 200)
+
+        # Replace facts
+        self.put(patch_url, facts_to_add, 200)
+
+    def test_replace_and_add_facts_to_multiple_hosts_including_nonexistent_host(self):
+        facts_to_add = self._valid_fact_doc()
+
+        host_list = self.added_hosts
+
+        target_namespace = host_list[0].facts[0]["namespace"]
+
+        url_host_id_list = self._build_host_id_list_for_url(host_list)
+
+        # Add a couple of host ids that should not exist in the database
+        url_host_id_list = url_host_id_list + "," + generate_uuid() + "," + generate_uuid()
+
+        patch_url = HOST_URL + "/" + url_host_id_list + "/facts/" + target_namespace
+
+        # Add facts
+        self.patch(patch_url, facts_to_add, 400)
+
+        # Replace facts
+        self.put(patch_url, facts_to_add, 400)
+
+    def test_add_facts_to_multiple_hosts_overwrite_empty_key_value_pair(self):
+        new_facts = {}
+        expected_facts = new_facts
+
+        # Set the value in the namespace to an empty fact set
+        self._basic_fact_test(new_facts, expected_facts, True)
+
+        new_facts = self._valid_fact_doc()
+        expected_facts = new_facts
+
+        # Overwrite the empty fact set
+        self._basic_fact_test(new_facts, expected_facts, False)
+
+    def test_add_facts_to_multiple_hosts_add_empty_fact_set(self):
+        new_facts = {}
+        target_namespace = self.added_hosts[0].facts[0]["namespace"]
+        valid_host_id = self.added_hosts[0].id
+
+        test_url = self._build_facts_url(valid_host_id, target_namespace)
+
+        # Test merging empty facts set
+        self.patch(test_url, new_facts, 400)
+
+    def test_replace_and_add_facts_to_namespace_that_does_not_exist(self):
+        valid_host_id = self.added_hosts[0].id
+        facts_to_add = self._valid_fact_doc()
+        test_url = self._build_facts_url(valid_host_id, "imanonexistentnamespace")
+
+        # Test replace
+        self.put(test_url, facts_to_add, 400)
+
+        # Test add/merge
+        self.patch(test_url, facts_to_add, 400)
+
+    def test_replace_facts_without_fact_dict(self):
+        put_url = self._build_facts_url(1, "ns1")
+        response = self.put(put_url, None, 400)
+        self.assertEqual(response["detail"], "Request body is not valid JSON")
+
+    def test_replace_facts_on_multiple_hosts(self):
+        new_facts = self._valid_fact_doc()
+        expected_facts = new_facts
+
+        self._basic_fact_test(new_facts, expected_facts, True)
+
+    def test_replace_facts_on_multiple_hosts_with_empty_fact_set(self):
+        new_facts = {}
+        expected_facts = new_facts
+
+        self._basic_fact_test(new_facts, expected_facts, True)
+
+    def test_replace_empty_facts_on_multiple_hosts(self):
+        new_facts = {}
+        expected_facts = new_facts
+
+        self._basic_fact_test(new_facts, expected_facts, True)
+
+        new_facts = self._valid_fact_doc()
+        expected_facts = new_facts
+
+        self._basic_fact_test(new_facts, expected_facts, True)
+
+    def test_invalid_host_id(self):
+        bad_id_list = ["notauuid", "1234blahblahinvalid"]
+        only_bad_id = bad_id_list.copy()
+
+        # Can’t have empty string as an only ID, that results in 404 Not Found.
+        more_bad_id_list = bad_id_list + [""]
+        valid_id = self.added_hosts[0].id
+        with_bad_id = [f"{valid_id},{bad_id}" for bad_id in more_bad_id_list]
+
+        operations = (self.patch, self.put)
+        fact_doc = self._valid_fact_doc()
+        for operation in operations:
+            for host_id_list in chain(only_bad_id, with_bad_id):
+                url = self._build_facts_url(host_id_list, "ns1")
+                with self.subTest(operation=operation, host_id_list=host_id_list):
+                    operation(url, fact_doc, 400)
+
+
+class FactsCullingTestCase(FactsTestCase, HostStalenessBaseTestCase):
+    def test_replace_and_merge_ignore_culled_hosts(self):
+        # Try to replace the facts on a host that has been marked as culled
+        target_namespace = self.culled_host["facts"][0]["namespace"]
+
+        facts_to_add = self._valid_fact_doc()
+        test_url = self._build_facts_url(self.culled_host["id"], target_namespace)
+        # Test replace
+        self.put(test_url, facts_to_add, 400)
+
+        # Test add/merge
+        self.patch(test_url, facts_to_add, 400)
+
+
+if __name__ == "__main__":
+    main()
