b12ab6f03bc46e04bf74969370ec62ef9f470bac
tahmidefaz <tefaz@redhat.com>
2020-07-23 10:16:43 -0400

    fix for prometheus client

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -5,7 +5,7 @@ from prometheus_client import multiprocess
 
 
 def child_exit(server, worker):
-    # multiproc_path = os.environ.get("prometheus_mutliproc_dir", tempfile.gettempdir() + "/prometheus-multiproc-dir/")
+    multiproc_path = os.environ.get("prometheus_mutliproc_dir", tempfile.gettempdir() + "/prometheus-multiproc-dir/")
     multiprocess.mark_process_dead(worker.pid)
 
 # def on_exit(server):
