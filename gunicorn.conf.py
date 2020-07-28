e9f3fe37bc66258ff53301bf809edb39a471b020
tahmidefaz <tefaz@redhat.com>
2020-07-28 13:57:49 -0400

    removing local prometheus fix

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,9 +1,5 @@
-import os
-import tempfile
-
 from prometheus_client import multiprocess
 
 
 def child_exit(server, worker):
-    multiproc_path = os.environ.get("prometheus_mutliproc_dir", tempfile.gettempdir() + "/prometheus-multiproc-dir/")
-    multiprocess.mark_process_dead(worker.pid, multiproc_path)
+    multiprocess.mark_process_dead(worker.pid)
