eae88c8fe276c9109231c7523b05f99b097c8e2b
Štěpán Tomsa <stomsa@redhat.com>
2020-09-04 12:18:59 +0200

    Revert unwanted payloads change

--- utils/payloads.py
+++ utils/payloads.py
@@ -373,7 +373,7 @@ def rpm_list():
 
 def create_system_profile():
     return {
-        "number_of_cpus": "1",
+        "number_of_cpus": 1,
         "number_of_sockets": 2,
         "cores_per_socket": 4,
         "system_memory_bytes": 1024,
