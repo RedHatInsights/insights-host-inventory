e34b423782ca258b1e7797238dee5c9119f27d9d
Ryan Felton <rfelton@redhat.com>
2020-09-25 10:12:39 -0400

    more style

--- tests/test_api_hosts_get.py
+++ tests/test_api_hosts_get.py
@@ -935,8 +935,8 @@ def test_get_hosts_sap_system(patch_xjoin_post, api_get, subtests):
 def test_get_hosts_sap_system_bad_parameter_values(patch_xjoin_post, api_get, subtests):
     patch_xjoin_post(response={})
 
-    implicit_url = build_hosts_url(query=f"?filter[system_profile][sap_system]=beans")
-    eq_url = build_hosts_url(query=f"?filter[system_profile][sap_system][eq]=Garfield")
+    implicit_url = build_hosts_url(query="?filter[system_profile][sap_system]=beans")
+    eq_url = build_hosts_url(query="?filter[system_profile][sap_system][eq]=Garfield")
 
     implicit_response_status, implicit_response_data = api_get(implicit_url, extra_headers=DATA_SOURCE_XJOIN_HEADER)
     eq_response_status, eq_response_data = api_get(eq_url, extra_headers=DATA_SOURCE_XJOIN_HEADER)
@@ -948,8 +948,8 @@ def test_get_hosts_sap_system_bad_parameter_values(patch_xjoin_post, api_get, su
 def test_get_hosts_unsupported_filter(patch_xjoin_post, api_get):
     patch_xjoin_post(response={})
 
-    implicit_url = build_hosts_url(query=f"?filter[system_profile][bad_thing]=Banana")
-    eq_url = build_hosts_url(query=f"?filter[Bad_thing][Extra_bad_one][eq]=Pinapple")
+    implicit_url = build_hosts_url(query="?filter[system_profile][bad_thing]=Banana")
+    eq_url = build_hosts_url(query="?filter[Bad_thing][Extra_bad_one][eq]=Pinapple")
 
     implicit_response_status, implicit_response_data = api_get(implicit_url, extra_headers=DATA_SOURCE_XJOIN_HEADER)
     eq_response_status, eq_response_data = api_get(eq_url, extra_headers=DATA_SOURCE_XJOIN_HEADER)
