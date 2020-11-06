ac5f4df0853385a474b4d8cdbabedb3d6d9273a7
Kyle Lape <klape@redhat.com>
2020-11-06 13:59:53 -0600

    Add build scripts

---
+++ pr_check.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# --------------------------------------------
+# Options that must be configured by app owner
+# --------------------------------------------
+APP_NAME="host-inventory"  # name of app-sre "application" folder this component lives in
+COMPONENT_NAME="host-inventory"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
+IMAGE="quay.io/cloudservices/insights-inventory"
+
+IQE_PLUGINS="host-inventory"
+IQE_MARKER_EXPRESSION="smoke"
+IQE_FILTER_EXPRESSION=""
+
+# ---------------------------
+# We'll take it from here ...
+# ---------------------------
+
+source build_deploy.sh
+
+CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
+curl -s $CICD_URL/bootstrap.sh -o bootstrap.sh
+source bootstrap.sh  # checks out bonfire and changes to "cicd" dir...
+
+source deploy_ephemeral_env.sh
+source smoke_test.sh
