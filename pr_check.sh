eff1dc8ad5baf692dec09fbb5e6ab1e1153f542f
thearifismail <arifismail@yahoo.com>
2020-11-16 16:06:32 -0500

    Dumb hack to get past pr_check problem.  Must be used IF unit tests are running

--- pr_check.sh
+++ pr_check.sh
@@ -1,4 +1,4 @@
-echo "This is temporary hack to get past pr_check, which fails due to the \
+echo "This is a temporary hack to get past pr_check, which fails due to the \
  addition of clowder_config. clowder_config used \"app_common_python\" \
  module, which blocks the addition of importib-resources library. \
  importlib-resources is needed by host-inventory.  To add, importlib-resources\
