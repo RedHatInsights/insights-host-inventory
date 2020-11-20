26d5080fda6ec649bb88ac60bb24c47640283f8d
thearifismail <arifismail@yahoo.com>
2020-11-20 11:32:47 -0500

    deleted test not needed any more replaced by a new test test_synchronize_multiple_host_events

--- tests/test_synchronizer.py
+++ tests/test_synchronizer.py
@@ -1,7 +1,6 @@
 from unittest import mock
 
 import pytest
-from kafka.errors import KafkaError
 
 from app import db
 from app import threadctx
@@ -59,40 +58,3 @@ def test_synchronize_multiple_host_events(event_producer, kafka_producer, db_cre
 
     assert event_count == host_count
     assert event_producer._kafka_producer.send.call_count == host_count
-
-
-@pytest.mark.host_synchronizer
-@pytest.mark.parametrize(
-    "send_side_effects",
-    ((mock.Mock(), mock.Mock(**{"get.side_effect": KafkaError()})), (mock.Mock(), KafkaError("oops"))),
-)
-def test_synchrnize_after_kafka_producer_error(
-    send_side_effects,
-    kafka_producer,
-    event_producer,
-    event_datetime_mock,
-    db_create_multiple_hosts,
-    db_get_hosts,
-    inventory_config,
-):
-    event_producer._kafka_producer.send.side_effect = send_side_effects
-
-    host_count = 3
-    created_hosts = db_create_multiple_hosts(how_many=host_count)
-    created_host_ids = [str(host.id) for host in created_hosts]
-
-    hosts = db_get_hosts(created_host_ids)
-    assert hosts.count() == host_count
-
-    threadctx.request_id = UNKNOWN_REQUEST_ID_VALUE
-
-    with pytest.raises(KafkaError):
-        host_synchronizer_run(
-            inventory_config,
-            mock.Mock(),
-            db.session,
-            event_producer,
-            shutdown_handler=mock.Mock(**{"shut_down.return_value": False}),
-        )
-
-    assert event_producer._kafka_producer.send.call_count == (host_count - 1)
