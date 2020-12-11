d8ed87edb90609b42295cf982171b34b6b073bff
Brandon Squizzato <bsquizza@redhat.com>
2020-12-10 19:29:11 -0500

    Enable smoke tests

--- pr_check.sh
+++ pr_check.sh
@@ -37,14 +37,4 @@ CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
 curl -s $CICD_URL/bootstrap.sh -o bootstrap.sh
 source bootstrap.sh  # checks out bonfire and changes to "cicd" dir...
 source deploy_ephemeral_env.sh
-
-# Need to make a dummy results file to make tests pass
-cd ../..
-mkdir -p artifacts
-cat << EOF > artifacts/junit-dummy.xml
-<testsuite tests="1">
-    <testcase classname="dummy" name="dummytest"/>
-</testsuite>
-EOF
-
-# source smoke_test.sh
+source smoke_test.sh
