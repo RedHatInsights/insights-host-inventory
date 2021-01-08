d0b16b378569e912adee2f8f5a9cf153bc7ea185
tahmidefaz <tefaz@redhat.com>
2021-01-08 16:47:50 -0500

    reverting back to using Draft7Validator
    This commit reverts back to using a Draft7Validator. It introduces a helper function to copy over connexion's customvValidators and merge them with the Draft7 one. It also updates the test which was not correctly implemented earlier.

--- app/custom_validator.py
+++ app/custom_validator.py
@@ -1,10 +1,28 @@
 from connexion.decorators.response import ResponseValidator
 from connexion.json_schema import Draft4ResponseValidator
+from jsonschema import Draft7Validator
 from jsonschema.validators import extend
 
-CustomDraft4ResponseValidator = extend(
-    Draft4ResponseValidator, {"x-propertyNames": Draft4ResponseValidator.VALIDATORS.get("propertyNames")}
-)
+
+def get_draft4_custom_validators():
+    """
+    Copies over the custom validators from connexion's Draft4ResponseValidator and returns them in a new dictionary
+    """
+    custom_validator_names = ["type", "enum", "required", "writeOnly", "x-writeOnly"]
+    custom_validators = {}
+
+    for name in custom_validator_names:
+        custom_validators[name] = Draft4ResponseValidator.VALIDATORS.get(name)
+
+    return custom_validators
+
+
+def get_custom_validator():
+    custom_validators = get_draft4_custom_validators()
+    CustomDraft7Validator = extend(
+        Draft7Validator, {**custom_validators, "x-propertyNames": Draft7Validator.VALIDATORS.get("propertyNames")}
+    )
+    return CustomDraft7Validator
 
 
 class CustomResponseValidator(ResponseValidator):
@@ -15,7 +33,7 @@ class CustomResponseValidator(ResponseValidator):
     """
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, validator=CustomDraft4ResponseValidator, **kwargs)
+        super().__init__(*args, validator=get_custom_validator(), **kwargs)
 
 
 VALIDATOR_MAP = {"response": CustomResponseValidator}
