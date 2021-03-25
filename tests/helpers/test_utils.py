129d7298d1fc26ff331500c9dcd8741f095d9e7a
Greg McCullough <gmccullo@redhat.com>
2021-03-25 08:49:57 -0400

    Fixes https://issues.redhat.com/browse/HBISPSC-38 (#835)
    Turns out sestatus code allows a value like error (X)" where X is more info about the error.
    This change allows the selinux_config_file field to contain any string up to 128 characters.
    
    Co-authored-by: Tahmid Efaz <tefaz@redhat.com>

--- tests/helpers/test_utils.py
+++ tests/helpers/test_utils.py
@@ -150,6 +150,8 @@ def valid_system_profile():
         "installed_services": ["ndb", "krb5"],
         "enabled_services": ["ndb", "krb5"],
         "sap_sids": ["ABC", "DEF", "GHI"],
+        "selinux_current_mode": "enforcing",
+        "selinux_config_file": "enforcing",
     }
 
 
