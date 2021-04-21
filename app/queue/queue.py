ddac8daf558cfef89fbf3ff9e5b6f04825fb5389
Tahmid Efaz <tefaz@redhat.com>
2021-04-21 15:03:11 -0400

    increment correct metrics for ValidationException (#884)

--- app/queue/queue.py
+++ app/queue/queue.py
@@ -183,7 +183,7 @@ def update_system_profile(host_data, identity):
             payload_tracker_processing_ctx.inventory_id = output_host["id"]
             return output_host, host_id, insights_id, update_result
         except ValidationException:
-            metrics.update_system_profile_failure.labels("Exception").inc()
+            metrics.update_system_profile_failure.labels("InventoryException").inc()
             raise
         except InventoryException:
             log_update_system_profile_failure(logger, host_data)
@@ -219,7 +219,7 @@ def add_host(host_data, identity):
             payload_tracker_processing_ctx.inventory_id = output_host["id"]
             return output_host, host_id, insights_id, add_result
         except ValidationException:
-            metrics.add_host_failure.labels("Exception", host_data.get("reporter", "null")).inc()
+            metrics.add_host_failure.labels("InventoryException", host_data.get("reporter", "null")).inc()
             raise
         except InventoryException:
             log_add_host_failure(logger, host_data)
