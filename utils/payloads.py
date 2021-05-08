f33a4c772df8d94c0feba470271cdb3b62006e44
thearifismail <arifismail@yahoo.com>
2021-05-08 10:34:56 -0400

    Add system_profile mq handler

--- utils/payloads.py
+++ utils/payloads.py
@@ -7,13 +7,13 @@ from datetime import timedelta
 from datetime import timezone
 
 # complete system identity
-# IDENTITY = {
-#     "account_number": "test",
-#     "type": "System",
-#     "auth_type": "cert-auth",
-#     "system": {"cn": "1b36b20f-7fa0-4454-a6d2-008294e06378", "cert_type": "system"},
-#     "internal": {"org_id": "3340851", "auth_time": 6300},
-# }
+IDENTITY = {
+    "account_number": "test",
+    "type": "System",
+    "auth_type": "cert-auth",
+    "system": {"cn": "1b36b20f-7fa0-4454-a6d2-008294e06378", "cert_type": "system"},
+    "internal": {"org_id": "3340851", "auth_time": 6300},
+}
 
 # system identity: invalid or incomplete for testing
 # IDENTITY = {
@@ -25,12 +25,12 @@ from datetime import timezone
 # }
 
 # complete user identity
-IDENTITY = {
-    "account_number": "test",
-    "type": "User",
-    "auth_type": "basic-auth",
-    "user": {"email": "tuser@redhat.com", "first_name": "test"},
-}
+# IDENTITY = {
+#     "account_number": "test",
+#     "type": "User",
+#     "auth_type": "basic-auth",
+#     "user": {"email": "tuser@redhat.com", "first_name": "test"},
+# }
 
 # incomplete or invalid user identity for testing
 # IDENTITY = {
