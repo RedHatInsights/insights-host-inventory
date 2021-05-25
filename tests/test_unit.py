183ed061bbfb260008dc37d891a75b24c38ec96c
Asa Price <aprice@redhat.com>
2021-05-25 10:12:23 -0400

    Make cert_type and auth_type case-insensitive (#914)
    * Make cert_type and auth_type case-insensitive
    
    * Fix comments
    
    * Remove redundant value, and fill out auth_types
    
    * Use square brackets for clarity

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -228,6 +228,32 @@ class AuthIdentityValidateTestCase(TestCase):
                 with self.assertRaises(ValueError):
                     Identity(test_identity)
 
+    def test_case_insensitive_cert_types(self):
+        # Validate that cert_type is case-insensitive
+        test_identity = deepcopy(SYSTEM_IDENTITY)
+        cert_types = ["RHUI", "Satellite", "system"]
+        for cert_type in cert_types:
+            with self.subTest(cert_type=cert_type):
+                test_identity["system"]["cert_type"] = cert_type
+                try:
+                    Identity(test_identity)
+                    self.assertTrue(True)
+                except Exception:
+                    self.fail()
+
+    def test_case_insensitive_auth_types(self):
+        # Validate that auth_type is case-insensitive
+        test_identity = deepcopy(SYSTEM_IDENTITY)
+        auth_types = ["CLASSIC-PROXY", "Cert-Auth", "basic-auth"]
+        for auth_type in auth_types:
+            with self.subTest(auth_type=auth_type):
+                test_identity["auth_type"] = auth_type
+                try:
+                    Identity(test_identity)
+                    self.assertTrue(True)
+                except Exception:
+                    self.fail()
+
 
 class TrustedIdentityTestCase(TestCase):
     shared_secret = "ImaSecret"
