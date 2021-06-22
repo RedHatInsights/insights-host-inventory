c910129e7e11518bf71f2e18d876fdf12e1c3e49
Tahmid Efaz <tefaz@redhat.com>
2021-06-22 17:14:54 -0400

    add encryption configs for kafka producer

--- app/config.py
+++ app/config.py
@@ -148,6 +148,11 @@ class Config:
             "max_in_flight_requests_per_connection": int(
                 os.environ.get("KAFKA_PRODUCER_MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION", "5")
             ),
+            "security_protocol": os.environ.get("KAFKA_SECURITY_PROTOCOL", "PLAINTEXT").upper(),
+            "ssl_cafile": os.environ.get("KAFKA_SSL_CAFILE"),
+            "sasl_mechanism": os.environ.get("KAFKA_SASL_MECHANISM", "").upper(),
+            "sasl_plain_username": os.environ.get("KAFKA_SASL_USERNAME", ""),
+            "sasl_plain_password": os.environ.get("KAFKA_SASL_PASSWORD", ""),
         }
 
         self.payload_tracker_service_name = os.environ.get("PAYLOAD_TRACKER_SERVICE_NAME", "inventory")
