4e3364bdf29474829f597733785f38f7baf625fc
Asa Price <aprice@redhat.com>
2021-07-07 16:12:18 -0400

    Don't try to get user/pass if sasl property doesn't exist (#950)
    * Don't try to get user/pass if sasl property doesn't exist
    
    * Set cafile to None instead of not declaring it

--- app/config.py
+++ app/config.py
@@ -51,9 +51,14 @@ class Config:
         self.kafka_consumer_topic = topic(os.environ.get("KAFKA_CONSUMER_TOPIC", "platform.inventory.host-ingress"))
         self.event_topic = topic("platform.inventory.events")
         self.payload_tracker_kafka_topic = topic("platform.payload-status")
-        self.kafka_ssl_cafile = self._kafka_ca(broker_cfg.cacert)
-        self.kafka_sasl_username = broker_cfg.sasl.username
-        self.kafka_sasl_password = broker_cfg.sasl.password
+        if broker_cfg.sasl:
+            self.kafka_ssl_cafile = self._kafka_ca(broker_cfg.cacert)
+            self.kafka_sasl_username = broker_cfg.sasl.username
+            self.kafka_sasl_password = broker_cfg.sasl.password
+        else:
+            self.kafka_ssl_cafile = None
+            self.kafka_sasl_username = ""
+            self.kafka_sasl_password = ""
 
     def non_clowder_config(self):
         self.metrics_port = 9126
