ff605e10e577ce50518a4b7d66d5316fc0e0da21
Ryan Dillin Felton <ryandillinfelton@gmail.com>
2022-01-27 17:23:26 -0500

    Only allow [contains] operand on array fields [ESSNTL-2069] (#1078)
    * Made operation validation aware of arrays. Contains only allowed on arrays
    
    * remove print
    
    * removed change to matces (it was unneccesary) extended [contains] removal to over types
    
    * disable contains for booleans and objects
    
    * made lookup always return array. Inlined is_array
    
    * fixed failing test

--- api/filtering/filtering_common.py
+++ api/filtering/filtering_common.py
@@ -9,12 +9,21 @@ class OPERATION_SETS(Enum):
 
 
 SPEC_OPERATIONS_LOOKUP = {
-    "string": OPERATION_SETS.eq.value,
-    "wildcard": OPERATION_SETS.eq.value,  # because on our side we want eq
-    "boolean": OPERATION_SETS.eq.value,
+    "string": [OPERATION_SETS.eq.value[0]],
+    "wildcard": [OPERATION_SETS.eq.value[0]],  # because on our side we want eq
+    "boolean": [OPERATION_SETS.eq.value[0]],
     "range": OPERATION_SETS.range.value,
     "operating_system": OPERATION_SETS.range.value,
-    "integer": OPERATION_SETS.eq.value[0],
+    "integer": [OPERATION_SETS.eq.value[0]],
+}
+
+ARRAY_SPEC_OPERATIONS_LOOKUP = {
+    "string": OPERATION_SETS.eq.value,
+    "wildcard": OPERATION_SETS.eq.value,  # because on our side we want eq
+    "boolean": [OPERATION_SETS.eq.value[0]],
+    "range": [OPERATION_SETS.range.value[0]],
+    "operating_system": OPERATION_SETS.range.value,
+    "integer": [OPERATION_SETS.eq.value[0]],
 }
 
 GRAPHQL_OPERATIONS_LOOKUP = {
@@ -27,7 +36,9 @@ GRAPHQL_OPERATIONS_LOOKUP = {
 }
 
 
-def lookup_operations(filter_type):
+def lookup_operations(filter_type, is_array=False):
+    if is_array:
+        return ARRAY_SPEC_OPERATIONS_LOOKUP[filter_type]
     return SPEC_OPERATIONS_LOOKUP[filter_type]
 
 
