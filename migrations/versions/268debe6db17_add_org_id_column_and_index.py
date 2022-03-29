ee2825f48b93807aa5bf19f5cfd5b30dc1b25d17
Asa Price <aprice@redhat.com>
2022-03-29 15:33:12 -0400

    reduce length to 36, run make style

--- migrations/versions/268debe6db17_add_org_id_column_and_index.py
+++ migrations/versions/268debe6db17_add_org_id_column_and_index.py
@@ -5,19 +5,19 @@ Revises: 3d06b0a8828f
 Create Date: 2022-03-28 13:46:34.982026
 
 """
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
 
 
 # revision identifiers, used by Alembic.
-revision = '268debe6db17'
-down_revision = '3d06b0a8828f'
+revision = "268debe6db17"
+down_revision = "3d06b0a8828f"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-    op.add_column("hosts", sa.Column("org_id", sa.String(length=255), nullable=True))
+    op.add_column("hosts", sa.Column("org_id", sa.String(length=36), nullable=True))
     op.create_index("idxorgid", "hosts", ["org_id"])
 
 
