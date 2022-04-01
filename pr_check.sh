4f4d70579546f48e2f69dcd04e6bd1201f0f6cd5
Brandon Squizzato <bsquizza@redhat.com>
2022-04-01 15:13:01 -0400

    Test bonfire v4

--- pr_check.sh
+++ pr_check.sh
@@ -28,7 +28,7 @@ IQE_CJI_TIMEOUT="30m"
 # ---------------------------
 
 # Get bonfire helper scripts
-CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
+CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/switch_prs_to_v4/cicd
 curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
 
 # build the PR commit image
