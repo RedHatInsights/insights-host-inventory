c2083d8a7ceedbb85df4b8a51309111fbd640e79
Asa Price <aprice@redhat.com>
2022-09-07 14:34:54 -0400

    fix event-rebuilder

--- rebuild_events_topic.py
+++ rebuild_events_topic.py
@@ -41,7 +41,10 @@ def run(config, logger, session, consumer, event_producer, shutdown_handler):
     partitions = []
 
     # Seek to beginning
-    for partition_id in consumer.partitions_for_topic(config.event_topic) or []:
+    logger.debug(f"Partitions for topic {config.event_topic}:")
+    logger.debug(consumer.partitions_for_topic(config.event_topic))
+    for partition_id in consumer.partitions_for_topic(config.event_topic):
+        logger.debug(f"Appending partition {partition_id} for topic: {config.event_topic}")
         partitions.append(TopicPartition(config.event_topic, partition_id))
 
     consumer.assign(partitions)
@@ -49,6 +52,7 @@ def run(config, logger, session, consumer, event_producer, shutdown_handler):
     consumer.seek_to_beginning(*partitions)
     total_messages_processed = 0
 
+    logger.debug("About to start the consumer loop")
     while num_messages > 0 and not shutdown_handler.shut_down():
         num_messages = 0
         msgs = consumer.poll(timeout_ms=1000)
