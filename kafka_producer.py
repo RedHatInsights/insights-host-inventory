af9e400631e5d38978f112aa7eddf205b85d72cd
Asa Price <aprice@redhat.com>
2022-09-07 14:01:57 -0400

    Fix app setup for producer

--- kafka_producer.py
+++ kafka_producer.py
@@ -1,12 +1,12 @@
 import logging
 import os
 
-import payloads
 from ttictoc import TicToc
 
-from app.config import Config
+from app import create_app
 from app.environment import RuntimeEnvironment
 from app.queue.event_producer import EventProducer
+from utils import payloads
 
 
 NUM_HOSTS = int(os.environ.get("NUM_HOSTS", 20))
@@ -15,11 +15,12 @@ NUM_HOSTS = int(os.environ.get("NUM_HOSTS", 20))
 def main():
     # Create list of host payloads to add to the message queue
     # payloads.build_payloads takes two optional args: number of hosts, and payload type ("default", "rhsm", "qpc")
+    application = create_app(RuntimeEnvironment.JOB)
+    config = application.config["INVENTORY_CONFIG"]
+
     with TicToc("Build payloads"):
         all_payloads = [payloads.build_mq_payload() for _ in range(NUM_HOSTS)]
     print("Number of hosts (payloads): ", len(all_payloads))
-
-    config = Config(RuntimeEnvironment.JOB)
 
     producer = EventProducer(config, "platform.inventory.host-ingress")
 
