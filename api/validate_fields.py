9f6ad9b31ba9721ad3e21a5669917e2a877b38f0
codeheeler <dawalker@redhat.com>
2022-09-08 15:47:52 -0400

    Adjust import, use existing spec function

--- api/validate_fields.py
+++ api/validate_fields.py
@@ -1,12 +1,12 @@
 import flask
 
-from app.__init__ import process_system_profile_spec
+from app import system_profile_spec
 
 
 def validate_fields_in_schema(fields):
     if fields.get("system_profile"):
         query_fields = list(fields.get("system_profile").keys())
-        system_profile_schema = process_system_profile_spec()
+        system_profile_schema = system_profile_spec()
         for field in query_fields:
             if field not in system_profile_schema.keys():
                 flask.abort(400, f"Requested field '{field}' is not present in the system_profile schema.")
