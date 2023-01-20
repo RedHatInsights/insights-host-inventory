356fec0cd4d5464978d7c3bd8e3b8fb34a79c833
Fabricia Diniz <agomesdi@redhat.com>
2023-01-20 04:02:18 +0000

    ESSNTL-4070: adds group.py file

---
+++ api/group.py
@@ -0,0 +1,30 @@
+def get_group_list():
+    pass
+
+
+def create_group():
+    pass
+
+
+def get_group():
+    pass
+
+
+def merge_group():
+    pass
+
+
+def replace_group_details():
+    pass
+
+
+def delete_group():
+    pass
+
+
+def get_groups_by_id():
+    pass
+
+
+def delete_hosts_from_group():
+    pass
