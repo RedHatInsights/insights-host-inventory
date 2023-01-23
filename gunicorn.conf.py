d95eb31c3122b811ad7434692946cfa3f9768015
Asa Price <aprice@redhat.com>
2023-01-23 17:08:06 -0500

    fix(ESSNTL-4096): Enable multiproc metrics, now using separate server (#1258)

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,13 @@
-from prometheus_client import multiprocess
+from prometheus_flask_exporter.multiprocess import GunicornPrometheusMetrics
+
+from app.config import Config
+from app.environment import RuntimeEnvironment
+
+
+def when_ready(server):
+    config = Config(RuntimeEnvironment.SERVICE)
+    GunicornPrometheusMetrics.start_http_server_when_ready(config.metrics_port)
 
 
 def child_exit(server, worker):
-    multiprocess.mark_process_dead(worker.pid)
+    GunicornPrometheusMetrics.mark_process_dead_on_child_exit(worker.pid)
