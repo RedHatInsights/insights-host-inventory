98c15d5bfa11b64023b40b971449d8e9780a5c0b
Fabricia Diniz <agomesdi@redhat.com>
2023-01-24 14:32:33 +0000

    ESSNTL-4070: add args to group.py methods

--- api/group.py
+++ api/group.py
@@ -1,30 +1,37 @@
-def get_group_list():
+def get_group_list(
+    group_name=None,
+    page=1,
+    per_page=100,
+    order_by=None,
+    order_how=None,
+    staleness=None,
+):
     pass
 
 
-def create_group():
+def create_group(body):
     pass
 
 
-def get_group():
+def get_group(group_id):
     pass
 
 
-def patch_group_by_id():
+def patch_group_by_id(group_id, body):
     pass
 
 
-def update_group_details():
+def update_group_details(group_id, body):
     pass
 
 
-def delete_group():
+def delete_group(group_id):
     pass
 
 
-def get_groups_by_id():
+def get_groups_by_id(group_id_list):
     pass
 
 
-def delete_hosts_from_group():
+def delete_hosts_from_group(group_id, host_id_list):
     pass
