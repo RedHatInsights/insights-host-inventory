60d148ed0e66a432c025c59acf0d22ebbe476d30
Fabrícia Diniz <104025448+FabriciaDinizRH@users.noreply.github.com>
2023-01-24 18:51:03 +0000

    ESSNTL-4070: add paths for groups (#1256)
    * ESSNTL-4070: add paths for groups
    
    * ESSNTL:4070 - remove comments and fix schema errors
    
    * ESSNTL-4070: fixes missing parameter in /groups/{group_id} get
    
    * comment out operationIds
    
    * ESSNTL-4070: fix patch operationId
    
    * ESSNTL-4070: adds group.py file
    
    * ESSNTL-4070: change the name of the methods
    
    * ESSNTL-4070: add args to group.py methods
    
    * ESSNTL-4070: improve args naming

---
+++ api/group.py
@@ -0,0 +1,36 @@
+def get_group_list(
+    group_name=None,
+    page=1,
+    per_page=100,
+    order_by=None,
+    order_how=None,
+):
+    pass
+
+
+def create_group(group_data):
+    pass
+
+
+def get_group(group_id):
+    pass
+
+
+def patch_group_by_id(group_id, group_dat):
+    pass
+
+
+def update_group_details(group_id, group_daty):
+    pass
+
+
+def delete_group(group_id):
+    pass
+
+
+def get_groups_by_id(group_id_list):
+    pass
+
+
+def delete_hosts_from_group(group_id, host_id_list):
+    pass
