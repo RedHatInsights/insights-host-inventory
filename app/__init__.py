f2446abc7ffa27717fdfa79e6b496d8d0e9f9950
Asa Price <aprice@redhat.com>
2023-02-07 09:32:24 -0500

    Fix config field name

--- app/__init__.py
+++ app/__init__.py
@@ -206,8 +206,8 @@ def create_app(runtime_environment):
         flask_app.config["UNLEASH_ENVIRONMENT"] = "default"
         flask_app.config["UNLEASH_URL"] = app_config.unleash_url
         flask_app.config["UNLEASH_CUSTOM_HEADERS"] = {"Authorization": app_config.unleash_token}
-        if app_config.unleash_cache_dir:
-            flask_app.config["UNLEASH_CACHE_DIRECTORY"] = app_config.unleash_cache_dir
+        if app_config.unleash_cache_directory:
+            flask_app.config["UNLEASH_CACHE_DIRECTORY"] = app_config.unleash_cache_directory
         init_unleash_app(flask_app)
     else:
         logger.warning(
