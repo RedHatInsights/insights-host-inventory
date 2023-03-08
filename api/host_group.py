a554f6355566be26ddd87a081a102a2192620880
Fellipe Henrique <2904206+fellipeh@users.noreply.github.com>
2023-03-08 15:06:45 +0100

    feat(ESSNTL-4375) - Create POST /groups/<group-id>/hosts endpoint
    This PR is being created to address ESSNTL-4375.
    Adds the POST `/groups/{group_id}/hosts` endpoint. The post doc looks like the following:
    
    ```
    [
      "host_id_1",
      "host_id_2",
      "host_id_3"
    ]
    ```
    If the list is empty, it fails validation and returns 400. If "host_ids" is provided, the list of hosts will be associate to the group.

---
+++ api/host_group.py
@@ -0,0 +1,45 @@
+import flask
+from flask_api import status
+
+from api import api_operation
+from api import flask_json_response
+from api import metrics
+from api.group_query import build_group_response
+from app import db
+from app import Permission
+from app.instrumentation import log_host_group_add_succeeded
+from app.instrumentation import log_patch_group_failed
+from app.logging import get_logger
+from lib.group_repository import add_host_list_for_group
+from lib.group_repository import get_group_by_id_from_db
+from lib.group_repository import get_hosts_from_db
+from lib.middleware import rbac
+
+logger = get_logger(__name__)
+
+
+@api_operation
+@rbac(Permission.WRITE)
+@metrics.api_request_time.time()
+def add_hosts_to_group(group_id, body):
+    group_to_update = get_group_by_id_from_db(group_id)
+
+    if not group_to_update:
+        log_patch_group_failed(logger, group_id)
+        return flask.abort(status.HTTP_404_NOT_FOUND)
+
+    host_id_list = body
+    if not get_hosts_from_db(host_id_list):
+        return flask.abort(status.HTTP_404_NOT_FOUND)
+
+    # Next, add the host-group associations
+    assoc_list = []
+    if host_id_list is not None:
+        assoc_list = add_host_list_for_group(db.session, group_to_update, host_id_list)
+
+    if any(db.session.is_modified(assoc) for assoc in assoc_list):
+        db.session.commit()
+
+    updated_group = get_group_by_id_from_db(group_id)
+    log_host_group_add_succeeded(logger, host_id_list, group_id)
+    return flask_json_response(build_group_response(updated_group), status.HTTP_200_OK)
