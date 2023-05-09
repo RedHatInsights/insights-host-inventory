4b90159e0ed8e9491ed3d8b813edc2d2b1c30e1e
Asa Price <aprice@redhat.com>
2023-05-09 11:31:10 -0400

    Update resource format

--- tests/test_api_resource_types_get.py
+++ tests/test_api_resource_types_get.py
@@ -20,7 +20,7 @@ def test_resource_types_groups_data(api_get, db_create_group):
     assert_response_status(response_status, 200)
 
     for group_result in response_data["data"]:
-        assert group_result["id"] in group_id_list
+        assert group_result["value"] in group_id_list
 
     assert_resource_types_pagination(response_data, 1, 1, "/inventory/v1/resource-types/inventory-groups/")
 
@@ -28,14 +28,12 @@ def test_resource_types_groups_data(api_get, db_create_group):
 def test_resource_types_groups_pagination(api_get, db_create_group, subtests):
     # Create a bunch of groups, then use subtests to validate output with different pagination params
     num_groups = 40
-    for idx in range(num_groups):
-        # Use leading zeros so that we can sort by name
-        db_create_group(f"testGroup_{idx:03}")
+    host_ids = [str(db_create_group(f"testGroup_{idx}").id) for idx in range(num_groups)]
 
     for per_page in [1, 5, 10]:
         for page in [1, 2, 3]:
             with subtests.test():
-                query = f"?page={page}&per_page={per_page}&order_by=name"
+                query = f"?page={page}&per_page={per_page}"
                 response_status, response_data = response_status, response_data = api_get(
                     build_resource_types_groups_url(query=query)
                 )
@@ -46,4 +44,4 @@ def test_resource_types_groups_pagination(api_get, db_create_group, subtests):
                 )
 
                 for idx in range(per_page):
-                    assert response_data["data"][idx]["name"] == f"testGroup_{((page-1)*per_page + idx):03}"
+                    assert response_data["data"][idx]["value"] in host_ids
