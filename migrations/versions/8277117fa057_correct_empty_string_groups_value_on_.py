cad2f604a672620797ec266cd4658c99b193c9c5
Asa Price <aprice@redhat.com>
2023-06-06 11:34:02 -0400

    Replace empty-string host.groups values with empty arrays instead

---
+++ migrations/versions/8277117fa057_correct_empty_string_groups_value_on_.py
@@ -0,0 +1,27 @@
+"""correct empty string groups value on some hosts
+
+Revision ID: 8277117fa057
+Revises: b48ad9e4c8c5
+Create Date: 2023-06-06 11:26:53.810997
+
+"""
+from app.models import Host
+from migrations.helpers import session
+
+# revision identifiers, used by Alembic.
+revision = "8277117fa057"
+down_revision = "b48ad9e4c8c5"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    with session() as s:
+        # For all hosts that have an empty string in their `groups` field,
+        # set the `groups` field to an empty array instead.
+        s.query(Host).filter(Host.groups == "").update({Host.groups: []})
+
+
+def downgrade():
+    # Nothing to do; we don't want to un-correct the data
+    pass
