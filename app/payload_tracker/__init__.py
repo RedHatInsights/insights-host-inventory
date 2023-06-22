83ac6ae233c4c7166f5a3ffa66ec248cf8d3e5f5
thearifismail <arifismail@yahoo.com>
2023-06-22 16:02:22 -0400

    ESSNTL-4044 add poll() call to clean up queue

--- app/payload_tracker/__init__.py
+++ app/payload_tracker/__init__.py
@@ -185,6 +185,7 @@ class KafkaPayloadTracker(PayloadTracker):
 
         try:
             self._producer.produce(self._topic, message.encode("utf-8"))
+            self._producer.poll(0)
         except Exception:
             logger.exception("Error sending payload tracker message")
             metrics.payload_tracker_message_send_failure.inc()
