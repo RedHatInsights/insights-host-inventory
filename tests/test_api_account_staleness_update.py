b6678b8409b22248bb7d17746cb781510b670ad0
Fellipe Henrique <2904206+fellipeh@users.noreply.github.com>
2023-07-31 15:50:18 -0300

    Moving reset test method to update test file.
    Signed-off-by: Fellipe Henrique <2904206+fellipeh@users.noreply.github.com>

--- tests/test_api_account_staleness_update.py
+++ tests/test_api_account_staleness_update.py
@@ -6,3 +6,9 @@ def test_update_staleness(api_patch, subtests):
     url = build_account_staleness_url()
     response_status, response_data = api_patch(url, host_data=None)
     assert_response_status(response_status, 200)
+
+
+def test_reset_staleness(api_patch, subtests):
+    url = build_account_staleness_url(path="/reset")
+    response_status, response_data = api_patch(url, host_data=None)
+    assert_response_status(response_status, 200)
