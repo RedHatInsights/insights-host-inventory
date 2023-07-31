ef703c352ead362c25e48406bf06f19e61fe892c
Richard Oliveri <roliveri@redhat.com>
2023-07-31 13:59:24 -0400

    Rebased with Master.
    Rebased with Master.

--- api/assignment_rule.py
+++ api/assignment_rule.py
@@ -9,6 +9,7 @@ from api import flask_json_response
 from api import json_error_response
 from api import metrics
 from api.assignment_rule_query import build_paginated_assignmentrule_list_response
+from api.assignment_rule_query import get_assignment_rules_list_by_id_list_db
 from api.assignment_rule_query import get_filtered_assignment_rule_list_db
 from app import RbacPermission
 from app import RbacResourceType
@@ -89,3 +90,57 @@ def create_assignment_rule(body, rbac_filter=None):
 
     log_post_assignment_rule_succeeded(logger, created_assignment_rule.get("id"))
     return flask_json_response(created_assignment_rule, status.HTTP_201_CREATED)
+
+
+@api_operation
+@rbac(RbacResourceType.GROUPS, RbacPermission.WRITE)
+@metrics.api_request_time.time()
+def patch_assignment_rule_by_id(group_id, body, rbac_filter=None):
+    return True
+
+
+@api_operation
+@rbac(RbacResourceType.GROUPS, RbacPermission.WRITE)
+@metrics.api_request_time.time()
+def delete_assignment_rules(group_id_list, rbac_filter=None):
+    return True
+
+
+@api_operation
+@rbac(RbacResourceType.GROUPS, RbacPermission.READ)
+@metrics.api_request_time.time()
+def get_assignment_rules_by_id(
+    assignment_rule_id_list,
+    page=1,
+    per_page=100,
+    order_by=None,
+    order_how=None,
+    rbac_filter=None,
+):
+    if not get_flag_value(FLAG_INVENTORY_ASSIGNMENT_RULES):
+        return Response(None, status.HTTP_501_NOT_IMPLEMENTED)
+
+    try:
+        assignment_rule_list, total = get_assignment_rules_list_by_id_list_db(
+            assignment_rule_id_list, page, per_page, order_by, order_how, rbac_filter
+        )
+    except ValueError as e:
+        log_get_assignment_rules_list_failed(logger)
+        abort(400, str(e))
+
+    log_get_assignment_rules_list_succeeded(logger, assignment_rule_list)
+
+    return flask_json_response(
+        build_paginated_assignmentrule_list_response(total, page, per_page, assignment_rule_list)
+    )
+
+
+@api_operation
+@rbac(RbacResourceType.GROUPS, RbacPermission.WRITE)
+@metrics.api_request_time.time()
+def delete_hosts_from_assignment_rule(assignment_rule_id, host_id_list, rbac_filter=None):
+    return True
+
+
+def _error_json_response(title, detail, status=status.HTTP_400_BAD_REQUEST):
+    return True
--- tests/test_api_assignment_rules_get.py
+++ tests/test_api_assignment_rules_get.py
@@ -12,6 +12,7 @@ def test_basic_assignment_rule_query(db_create_assignment_rule, db_create_group,
     assignment_rule_id_list = [
         str(db_create_assignment_rule(f"assignment {idx}", group.id, filter, True).id) for idx in range(3)
     ]
+
     response_status, response_data = api_get(build_assignment_rules_url())
 
     assert_response_status(response_status, 200)
@@ -136,3 +137,29 @@ def test_page_and_page_number(db_create_assignment_rule, db_create_group, api_ge
     assert new_response_status == 200
     assert new_response_data["page"] == page
     assert new_response_data["per_page"] == per_page
+
+
+@pytest.mark.parametrize(
+    "num_rules",
+    [1, 3, 5],
+)
+def test_assignment_rule_id_list_filter(num_rules, db_create_assignment_rule, db_create_group, api_get):
+    group = db_create_group("TestGroup")
+    filter = {"AND": [{"fqdn": {"eq": "foo.bar.com"}}]}
+
+    assignment_rule_id_list = [
+        str(db_create_assignment_rule(f"assignment {idx}", group.id, filter, True).id) for idx in range(num_rules)
+    ]
+    # Create extra rules to filter out
+    for idx in range(10):
+        db_create_assignment_rule(f"extraRule_{idx}", group.id, filter, True)
+
+    url = ASSIGNMENT_RULE_URL + "/" + ",".join(assignment_rule_id_list)
+    response_status, response_data = api_get(url)
+
+    assert response_status == 200
+    assert response_data["total"] == num_rules
+    assert response_data["count"] == num_rules
+    assert len(response_data["results"]) == num_rules
+    for rule_result in response_data["results"]:
+        assert rule_result["id"] in assignment_rule_id_list
