061205cc3847085358d51bfbc1c01c25b65e1589
Asa Price <aprice@redhat.com>
2023-08-02 11:09:37 -0400

    Add group name filter to /resource-types/inventory-groups endpoint

--- tests/test_api_resource_types_get.py
+++ tests/test_api_resource_types_get.py
@@ -4,6 +4,7 @@ from tests.helpers.api_utils import assert_resource_types_pagination
 from tests.helpers.api_utils import assert_response_status
 from tests.helpers.api_utils import build_resource_types_groups_url
 from tests.helpers.api_utils import build_resource_types_url
+from tests.helpers.api_utils import create_mock_rbac_response
 
 
 @pytest.mark.parametrize(
@@ -61,3 +62,35 @@ def test_resource_types_groups_pagination(api_get, db_create_group, subtests):
 
                 for idx in range(per_page):
                     assert response_data["data"][idx]["name"] == f"testGroup_{((page-1)*per_page + idx):03}"
+
+
+def test_resource_types_groups_filter(api_get, db_create_group):
+    # Create a few groups, request the data using the filter, then check that the data is correct in the response
+    included_group_id_list = [str(db_create_group(f"testGroup{idx}").id) for idx in range(3)]
+    excluded_group_id_list = [str(db_create_group(f"diffGroup{idx}").id) for idx in range(5)]
+
+    response_status, response_data = api_get(build_resource_types_groups_url(query="?name=testGroup"))
+
+    assert_response_status(response_status, 200)
+
+    for group_result in response_data["data"]:
+        assert group_result["id"] in included_group_id_list
+        assert group_result["id"] not in excluded_group_id_list
+
+    assert_resource_types_pagination(response_data, 1, 10, 1, "/inventory/v1/resource-types/inventory-groups")
+
+
+@pytest.mark.parametrize(
+    "url_builder",
+    [build_resource_types_url, build_resource_types_groups_url],
+)
+def test_get_resource_types_RBAC_allowed(mocker, api_get, url_builder, enable_rbac):
+    get_rbac_permissions_mock = mocker.patch("lib.middleware.get_rbac_permissions")
+
+    # RBAC admin should have permission to both resource-types endpoints
+    mock_rbac_response = create_mock_rbac_response("tests/helpers/rbac-mock-data/rbac-admin.json")
+
+    get_rbac_permissions_mock.return_value = mock_rbac_response
+
+    response_status, _ = api_get(url_builder())
+    assert_response_status(response_status, 200)
