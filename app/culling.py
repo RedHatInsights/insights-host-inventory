f814886dd01eb1879fc47912d10de0f7f3dcbd19
Joao Paulo Ramos <jramos@redhat.com>
2023-08-03 15:38:58 -0300

    feat(ESSNTL-5038): Add system default values handling

--- app/culling.py
+++ app/culling.py
@@ -3,6 +3,8 @@ from datetime import datetime
 from datetime import timedelta
 from datetime import timezone
 
+from app import inventory_config
+
 __all__ = ("Conditions", "staleness_to_conditions", "Timestamps")
 
 
@@ -71,3 +73,28 @@ def staleness_to_conditions(config, staleness, timestamp_filter_func):
     condition = Conditions.from_config(config)
     filtered_states = (state for state in staleness if state not in ("unknown",))
     return (timestamp_filter_func(*getattr(condition, state)()) for state in filtered_states)
+
+
+class AttrDict(dict):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__dict__ = self
+
+
+def build_acc_staleness_sys_default(org_id, account):
+    config = inventory_config()
+    return AttrDict(
+        {
+            "id": "system_default",
+            "account": account,
+            "org_id": org_id,
+            "conventional_staleness_delta": config.conventional_staleness_seconds,
+            "conventional_stale_warning_delta": config.conventional_stale_warning_seconds,
+            "conventional_culling_delta": config.conventional_culling_seconds,
+            "immutable_staleness_delta": config.immutable_staleness_seconds,
+            "immutable_stale_warning_delta": config.immutable_stale_warning_seconds,
+            "immutable_culling_delta": config.immutable_culling_seconds,
+            "created_at": datetime.now(timezone.utc),
+            "updated_at": datetime.now(timezone.utc),
+        }
+    )
