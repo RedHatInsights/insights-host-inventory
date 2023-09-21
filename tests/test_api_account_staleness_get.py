0ac2adf47d37ebd7ea0c644ac529e70f533bece0
Joao Paulo Ramos <jramos@redhat.com>
2023-09-21 16:14:29 -0300

    feat(ESSNTL-5424): Add tests

--- tests/test_api_account_staleness_get.py
+++ tests/test_api_account_staleness_get.py
@@ -1,6 +1,7 @@
 from tests.helpers.api_utils import _INPUT_DATA
 from tests.helpers.api_utils import assert_response_status
 from tests.helpers.api_utils import build_account_staleness_url
+from tests.helpers.api_utils import build_sys_default_staleness_url
 
 
 def test_get_default_staleness(api_get):
@@ -34,3 +35,23 @@ def test_get_custom_staleness(api_create_account_staleness, api_get):
     assert response_data["immutable_stale_warning_delta"] == _INPUT_DATA["immutable_stale_warning_delta"]
     assert response_data["immutable_culling_delta"] == _INPUT_DATA["immutable_culling_delta"]
     assert_response_status(response_status, 200)
+
+
+def test_get_sys_default_staleness(api_get):
+    url = build_sys_default_staleness_url()
+    response_status, response_data = api_get(url)
+    assert_response_status(response_status, 200)
+    expected_result = {
+        "conventional_staleness_delta": "86400",
+        "conventional_stale_warning_delta": "604800",
+        "conventional_culling_delta": "1209600",
+        "immutable_staleness_delta": "172800",
+        "immutable_stale_warning_delta": "10368000",
+        "immutable_culling_delta": "15552000",
+    }
+    assert response_data["conventional_staleness_delta"] == expected_result["conventional_staleness_delta"]
+    assert response_data["conventional_stale_warning_delta"] == expected_result["conventional_stale_warning_delta"]
+    assert response_data["conventional_culling_delta"] == expected_result["conventional_culling_delta"]
+    assert response_data["immutable_staleness_delta"] == expected_result["immutable_staleness_delta"]
+    assert response_data["immutable_stale_warning_delta"] == expected_result["immutable_stale_warning_delta"]
+    assert response_data["immutable_culling_delta"] == expected_result["immutable_culling_delta"]
