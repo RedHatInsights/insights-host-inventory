e0c52c9464c3077cbeba67e7491acdfb1ee8667b
Fellipe Henrique <2904206+fellipeh@users.noreply.github.com>
2023-09-27 11:05:28 -0300

    fix(ESSNTL-5422): Improve error message
    Improving error messages on staleness endpoint
    
    Signed-off-by: Fellipe Henrique <2904206+fellipeh@users.noreply.github.com>

--- api/staleness.py
+++ api/staleness.py
@@ -58,6 +58,11 @@ def _validate_input_data(body):
     return validated_data
 
 
+def _get_org_id():
+    current_identity = get_current_identity()
+    return current_identity.org_id
+
+
 @api_operation
 @rbac(RbacResourceType.STALENESS, RbacPermission.READ)
 @metrics.api_request_time.time()
@@ -91,9 +96,6 @@ def create_staleness(body):
     if not get_flag_value(FLAG_INVENTORY_CUSTOM_STALENESS):
         return Response(None, status.HTTP_501_NOT_IMPLEMENTED)
 
-    current_identity = get_current_identity()
-    org_id = current_identity.org_id
-
     # Validate account staleness input data
     try:
         validated_data = StalenessSchema().load(body)
@@ -107,9 +109,9 @@ def create_staleness(body):
 
         log_create_staleness_succeeded(logger, created_staleness.id)
     except IntegrityError:
-        error_message = f"An account staleness with org_id {org_id} already exists."
+        error_message = f"An account staleness record for org_id {_get_org_id()} already exists."
 
-        log_create_staleness_failed(logger, org_id)
+        log_create_staleness_failed(logger, _get_org_id())
         logger.exception(error_message)
         return json_error_response("Integrity error", error_message, status.HTTP_400_BAD_REQUEST)
 
@@ -127,7 +129,7 @@ def delete_staleness():
         remove_staleness()
         return flask_json_response(None, status.HTTP_204_NO_CONTENT)
     except NoResultFound:
-        abort(status.HTTP_404_NOT_FOUND, "Account Staleness not found.")
+        abort(status.HTTP_404_NOT_FOUND, f"An account staleness record for org_id {_get_org_id()} does not exist.")
 
 
 @api_operation
@@ -149,4 +151,4 @@ def update_staleness(body):
 
         return flask_json_response(serialize_staleness_response(updated_staleness), status.HTTP_200_OK)
     except NoResultFound:
-        abort(status.HTTP_404_NOT_FOUND, "Account Staleness not found.")
+        abort(status.HTTP_404_NOT_FOUND, f"An account staleness record for org_id {_get_org_id()} does not exist.")
