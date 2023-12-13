36b6d3b294586c7aef5ad3e40b29d405e8f6d4c9
Fellipe Henrique <2904206+fellipeh@users.noreply.github.com>
2023-12-13 15:18:54 -0300

    fix(RHINENG-4565): Add staleness GET permission tests
    Make sure we test RBAC permissions to GET staleness endpoint
    
    Signed-off-by: Fellipe Henrique <2904206+fellipeh@users.noreply.github.com>

--- tests/test_api_staleness_get.py
+++ tests/test_api_staleness_get.py
@@ -2,6 +2,8 @@ from tests.helpers.api_utils import _INPUT_DATA
 from tests.helpers.api_utils import assert_response_status
 from tests.helpers.api_utils import build_staleness_url
 from tests.helpers.api_utils import build_sys_default_staleness_url
+from tests.helpers.api_utils import create_mock_rbac_response
+from tests.helpers.api_utils import STALENESS_READ_PROHIBITED_RBAC_RESPONSE_FILES
 
 
 def test_get_default_staleness(api_get):
@@ -55,3 +57,18 @@ def test_get_sys_default_staleness(api_get):
     assert response_data["immutable_time_to_stale"] == expected_result["immutable_time_to_stale"]
     assert response_data["immutable_time_to_stale_warning"] == expected_result["immutable_time_to_stale_warning"]
     assert response_data["immutable_time_to_delete"] == expected_result["immutable_time_to_delete"]
+
+
+def test_get_staleness_rbac_denied(subtests, mocker, api_get, db_get_staleness_culling, enable_rbac):
+    get_rbac_permissions_mock = mocker.patch("lib.middleware.get_rbac_permissions")
+    url = build_sys_default_staleness_url()
+
+    for response_file in STALENESS_READ_PROHIBITED_RBAC_RESPONSE_FILES:
+        mock_rbac_response = create_mock_rbac_response(response_file)
+
+        with subtests.test():
+            get_rbac_permissions_mock.return_value = mock_rbac_response
+
+            response_status, _ = api_get(url)
+
+            assert_response_status(response_status, 403)
