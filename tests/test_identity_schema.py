451252b7f19aff7548486604b58b0a7734574cb3
Fabrícia Diniz <104025448+FabriciaDinizRH@users.noreply.github.com>
2024-01-09 16:18:30 +0000

    feat(RHINENG-5479): Update API to handle Service Account type (#1588)
    Co-authored-by: Asa Price <aprice@redhat.com>

--- tests/test_identity_schema.py
+++ tests/test_identity_schema.py
@@ -6,6 +6,7 @@ from app.auth.identity import AuthType
 from app.auth.identity import CertType
 from app.auth.identity import IdentitySchema
 from app.auth.identity import IdentityType
+from tests.helpers.test_utils import SERVICE_ACCOUNT_IDENTITY
 from tests.helpers.test_utils import SYSTEM_IDENTITY
 from tests.helpers.test_utils import USER_IDENTITY
 
@@ -28,16 +29,22 @@ def identity_test_common(identity):
     if result.get("type") == IdentityType.USER:
         assert "user" in result
         assert type(result.get("user")) is dict
-    else:  # IdentityType.SYSTEM
+    elif result.get("type") == IdentityType.SYSTEM:
         assert "system" in result
         system = result.get("system")
         assert type(system) is dict
         assert "cert_type" in system
         assert system.get("cert_type") in CertType.__members__.values()
         assert "cn" in system
+    else:  # IdentityType.SERVICE_ACCOUNT
+        assert "service_account" in result
+        service_acc = result.get("service_account")
+        assert type(service_acc) is dict
+        assert "client_id" in service_acc
+        assert "username" in service_acc
 
 
-@pytest.mark.parametrize("identity", (USER_IDENTITY, SYSTEM_IDENTITY))
+@pytest.mark.parametrize("identity", (USER_IDENTITY, SYSTEM_IDENTITY, SERVICE_ACCOUNT_IDENTITY))
 def test_validate_valid_identity_schema(identity):
     identity_test_common(identity)
 
@@ -56,6 +63,10 @@ def test_validate_valid_user_identity_schema(remove_acct_number):
 
 def test_validate_valid_system_identity_schema():
     identity_test_common(SYSTEM_IDENTITY)
+
+
+def test_validate_valid_service_account_identity_schema():
+    identity_test_common(SERVICE_ACCOUNT_IDENTITY)
 
 
 @pytest.mark.parametrize("required", ("type", "auth_type", "org_id"))
@@ -100,6 +111,23 @@ def test_system_identity_missing_required(required):
     bad_identity = copy.deepcopy(SYSTEM_IDENTITY)
     assert "system" in bad_identity
     bad_identity["system"].pop(required, None)
+    with pytest.raises(ValueError):
+        identity_test_common(bad_identity)
+
+
+@pytest.mark.parametrize("required", ("client_id", "username"))
+def test_service_account_identity_missing_required(required):
+    # Test blank values
+    bad_identity = copy.deepcopy(SERVICE_ACCOUNT_IDENTITY)
+    assert "service_account" in bad_identity
+    bad_identity["service_account"][required] = ""
+    with pytest.raises(ValueError):
+        identity_test_common(bad_identity)
+
+    # Test missing values
+    bad_identity = copy.deepcopy(SERVICE_ACCOUNT_IDENTITY)
+    assert "service_account" in bad_identity
+    bad_identity["service_account"].pop(required, None)
     with pytest.raises(ValueError):
         identity_test_common(bad_identity)
 
