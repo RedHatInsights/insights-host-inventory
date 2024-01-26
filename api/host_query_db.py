6a2a99ae8ac3de43d8b54939f94893d36c2e3c44
Asa Price <aprice@redhat.com>
2024-01-26 11:41:25 -0500

    Replace tuples, fix typo

--- api/host_query_db.py
+++ api/host_query_db.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import List
 from uuid import UUID
 
 from dateutil import parser
@@ -31,11 +31,11 @@ def get_all_hosts():
 
 
 def _canonical_fact_filter(canonical_fact, value):
-    return (Host.canonical_facts[canonical_fact].astext == value,)
+    return [Host.canonical_facts[canonical_fact].astext == value]
 
 
 def _display_name_filter(display_name):
-    return (Host.display_name.comparator.contains(display_name),)
+    return [Host.display_name.comparator.contains(display_name)]
 
 
 def _tags_filter(string_tags):
@@ -46,15 +46,15 @@ def _tags_filter(string_tags):
 
     tags_to_find = Tag.create_nested_from_tags(tags)
 
-    return (Host.tags.contains(tags_to_find),)
+    return [Host.tags.contains(tags_to_find)]
 
 
 def _group_names_filter(group_name_list):
-    _query_filter = tuple()
+    _query_filter = []
     if len(group_name_list) > 0:
-        group_filters = (Group.name.in_(group_name_list),)
+        group_filters = [Group.name.in_(group_name_list)]
         if "" in group_name_list:
-            group_filters += (HostGroupAssoc.group_id.is_(None),)
+            group_filters += [HostGroupAssoc.group_id.is_(None)]
 
         _query_filter += (or_(*group_filters),)
 
@@ -62,31 +62,31 @@ def _group_names_filter(group_name_list):
 
 
 def _group_ids_filter(group_id_list):
-    _query_filter = tuple()
+    _query_filter = []
     if len(group_id_list) > 0:
-        group_filters = (HostGroupAssoc.group_id.in_(group_id_list),)
+        group_filters = [HostGroupAssoc.group_id.in_(group_id_list)]
         if None in group_id_list:
-            group_filters += (HostGroupAssoc.group_id.is_(None),)
+            group_filters += [HostGroupAssoc.group_id.is_(None)]
 
-        _query_filter += (or_(*group_filters),)
+        _query_filter += [or_(*group_filters)]
 
     return _query_filter
 
 
 def _staleness_filter(staleness):
-    _query_filter = tuple()
+    _query_filter = []
     # TODO
     return _query_filter
 
 
 def _registered_with_filter(registered_with):
-    _query_filter = tuple()
+    _query_filter = []
     # TODO
     return _query_filter
 
 
-def _system_profile_factor(sp_filter):
-    _query_filter = tuple()
+def _system_profile_filter(sp_filter):
+    _query_filter = []
     # TODO
     return _query_filter
 
@@ -109,8 +109,8 @@ def _hostname_or_id_filter(hostname_or_id):
     return (or_(*filter_list),)
 
 
-def _modified_on_filter(updated_start: str, updated_end: str) -> Tuple:
-    modified_on_filter = tuple()
+def _modified_on_filter(updated_start: str, updated_end: str) -> List:
+    modified_on_filter = []
     updated_start_date = parser.isoparse(updated_start) if updated_start else None
     updated_end_date = parser.isoparse(updated_end) if updated_end else None
 
@@ -118,19 +118,19 @@ def _modified_on_filter(updated_start: str, updated_end: str) -> Tuple:
         raise ValueError("updated_start cannot be after updated_end.")
 
     if updated_start_date and updated_start_date.year > 1970:
-        modified_on_filter += (Host.modified_on >= updated_start_date,)
+        modified_on_filter += [Host.modified_on >= updated_start_date]
     if updated_end_date and updated_end_date.year > 1970:
-        modified_on_filter += (Host.modified_on <= updated_end_date,)
+        modified_on_filter += [Host.modified_on <= updated_end_date]
 
-    return (and_(*modified_on_filter),)
+    return [and_(*modified_on_filter)]
 
 
 def _host_id_list_filter(host_id_list):
-    return (Host.id.in_(host_id_list),)
+    return [Host.id.in_(host_id_list)]
 
 
 def _rbac_filter(rbac_filter):
-    _query_filter = tuple()
+    _query_filter = []
     if rbac_filter and "groups" in rbac_filter:
         _query_filter = _group_ids_filter(rbac_filter["groups"])
 
@@ -154,7 +154,7 @@ def query_filters(
     filter=None,
     rbac_filter=None,
 ):
-    filters = tuple()
+    filters = []
     if fqdn:
         filters += _canonical_fact_filter("fqdn", fqdn)
     elif display_name:
@@ -181,7 +181,7 @@ def query_filters(
     if registered_with:
         filters += _registered_with_filter(registered_with)
     if filter:
-        filters += _system_profile_factor(filter)
+        filters += _system_profile_filter(filter)
     if rbac_filter:
         filters += _rbac_filter(rbac_filter)
 
