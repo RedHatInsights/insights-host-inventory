1fb99b2c4faaf0e749c75eae4ca010737bfa52a1
Chris Hambridge <chambrid@redhat.com>
2024-02-29 09:51:18 -0500

    feat(api): Allow in development APIs to be processed by Connexion but hidden from viewable spec
    * Use middleware to capture the after request call and alter the json

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import json
 import os
 
 from app import create_app
@@ -6,6 +7,36 @@ from app.environment import RuntimeEnvironment
 
 application = create_app(RuntimeEnvironment.SERVER)
 app = application.app
+scrubbed_apis = ["assignment-rules"]
+
+
+@app.after_request
+def after_request(response):
+    if response.status_code != 200:
+        return
+    response_text = response.get_data(as_text=True)
+    if "openapi" not in response_text:
+        return
+
+    try:
+        response_json = json.loads(response_text)
+    except ValueError:
+        return response
+
+    paths_to_delete = []
+    for path in response_json.get("paths", {}).keys():
+        for api in scrubbed_apis:
+            if api in path:
+                paths_to_delete.append(path)
+    for path in paths_to_delete:
+        del response_json["paths"][path]
+    try:
+        response_text = json.dumps(response_json)
+    except ValueError:
+        return response
+
+    response.set_data(response_text)
+    return response
 
 
 if __name__ == "__main__":
