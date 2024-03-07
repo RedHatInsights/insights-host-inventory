60601e53599722b2f8f25d0ceaacd5344a711e3b
Chris Hambridge <chambrid@redhat.com>
2024-03-07 07:23:23 -0500

    feat(RHINENG-8613): Fix blueprint serving customer facing openapi spec
    * Altered the base_url of the connexion openapi spec to avoid conflicts
    * Corrected file loading

--- api/spec.py
+++ api/spec.py
@@ -1,8 +1,11 @@
 import json
+import os
 
 from flask import Blueprint
 from flask import jsonify
 
+CURRENT_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+SPECIFICATION_FILE = os.path.join(CURRENT_DIR, "swagger", "openapi.json")
 spec_blueprint = Blueprint("openapispec", __name__)
 
 
@@ -10,7 +13,8 @@ spec_blueprint = Blueprint("openapispec", __name__)
 def openapi():
     openapi_spec = {}
     try:
-        openapi_spec = json.loads("../swagger/openapi.json")
+        with open(SPECIFICATION_FILE) as spec_file:
+            openapi_spec = json.load(spec_file)
     except ValueError:
         pass
 
