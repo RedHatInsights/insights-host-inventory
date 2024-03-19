1bbe8add301975919567eef811c5fb5e10679203
thearifismail <arifismail@yahoo.com>
2024-03-19 14:27:34 -0400

    Update unit_test.sh to use podman for remaing docker commands

--- unit_test.sh
+++ unit_test.sh
@@ -8,21 +8,15 @@ echo '===      Running Pre-commit     ===='
 echo '===================================='
 # copy workspace directory and chown it to match podman user namespace
 podman unshare rm -fr ./workspace_copy
-echo 'Running rsync ...'
 rsync -Rr . ./workspace_copy
-echo 'Running unshare ...'
 podman unshare chown -R 1001:1001 workspace_copy
 set +e
 # run pre-commit with the copied workspace mounted as a volume
-echo 'Running pre-commit...'
 podman run -u 1001:1001 -t -v ./workspace_copy:/workspace:Z --workdir /workspace --env HOME=/workspace $IMAGE:$IMAGE_TAG pre-commit run --all-files
-echo 'Done running pre-commit run!!!'
 TEST_RESULT=$?
 set -e
 # remove copy of the workspace
-echo 'Running podman unshare...'
 podman unshare rm -rf workspace_copy
-echo 'Running podman Done!!!'
 if [ $TEST_RESULT -ne 0 ]; then
 	echo '====================================='
 	echo '====  ✖ ERROR: PRECOMMIT FAILED  ===='
@@ -35,17 +29,17 @@ DB_CONTAINER_NAME="inventory-db-${IMAGE_TAG}"
 NETWORK="inventory-test-${IMAGE_TAG}"
 POSTGRES_IMAGE="quay.io/cloudservices/postgresql-rds:cyndi-13-1"
 
-function teardown_docker {
-	docker rm -f $DB_CONTAINER_ID || true
-	docker rm -f $TEST_CONTAINER_ID || true
-	docker network rm $NETWORK || true
+function teardown_podman {
+	podman rm -f $DB_CONTAINER_ID || true
+	podman rm -f $TEST_CONTAINER_ID || true
+	podman network rm $NETWORK || true
 }
 
-trap "teardown_docker" EXIT SIGINT SIGTERM
+trap "teardown_podman" EXIT SIGINT SIGTERM
 
-docker network create --driver bridge $NETWORK
+podman network create --driver bridge $NETWORK
 
-DB_CONTAINER_ID=$(docker run -d \
+DB_CONTAINER_ID=$(podman run -d \
 	--name "${DB_CONTAINER_NAME}" \
 	--network "${NETWORK}" \
 	-e POSTGRESQL_USER="inventory-test" \
@@ -58,10 +52,10 @@ if [[ "$DB_CONTAINER_ID" == "0" ]]; then
 	exit 1
 fi
 
-DB_IP_ADDR=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $DB_CONTAINER_ID)
+DB_IP_ADDR=$(podman inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $DB_CONTAINER_ID)
 
 # Do tests
-TEST_CONTAINER_ID=$(docker run -d \
+TEST_CONTAINER_ID=$(podman run -d \
 	--network ${NETWORK} \
 	-e INVENTORY_DB_NAME="inventory-test" \
 	-e INVENTORY_DB_HOST="${DB_IP_ADDR}" \
@@ -84,7 +78,7 @@ echo '===================================='
 echo '=== Installing Pip Dependencies ===='
 echo '===================================='
 set +e
-docker exec $TEST_CONTAINER_ID /bin/bash -c 'pipenv install --system --dev'
+podman exec $TEST_CONTAINER_ID /bin/bash -c 'pipenv install --system --dev'
 TEST_RESULT=$?
 set -e
 if [ $TEST_RESULT -ne 0 ]; then
@@ -99,12 +93,12 @@ echo '===================================='
 echo '====        Running Tests       ===='
 echo '===================================='
 set +e
-docker exec $TEST_CONTAINER_ID /bin/bash -c 'FLASK_APP=manage.py flask db upgrade && pytest --cov=. --junitxml=junit-unittest.xml --cov-report html -sv'
+podman exec $TEST_CONTAINER_ID /bin/bash -c 'FLASK_APP=manage.py flask db upgrade && pytest --cov=. --junitxml=junit-unittest.xml --cov-report html -sv'
 TEST_RESULT=$?
 set -e
 
 # Copy junit report
-docker cp $TEST_CONTAINER_ID:junit-unittest.xml $WORKSPACE/artifacts
+podman cp $TEST_CONTAINER_ID:junit-unittest.xml $WORKSPACE/artifacts
 
 if [ $TEST_RESULT -ne 0 ]; then
 	echo '====================================='
@@ -117,4 +111,4 @@ echo '====================================='
 echo '====   ✔ SUCCESS: PASSED TESTS   ===='
 echo '====================================='
 
-teardown_docker
+teardown_podman
