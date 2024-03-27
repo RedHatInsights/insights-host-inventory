5c593ce8914141e869576c10cf2b1b08b725fe37
Fabricia Diniz <agomesdi@redhat.com>
2024-03-27 13:17:01 +0000

    reintroduce schemas

--- app/queue/notifications.py
+++ app/queue/notifications.py
@@ -33,49 +33,62 @@ class BaseNotificationEvent(MarshmallowSchema):
     events = fields.List(fields.Nested(BaseMetadataSchema()))
 
 
-def base_notification_event(notification_type, message_id, host, context, payload):
-    event = {
-        "id": message_id,
-        "version": "v1.0.0",  # default value, no need to update unless we want to use newer features
-        "bundle": "rhel",
-        "application": "inventory",
-        "event_type": notification_type.name.replace("_", "-"),
-        "timestamp": datetime.now(timezone.utc),
-        "account_id": host.get("account_id") or "",
-        "org_id": host.get("org_id"),
-        "context": context,
-        "events": [
-            {
-                "metadata": {},
-                "payload": payload,
-            }
-        ],
-    }
-    return (BaseNotificationEvent, event)
+class HostValidationErrorContextSchema(MarshmallowSchema):
+    event_name = fields.Str(required=True, validate=marshmallow_validate.Length(max=255))
+    display_name = fields.Str(required=True)
+    inventory_id = fields.Str()
+
+
+class HostValidationErrorSchema(MarshmallowSchema):
+    code = fields.Str(required=True)
+    message = fields.Str(required=True, validate=marshmallow_validate.Length(max=1024))
+    stack_trace = fields.Str()
+    severity = fields.Str(required=True, validate=marshmallow_validate.OneOf(EventSeverity))
+
+
+class HostValidationErrorPayloadSchema(MarshmallowSchema):
+    request_id = fields.Str(required=True)
+    host_id = fields.UUID(required=True)
+    display_name = fields.Str()
+    canonical_facts = fields.Dict()
+    error = fields.Nested(HostValidationErrorSchema())
+
+
+class HostValidationErrorMetadataSchema(BaseMetadataSchema):
+    payload = fields.Nested(HostValidationErrorPayloadSchema())
+
+
+class HostValidationErrorNotificationEvent(BaseNotificationEvent):
+    context = fields.Nested(HostValidationErrorContextSchema())
+    events = fields.List(fields.Nested(HostValidationErrorMetadataSchema()))
 
 
 def host_validation_error_event(notification_type, message_id, host, detail, stack_trace=None):
-    context = {
-        "event_name": "Host Validation Error",
-        "display_name": host.get("display_name"),
-        "inventory_id": host.get("id"),
-    }
-
-    payload = {
-        "request_id": threadctx.request_id,
-        "display_name": host.get("display_name"),
-        "canonical_facts": host.get("canonical_facts"),
-        "error": {
-            "code": "VE001",
-            "message": detail,
-            "stack_trace": stack_trace,
-            "severity": "error",
+    base_notification_obj = build_base_notification_obj(notification_type, message_id, host)
+    event = {
+        "context": {
+            "event_name": "Host Validation Error",
+            "display_name": host.get("display_name"),
+            "inventory_id": host.get("id"),
         },
+        "events": [
+            {
+                "metadata": {},
+                "payload": {
+                    "request_id": threadctx.request_id,
+                    "display_name": host.get("display_name"),
+                    "canonical_facts": host.get("canonical_facts"),
+                    "error": {
+                        "code": "VE001",
+                        "message": detail,
+                        "stack_trace": stack_trace,
+                        "severity": EventSeverity.error,
+                    },
+                },
+            },
+        ],
     }
-
-    validation_error_event = base_notification_event(notification_type, message_id, host, context, payload)
-
-    return validation_error_event
+    return (HostValidationErrorNotificationEvent, event.update(base_notification_obj))
 
 
 def notification_message_headers(event_type: NotificationType, rh_message_id: bytearray = None):
@@ -93,6 +106,20 @@ def build_notification_event(notification_type, message_id, host, detail, **kwar
         schema, event = build(notification_type, message_id, host, detail, **kwargs)
         result = schema().dumps(event)
         return result
+
+
+def build_base_notification_obj(notification_type, message_id, host):
+    base_obj = {
+        "id": message_id,
+        "version": "v1.0.0",  # default value, no need to update unless we want to use newer features
+        "bundle": "rhel",
+        "application": "inventory",
+        "event_type": notification_type.name.replace("_", "-"),
+        "timestamp": datetime.now(timezone.utc),
+        "account_id": host.get("account_id") or "",
+        "org_id": host.get("org_id"),
+    }
+    return base_obj
 
 
 NOTIFICATION_TYPE_MAP = {
