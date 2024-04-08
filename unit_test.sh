8eaf43f21ea8f46542fed59f16d603fa46466834
Victor M. <vmugicag@redhat.com>
2024-04-08 11:30:26 +0200

    fix(ci): formatting

--- unit_test.sh
+++ unit_test.sh
@@ -19,10 +19,10 @@ set -e
 # remove copy of the workspace
 podman unshare rm -rf workspace_copy
 if [[ "$TEST_RESULT" -ne 0 ]]; then
-	echo '====================================='
-	echo '====  ✖ ERROR: PRECOMMIT FAILED  ===='
-	echo '====================================='
-	exit 1
+    echo '====================================='
+    echo '====  ✖ ERROR: PRECOMMIT FAILED  ===='
+    echo '====================================='
+    exit 1
 fi
 
 # run unit tests in containers
@@ -31,9 +31,9 @@ NETWORK="inventory-test-${IMAGE_TAG}"
 POSTGRES_IMAGE="quay.io/cloudservices/postgresql-rds:cyndi-13"
 
 function teardown_podman {
-	podman rm -f "$DB_CONTAINER_ID" || true
-	podman rm -f "$TEST_CONTAINER_ID" || true
-	podman network rm "$NETWORK" || true
+    podman rm -f "$DB_CONTAINER_ID" || true
+    podman rm -f "$TEST_CONTAINER_ID" || true
+    podman network rm "$NETWORK" || true
 }
 
 trap "teardown_podman" EXIT SIGINT SIGTERM
@@ -41,34 +41,34 @@ trap "teardown_podman" EXIT SIGINT SIGTERM
 podman network create --driver bridge "$NETWORK"
 
 DB_CONTAINER_ID=$(podman run -d \
-	--name "$DB_CONTAINER_NAME" \
-	--network "$NETWORK" \
-	-e POSTGRESQL_USER="inventory-test" \
-	-e POSTGRESQL_PASSWORD="inventory-test" \
-	-e POSTGRESQL_DATABASE="inventory-test" \
-	"$POSTGRES_IMAGE" || echo "0")
+    --name "$DB_CONTAINER_NAME" \
+    --network "$NETWORK" \
+    -e POSTGRESQL_USER="inventory-test" \
+    -e POSTGRESQL_PASSWORD="inventory-test" \
+    -e POSTGRESQL_DATABASE="inventory-test" \
+    "$POSTGRES_IMAGE" || echo "0")
 
 if [[ "$DB_CONTAINER_ID" == "0" ]]; then
-	echo "Failed to start DB container"
-	exit 1
+    echo "Failed to start DB container"
+    exit 1
 fi
 
 DB_IP_ADDR=$(podman inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$DB_CONTAINER_ID")
 
 # Do tests
 TEST_CONTAINER_ID=$(podman run -d \
-	--network "$NETWORK" \
-	-e INVENTORY_DB_NAME="inventory-test" \
-	-e INVENTORY_DB_HOST="$DB_IP_ADDR" \
-	-e INVENTORY_DB_PORT="5432" \
-	-e INVENTORY_DB_USER="inventory-test" \
-	-e INVENTORY_DB_PASS="inventory-test" \
-	"${IMAGE}:${IMAGE_TAG}" \
-	sleep infinity || echo "0")
+    --network "$NETWORK" \
+    -e INVENTORY_DB_NAME="inventory-test" \
+    -e INVENTORY_DB_HOST="$DB_IP_ADDR" \
+    -e INVENTORY_DB_PORT="5432" \
+    -e INVENTORY_DB_USER="inventory-test" \
+    -e INVENTORY_DB_PASS="inventory-test" \
+    "${IMAGE}:${IMAGE_TAG}" \
+    sleep infinity || echo "0")
 
 if [[ "$TEST_CONTAINER_ID" == "0" ]]; then
-	echo "Failed to start test container"
-	exit 1
+    echo "Failed to start test container"
+    exit 1
 fi
 
 ARTIFACTS_DIR="$WORKSPACE/artifacts"
@@ -83,10 +83,10 @@ podman exec "$TEST_CONTAINER_ID" pipenv install --system --dev
 TEST_RESULT=$?
 set -e
 if [[ "$TEST_RESULT" -ne 0 ]]; then
-	echo '====================================='
-	echo '==== ✖ ERROR: PIP INSTALL FAILED ===='
-	echo '====================================='
-	exit 1
+    echo '====================================='
+    echo '==== ✖ ERROR: PIP INSTALL FAILED ===='
+    echo '====================================='
+    exit 1
 fi
 
 # pytest
@@ -102,10 +102,10 @@ set -e
 podman cp "${TEST_CONTAINER_ID}:junit-unittest.xml" "${WORKSPACE}/artifacts"
 
 if [[ "$TEST_RESULT" -ne 0 ]]; then
-	echo '====================================='
-	echo '====    ✖ ERROR: TEST FAILED     ===='
-	echo '====================================='
-	exit 1
+    echo '====================================='
+    echo '====    ✖ ERROR: TEST FAILED     ===='
+    echo '====================================='
+    exit 1
 fi
 
 echo '====================================='
