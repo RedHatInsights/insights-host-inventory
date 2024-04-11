018bc2a38863db80cb434a25af16f63441ebe983
Fabrícia Diniz <104025448+FabriciaDinizRH@users.noreply.github.com>
2024-04-11 13:48:44 +0100

    refactor(RHINENG-8973): creates a general notification function (#1665)
    * feat(RHINENG-8973): creates a general notification function
    
    * reintroduce schemas
    
    * adds required to inventory_id
    
    * make variable names more consistent
    
    * fix one more naming inconsistency
    
    * clean the code up
    
    * add named parameter
    
    * fixes dict error
    
    * bring back account_id
    
    ---------
    
    Co-authored-by: Asa Price <aprice@redhat.com>

--- app/queue/notifications.py
+++ app/queue/notifications.py
@@ -8,13 +8,38 @@ from marshmallow import validate as marshmallow_validate
 
 from app.logging import threadctx
 from app.queue.events import hostname
-from app.queue.metrics import notification_event_serialization_time
+from app.queue.metrics import notification_serialization_time
 
 NotificationType = Enum("NotificationType", ("validation_error"))
 EventSeverity = Enum("EventSeverity", ("warning", "error", "critical"))
 
 
 # Schemas
+class EventListSchema(MarshmallowSchema):
+    metadata = fields.Dict(
+        validate=marshmallow_validate.Equal({})
+    )  # future-proofing as per notification documentation
+    payload = fields.Dict()
+
+
+class NotificationSchema(MarshmallowSchema):
+    id = fields.UUID(required=True)  # message_id, also sent in the reader as rh_message_id
+    account_id = fields.Str(validate=marshmallow_validate.Length(min=0, max=36))  # will be removed in future PR
+    org_id = fields.Str(required=True, validate=marshmallow_validate.Length(min=0, max=36))
+    application = fields.Str(required=True, validate=marshmallow_validate.Equal("inventory"))
+    bundle = fields.Str(required=True, validate=marshmallow_validate.Equal("rhel"))
+    context = fields.Dict()
+    events = fields.List(fields.Nested(EventListSchema()))
+    event_type = fields.Str(required=True, validate=marshmallow_validate.OneOf(NotificationType))
+    timestamp = fields.DateTime(required=True, format="iso8601")
+
+
+class HostValidationErrorContextSchema(MarshmallowSchema):
+    event_name = fields.Str(required=True, validate=marshmallow_validate.Length(max=255))
+    display_name = fields.Str(required=True)
+    inventory_id = fields.Str(required=True)
+
+
 class HostValidationErrorSchema(MarshmallowSchema):
     code = fields.Str(required=True)
     message = fields.Str(required=True, validate=marshmallow_validate.Length(max=1024))
@@ -22,7 +47,7 @@ class HostValidationErrorSchema(MarshmallowSchema):
     severity = fields.Str(required=True, validate=marshmallow_validate.OneOf(EventSeverity))
 
 
-class PayloadSchema(MarshmallowSchema):
+class HostValidationErrorPayloadSchema(MarshmallowSchema):
     request_id = fields.Str(required=True)
     host_id = fields.UUID(required=True)
     display_name = fields.Str()
@@ -30,43 +55,18 @@ class PayloadSchema(MarshmallowSchema):
     error = fields.Nested(HostValidationErrorSchema())
 
 
-class HostValidationErrorMetadataSchema(MarshmallowSchema):
-    metadata = fields.Dict(validate=marshmallow_validate.Equal({}))
-    payload = fields.Nested(PayloadSchema())
+class HostValidationErrorEventListSchema(EventListSchema):
+    payload = fields.Nested(HostValidationErrorPayloadSchema())
 
 
-class HostValidationErrorNotificationEvent(MarshmallowSchema):
-    id = fields.UUID(required=True)
-    version = fields.Str(required=True, validate=marshmallow_validate.Length(max=10))
-    bundle = fields.Str(required=True, validate=marshmallow_validate.Equal("rhel"))
-    application = fields.Str(required=True, validate=marshmallow_validate.Equal("inventory"))
-    event_type = fields.Str(required=True, validate=marshmallow_validate.Length(max=256))
-    timestamp = fields.DateTime(required=True, format="iso8601")
-    account_id = fields.Str(validate=marshmallow_validate.Length(min=0, max=36))
-    org_id = fields.Str(required=True, validate=marshmallow_validate.Length(min=0, max=36))
-    context = fields.Dict()
-    events = fields.List(fields.Nested(HostValidationErrorMetadataSchema()))
+class HostValidationErrorNotificationSchema(NotificationSchema):
+    context = fields.Nested(HostValidationErrorContextSchema())
+    events = fields.List(fields.Nested(HostValidationErrorEventListSchema()))
 
 
-def notification_message_headers(event_type: NotificationType, rh_message_id: bytearray = None):
-    return {
-        "event_type": event_type.name,
-        "request_id": threadctx.request_id,
-        "producer": hostname(),
-        "rh-message-id": rh_message_id,
-    }
-
-
-def host_validation_error_event(notification_type, message_id, host, detail, stack_trace=None):
-    validation_error_event = {
-        "id": message_id,
-        "version": "v1.0.0",
-        "bundle": "rhel",
-        "application": "inventory",
-        "event_type": notification_type.name.replace("_", "-"),
-        "timestamp": datetime.now(timezone.utc),
-        "account_id": host.get("account_id") or "",
-        "org_id": host.get("org_id"),
+def host_validation_error_notification(notification_type, message_id, host, detail, stack_trace=None):
+    base_notification_obj = build_base_notification_obj(notification_type, message_id, host)
+    notification = {
         "context": {
             "event_name": "Host Validation Error",
             "display_name": host.get("display_name"),
@@ -74,7 +74,6 @@ def host_validation_error_event(notification_type, message_id, host, detail, sta
         },
         "events": [
             {
-                "metadata": {},
                 "payload": {
                     "request_id": threadctx.request_id,
                     "display_name": host.get("display_name"),
@@ -83,24 +82,45 @@ def host_validation_error_event(notification_type, message_id, host, detail, sta
                         "code": "VE001",
                         "message": detail,
                         "stack_trace": stack_trace,
-                        "severity": "error",
+                        "severity": EventSeverity.error.name,
                     },
                 },
-            }
+            },
         ],
     }
-
-    return (HostValidationErrorNotificationEvent, validation_error_event)
-
-
-NOTIFICATION_TYPE_MAP = {
-    NotificationType.validation_error: host_validation_error_event,
-}
+    notification.update(base_notification_obj)
+    return (HostValidationErrorNotificationSchema, notification)
 
 
-def build_notification_event(notification_type, message_id, host, detail, **kwargs):
-    with notification_event_serialization_time.labels(notification_type.name).time():
+def notification_headers(event_type: NotificationType):
+    return {
+        "event_type": event_type.name,
+        "request_id": threadctx.request_id,
+        "producer": hostname(),
+    }
+
+
+def build_notification(notification_type, message_id, host, detail, **kwargs):
+    with notification_serialization_time.labels(notification_type.name).time():
         build = NOTIFICATION_TYPE_MAP[notification_type]
         schema, event = build(notification_type, message_id, host, detail, **kwargs)
         result = schema().dumps(event)
         return result
+
+
+def build_base_notification_obj(notification_type, message_id, host):
+    base_obj = {
+        "id": message_id,
+        "account_id": host.get("account_id") or "",
+        "org_id": host.get("org_id"),
+        "application": "inventory",
+        "bundle": "rhel",
+        "event_type": notification_type.name.replace("_", "-"),
+        "timestamp": datetime.now(timezone.utc),
+    }
+    return base_obj
+
+
+NOTIFICATION_TYPE_MAP = {
+    NotificationType.validation_error: host_validation_error_notification,
+}
