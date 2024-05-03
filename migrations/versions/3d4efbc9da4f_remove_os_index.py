b7437761b4211a9598374ffda97121f8848ad9dd
Asa Price <aprice@redhat.com>
2024-05-03 09:04:23 -0400

    Remove OS index

---
+++ migrations/versions/3d4efbc9da4f_remove_os_index.py
@@ -0,0 +1,32 @@
+"""Remove OS index
+
+Revision ID: 3d4efbc9da4f
+Revises: 90879eb18c66
+Create Date: 2024-05-03 09:01:46.316467
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "3d4efbc9da4f"
+down_revision = "90879eb18c66"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    with op.get_context().autocommit_block():
+        op.drop_index("idx_operating_system", table_name="hosts", postgresql_concurrently=True, if_exists=True)
+
+
+def downgrade():
+    with op.get_context().autocommit_block():
+        op.create_index(
+            "idx_operating_system",
+            "hosts",
+            [sa.text("(COALESCE(system_profile_facts -> 'operating_system' ->> 'name', '')) gin_trgm_ops")],
+            postgresql_using="gin",
+            postgresql_concurrently=True,
+            if_not_exists=True,
+        )
