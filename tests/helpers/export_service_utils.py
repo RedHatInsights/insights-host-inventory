af02aa8f071a831c3ce9b60c5b3c53f1d815bf84
Joao Paulo Ramos <jramos@redhat.com>
2024-05-28 17:08:04 -0300

    feat(RHINENG-9892): Add tests

---
+++ tests/helpers/export_service_utils.py
@@ -0,0 +1,73 @@
+import json
+
+
+def create_export_message_mock():
+    return json.dumps(
+        {
+            "id": "b4228e37-8ae8-4c67-81d5-d03f39bbe309",
+            "$schema": "someSchema",
+            "source": "urn:redhat:source:console:app:export-service",
+            "subject": "urn:redhat:subject:export-service:request:9becbc61-49a4-49be-beb1-1f0a7cbc6e36",
+            "specversion": "1.0",
+            "type": "com.redhat.console.export-service.request",
+            "time": "2024-05-28T14:59:36Z",
+            "redhatorgid": "5894300",
+            "dataschema": "https://console.redhat.com/api/schemas/apps/export-service/v1/resource-request.json",
+            "data": {
+                "resource_request": {
+                    "application": "host-inventory",
+                    "export_request_uuid": "9becbc61-49a4-49be-beb1-1f0a7cbc6e36",
+                    "filters": {
+                        "endDate": "2024-03-01T00:00:00Z",
+                        "productId": "RHEL",
+                        "startDate": "2024-01-01T00:00:00Z",
+                    },
+                    "format": "json",
+                    "resource": "exampleResource",
+                    "uuid": "2844f3a1-e047-45b1-b0ce-fb9812ad6a6f",
+                    "x-rh-identity": (
+                        "eyJpZGVudGl0eSI6IHsib3JnX2lkIjogIjU4OTQzMD"
+                        "AiLCAidHlwZSI6ICJVc2VyIiwgImF1dGhfdHlwZSI6"
+                        "ICJiYXNpYy1hdXRoIiwgInVzZXIiOiB7ImVtYWlsIj"
+                        "ogImpyYW1vc0ByZWRoYXQuY29tIiwgImZpcnN0X25hbWUiOiAidGVzdCJ9fX0="
+                    ),
+                }
+            },
+        }
+    )
+
+
+def create_export_message_missing_field_mock(field_to_remove):
+    message = {
+        "id": "b4228e37-8ae8-4c67-81d5-d03f39bbe309",
+        "$schema": "someSchema",
+        "source": "urn:redhat:source:console:app:export-service",
+        "subject": "urn:redhat:subject:export-service:request:9becbc61-49a4-49be-beb1-1f0a7cbc6e36",
+        "specversion": "1.0",
+        "type": "com.redhat.console.export-service.request",
+        "time": "2024-05-28T14:59:36Z",
+        "redhatorgid": "5894300",
+        "dataschema": "https://console.redhat.com/api/schemas/apps/export-service/v1/resource-request.json",
+        "data": {
+            "resource_request": {
+                "application": "host-inventory",
+                "export_request_uuid": "9becbc61-49a4-49be-beb1-1f0a7cbc6e36",
+                "filters": {
+                    "endDate": "2022-03-01T00:00:00Z",
+                    "productId": "RHEL",
+                    "startDate": "2022-01-01T00:00:00Z",
+                },
+                "format": "json",
+                "resource": "exampleResource",
+                "uuid": "2844f3a1-e047-45b1-b0ce-fb9812ad6a6f",
+                "x-rh-identity": (
+                    "eyJpZGVudGl0eSI6IHsib3JnX2lkIjogIjU4OTQzMD"
+                    "AiLCAidHlwZSI6ICJVc2VyIiwgImF1dGhfdHlwZSI6"
+                    "ICJiYXNpYy1hdXRoIiwgInVzZXIiOiB7ImVtYWlsIj"
+                    "ogImpyYW1vc0ByZWRoYXQuY29tIiwgImZpcnN0X25hbWUiOiAidGVzdCJ9fX0="
+                ),
+            }
+        },
+    }
+    message.pop(field_to_remove)
+    return json.dumps(message)
---
+++ tests/test_export_service.py
@@ -0,0 +1,43 @@
+import json
+
+import pytest
+from marshmallow.exceptions import ValidationError
+
+from app.queue.queue import handle_export_message
+from tests.helpers.export_service_utils import create_export_message_missing_field_mock
+from tests.helpers.export_service_utils import create_export_message_mock
+
+
+def test_handle_create_export_request():
+    export_message = create_export_message_mock()
+
+    resp = handle_export_message(message=export_message, event_producer=None, notification_event_producer=None)
+
+    assert resp is True
+
+
+@pytest.mark.parametrize(
+    "field_to_remove", ["id", "source", "subject", "specversion", "type", "time", "redhatorgid", "dataschema", "data"]
+)
+def test_handle_create_export_missing_field(field_to_remove):
+    with pytest.raises(ValidationError):
+        export_message = create_export_message_missing_field_mock(field_to_remove)
+        handle_export_message(message=export_message, event_producer=None, notification_event_producer=None)
+
+
+def test_handle_create_export_wrong_application():
+    with pytest.raises(ValidationError):
+        export_message = create_export_message_mock()
+        export_message = json.loads(export_message)
+        export_message["data"]["resource_request"]["application"] = "foo"
+        export_message = json.dumps(export_message)
+
+        handle_export_message(message=export_message, event_producer=None, notification_event_producer=None)
+
+
+def test_handle_create_export_empty_message():
+    with pytest.raises(ValidationError):
+        export_message = ""
+        export_message = json.dumps(export_message)
+
+        handle_export_message(message=export_message, event_producer=None, notification_event_producer=None)
