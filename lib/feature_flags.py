829251e580f28a9a03ffdc59f9f275df9e8402b8
Muhammad Arif <40067075+thearifismail@users.noreply.github.com>
2024-06-04 13:21:13 -0400

    Disable xjoin with minimum changes (#1770)
    * Disable xjoin with minimum changes
    
    * Update lib/feature_flags.py
    
    Co-authored-by: Joao Paulo Ramos <jramos@redhat.com>
    
    * Update lib/feature_flags.py
    
    Co-authored-by: Joao Paulo Ramos <jramos@redhat.com>
    
    ---------
    
    Co-authored-by: Joao Paulo Ramos <jramos@redhat.com>

--- lib/feature_flags.py
+++ lib/feature_flags.py
@@ -3,6 +3,7 @@ from typing import Tuple
 from flask_unleash import Unleash
 from UnleashClient.strategies import Strategy
 
+from app.common import inventory_config
 from app.logging import get_logger
 
 UNLEASH = Unleash()
@@ -47,8 +48,12 @@ def custom_fallback(feature_name: str, context: dict) -> bool:
 # Returns a tuple containing the flag's value and whether or not the fallback value was used.
 def get_flag_value_and_fallback(flag_name: str, context: dict = {}) -> Tuple[bool, bool]:
     # Get flag name and default to fallback value
-    flag_value = FLAG_FALLBACK_VALUES[flag_name]
-    using_fallback = True
+    if FLAG_FALLBACK_VALUES[flag_name] is False and inventory_config().bypass_xjoin is False:
+        flag_value = False
+        using_fallback = True
+    else:  # change the default False in FLAG_FALLBACK_VALUES
+        flag_value = FLAG_FALLBACK_VALUES[flag_name]
+        using_fallback = False
 
     # Attempt to get the feature flag via Unleash
     try:
