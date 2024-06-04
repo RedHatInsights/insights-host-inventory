41702454a96cbd7fe5c108cf0b74051fac915dba
František Viktor Stavěla <45922782+fstavela@users.noreply.github.com>
2024-06-04 16:24:26 +0200

    RHINENG-4648: Add PR check pipeline with RBAC integration tests (#1772)
    rh-pre-commit.version: 2.2.0
    rh-pre-commit.check-secrets: ENABLED

--- pr_check.sh
+++ pr_check.sh
@@ -1,42 +1,16 @@
 #!/bin/bash
 
-# --------------------------------------------
-# Pre-commit checks
-# --------------------------------------------
-export APP_NAME="host-inventory"  # name of app-sre "application" folder this component lives in
-export IMAGE="quay.io/cloudservices/insights-inventory"
-export LC_ALL=en_US.utf-8
-export LANG=en_US.utf-8
-export APP_ROOT=$(pwd)
-export WORKSPACE=${WORKSPACE:-$APP_ROOT}  # if running in jenkins, use the build's workspace
-export IMAGE_TAG=$(git rev-parse --short=7 HEAD)
-export GIT_COMMIT=$(git rev-parse HEAD)
-export QUAY_EXPIRE_TIME="40d"
-cat /etc/redhat-release
+# Prepare env
+source $(pwd)/pr_check_common.sh
 
-# --------------------------------------------
-# Options that must be configured by app owner
-# --------------------------------------------
-COMPONENT_NAME="host-inventory"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
-COMPONENTS_W_RESOURCES="host-inventory"  # this gives us resources defined in app-sre, otherwise we run low on memory
-
-IQE_PLUGINS="host_inventory"
-IQE_MARKER_EXPRESSION="smoke"
-IQE_FILTER_EXPRESSION=""
-IQE_CJI_TIMEOUT="30m"
-
-# ---------------------------
-# We'll take it from here ...
-# ---------------------------
-
-# Get bonfire helper scripts
-CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
-curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
+export IQE_MARKER_EXPRESSION="smoke"
+export IQE_FILTER_EXPRESSION=""
+export IQE_CJI_TIMEOUT="30m"
 
 # build the PR commit image
 source $CICD_ROOT/build.sh
 
-# Run the django unit tests
+# Run the unit tests
 source $APP_ROOT/unit_test.sh
 
 # Run IQE tests
