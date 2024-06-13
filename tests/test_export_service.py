7ce9a0fcbda4a1f6900dfa4e7899e15da1bc2052
Joao Paulo Ramos <jramos@redhat.com>
2024-06-13 12:57:47 -0300

    feat(RHINENG-8714): Adapt tests

--- tests/test_export_service.py
+++ tests/test_export_service.py
@@ -11,7 +11,7 @@ from tests.helpers.export_service_utils import create_export_message_mock
 def test_handle_create_export_request():
     export_message = create_export_message_mock()
 
-    resp = handle_export_message(message=export_message, event_producer=None, notification_event_producer=None)
+    resp = handle_export_message(message=export_message)
 
     assert resp is True
 
@@ -22,7 +22,7 @@ def test_handle_create_export_request():
 def test_handle_create_export_missing_field(field_to_remove):
     with pytest.raises(ValidationError):
         export_message = create_export_message_missing_field_mock(field_to_remove)
-        handle_export_message(message=export_message, event_producer=None, notification_event_producer=None)
+        handle_export_message(message=export_message)
 
 
 def test_handle_create_export_wrong_application():
@@ -32,7 +32,7 @@ def test_handle_create_export_wrong_application():
         export_message["data"]["resource_request"]["application"] = "foo"
         export_message = json.dumps(export_message)
 
-        handle_export_message(message=export_message, event_producer=None, notification_event_producer=None)
+        handle_export_message(message=export_message)
 
 
 def test_handle_create_export_empty_message():
@@ -40,4 +40,4 @@ def test_handle_create_export_empty_message():
         export_message = ""
         export_message = json.dumps(export_message)
 
-        handle_export_message(message=export_message, event_producer=None, notification_event_producer=None)
+        handle_export_message(message=export_message)
