386c47ce071131a21cca90a8f4fa9de2504636a0
Gaël Chamoulaud <gchamoul@redhat.com>
2024-06-17 17:11:22 +0200

    feat(RHINENG-9060) Add DB Call Implementation for Export Service
    This commit addresses the following JIRA Cards:
    - RHINENG-9060
      * RHINENG-9824
      * RHINENG-9825
    
    Signed-off-by: Gaël Chamoulaud <gchamoul@redhat.com>
    Co-Authored-by: João Paulo Ramos <jramos@redhat.com>

--- tests/test_export_service.py
+++ tests/test_export_service.py
@@ -1,19 +1,27 @@
 import json
+from unittest import mock
 
 import pytest
 from marshmallow.exceptions import ValidationError
 
 from app.queue.queue import handle_export_message
-from tests.helpers.export_service_utils import create_export_message_missing_field_mock
-from tests.helpers.export_service_utils import create_export_message_mock
+from tests.helpers import export_service_utils as es_utils
 
 
-def test_handle_create_export_request():
-    export_message = create_export_message_mock()
-
+@mock.patch("app.queue.export_service._handle_rbac_to_export", return_value=es_utils.EXPORT_DATA)
+@mock.patch("app.queue.export_service.create_export", return_value=True)
+def test_handle_create_export_request_with_data_to_export(mock_export, mock_rbac, inventory_config):
+    export_message = es_utils.create_export_message_mock()
     resp = handle_export_message(message=export_message)
-
     assert resp is True
+
+
+@mock.patch("app.queue.export_service._handle_rbac_to_export", return_value=[])
+@mock.patch("app.queue.export_service.create_export", return_value=False)
+def test_handle_create_export_request_with_no_data_to_export(mock_export, mock_rbac, inventory_config):
+    export_message = es_utils.create_export_message_mock()
+    resp = handle_export_message(message=export_message)
+    assert resp is False
 
 
 @pytest.mark.parametrize(
@@ -21,12 +29,12 @@ def test_handle_create_export_request():
 )
 def test_handle_create_export_missing_field(field_to_remove):
     with pytest.raises(ValidationError):
-        export_message = create_export_message_missing_field_mock(field_to_remove)
+        export_message = es_utils.create_export_message_missing_field_mock(field_to_remove)
         handle_export_message(message=export_message)
 
 
 def test_handle_create_export_wrong_application():
-    export_message = create_export_message_mock()
+    export_message = es_utils.create_export_message_mock()
     export_message = json.loads(export_message)
     export_message["data"]["resource_request"]["application"] = "foo"
     export_message = json.dumps(export_message)
