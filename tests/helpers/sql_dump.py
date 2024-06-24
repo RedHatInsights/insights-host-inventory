c3e8d21ea89275e30a7b96bfdc060505d1ce19d8
Richard Oliveri <roliveri@redhat.com>
2024-06-24 17:36:14 -0400

    Added SQL dump facility to aid in testing, debugging and reviewing.

---
+++ tests/helpers/sql_dump.py
@@ -0,0 +1,29 @@
+from sqlalchemy import event as sqlevent
+import json
+
+try:
+	from sqlparse import format as sql_formatter
+except ModuleNotFoundError:
+	def sql_formatter(sql_str, reindent=True, keyword_case='upper'):
+		return sql_str
+
+from app import db
+
+class SQLDump(object):
+	def __init__(self, dump_method=None, write_method=print):
+		if dump_method is None:
+			self.dump_method = self.dump_sql
+		self.write_method = write_method
+
+	def __enter__(self):
+		sqlevent.listen(db.engine, "before_execute", self.dump_method)
+
+	def __exit__(self, exc_type, exc_value, exc_traceback):
+		sqlevent.remove(db.engine, "before_execute", self.dump_method)
+
+	def dump_sql(self, conn, clauseelement, multiparams, params, execution_options):
+		self.write_method("**** QUERY:\n")
+		self.write_method(sql_formatter(str(clauseelement.compile()), reindent=True, keyword_case='upper'))
+		self.write_method("\n**** PARAMETERS:\n")
+		self.write_method(json.dumps(clauseelement.compile().params, sort_keys=True, indent=4, default=str))
+		self.write_method("\n****************\n")
