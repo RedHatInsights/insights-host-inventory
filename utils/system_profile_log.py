ad52926abdd9c583b86a28831d54f4d784ed185d
Asa Price <aprice@redhat.com>
2025-01-06 11:41:57 -0500

    Fix SP logger; tweak order of operations; parameterize test

--- utils/system_profile_log.py
+++ utils/system_profile_log.py
@@ -2,16 +2,16 @@ from app.common import inventory_config
 from app.models import Host
 
 
-def extract_sp_to_log(sp_data: dict) -> dict:
-    return {k: sp_data[k] for k in inventory_config().sp_fields_to_log if k in sp_data}
+def _extract_sp_to_log(sp_data: dict) -> dict:
+    if not sp_data:
+        return {}
+    else:
+        return {k: sp_data[k] for k in inventory_config().sp_fields_to_log if k in sp_data}
 
 
 def extract_host_model_sp_to_log(host: Host) -> dict:
-    return extract_sp_to_log(host.system_profile_facts)
+    return _extract_sp_to_log(host.system_profile_facts)
 
 
 def extract_host_dict_sp_to_log(host_data: dict) -> dict:
-    if "system_profile" in host_data:
-        return extract_sp_to_log(host_data["system_profile"])
-    else:
-        return {}
+    return _extract_sp_to_log(host_data.get("system_profile", {}))
