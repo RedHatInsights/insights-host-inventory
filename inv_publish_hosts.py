e7e243b812a9d82cdee8aa5ff95b80acafd7fba6
Muhammad Arif <40067075+thearifismail@users.noreply.github.com>
2025-01-31 10:20:45 -0500

    Add metrics for hosts syndication (#2229)
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED

--- inv_publish_hosts.py
+++ inv_publish_hosts.py
@@ -2,6 +2,8 @@
 import sys
 from functools import partial
 
+from prometheus_client import CollectorRegistry
+from prometheus_client import push_to_gateway
 from sqlalchemy import create_engine
 from sqlalchemy import text as sa_text
 from sqlalchemy.orm import sessionmaker
@@ -12,10 +14,20 @@ from app.environment import RuntimeEnvironment
 from app.logging import get_logger
 from lib.handlers import ShutdownHandler
 from lib.handlers import register_shutdown
+from lib.metrics import hosts_syndication_fail_count
+from lib.metrics import hosts_syndication_success_count
+from lib.metrics import hosts_syndication_time
 
 __all__ = ("main", "run")
 
 LOGGER_NAME = "hosts-syndicator"
+PROMETHEUS_JOB = "hosts-syndicator"
+COLLECTED_METRICS = (
+    hosts_syndication_fail_count,
+    hosts_syndication_success_count,
+    hosts_syndication_time,
+)
+
 RUNTIME_ENVIRONMENT = RuntimeEnvironment.JOB
 
 PUBLICATION_NAME = "hbi_hosts_pub"
@@ -34,6 +46,10 @@ def _init_config():
 def _init_db(config):
     engine = create_engine(config.db_uri)
     return sessionmaker(bind=engine)
+
+
+def _prometheus_job(namespace):
+    return f"{PROMETHEUS_JOB}-{namespace}" if namespace else PROMETHEUS_JOB
 
 
 def _excepthook(logger, type, value, traceback):
@@ -65,15 +81,24 @@ def run(logger, session, application):
             except Exception as e:
                 session.rollback()
                 logger.error(f'Error encountered when creating the publication "{PUBLICATION_NAME}" \n\t{e}')
+                hosts_syndication_fail_count.inc()
                 raise e
             else:
                 session.commit()
                 logger.info(f'Publication "{PUBLICATION_NAME}" created!!!')
+                hosts_syndication_success_count.inc()
 
 
 def main(logger):
     config = _init_config()
     application = create_app(RUNTIME_ENVIRONMENT)
+
+    registry = CollectorRegistry()
+    for metric in COLLECTED_METRICS:
+        registry.register(metric)
+    job = _prometheus_job(config.kubernetes_namespace)
+    prometheus_shutdown = partial(push_to_gateway, config.prometheus_pushgateway, job, registry)
+    register_shutdown(prometheus_shutdown, "Pushing metrics")
 
     Session = _init_db(config)
     session = Session()
