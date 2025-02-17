d310236911c2fcec26fa2729bb1e7a2d0a8deefc
Fabrícia Diniz <104025448+FabriciaDinizRH@users.noreply.github.com>
2025-02-17 15:56:02 +0000

    Apply suggestions from code review
    Co-authored-by: Chris Hambridge <chambrid@redhat.com>

--- app_migrations/64a037bb5344.sql
+++ app_migrations/64a037bb5344.sql
@@ -1,9 +1,9 @@
 BEGIN;
 
--- Running upgrade 4f0b36e6cf28 -> 64a037bb5344
+-- Running upgrade 286343296975 -> 64a037bb5344
 
 ALTER TABLE hbi.hosts ADD COLUMN tags_alt JSONB;
 
-UPDATE hbi.alembic_version SET version_num='64a037bb5344' WHERE hbi.alembic_version.version_num = '4f0b36e6cf28';
+UPDATE hbi.alembic_version SET version_num='64a037bb5344' WHERE hbi.alembic_version.version_num = '286343296975';
 
 COMMIT;
