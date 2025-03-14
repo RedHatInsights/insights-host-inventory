60dfe931904d73731b93268ccbdc3e499496fba0
Joao Paulo Ramos <jramos@redhat.com>
2025-03-14 18:59:51 -0300

    feat: Add last_check_in value to host table and Host class

---
+++ migrations/versions/837d2ee7bbd8_add_last_check_in_column_to_host_table.py
@@ -0,0 +1,30 @@
+"""Add last_check_in column to host table
+
+Revision ID: 837d2ee7bbd8
+Revises: 6f44b7ecd7be
+Create Date: 2025-03-12 11:39:53.550203
+
+"""
+
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "837d2ee7bbd8"
+down_revision = "6f44b7ecd7be"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    # This columns is going to be nullable until data migration is performed
+    # This is going to be kept up to date in the code
+    op.add_column("hosts", sa.Column("last_check_in", sa.DateTime(timezone=True), nullable=True), schema="hbi")
+
+    op.execute("""
+               UPDATE hbi.hosts h SET last_check_in  = sub.modified_on
+               FROM (SELECT id, modified_on FROM hbi.hosts) AS sub WHERE h.id = sub.id;""")
+
+
+def downgrade():
+    op.drop_column("hosts", "last_check_in", schema="hbi")
