344d589fcbbcffb7ad5ce3ce7ed1c3c6c907894f
Rodrigo Antunes <rantunes@redhat.com>
2025-04-28 13:20:14 -0300

    fix: sonarqube script

--- sonarqube.sh
+++ sonarqube.sh
@@ -1,47 +1,21 @@
 #!/bin/bash
 
-mkdir $PWD/sonarqube/
-mkdir $PWD/sonarqube/download/
-mkdir $PWD/sonarqube/extract/
-mkdir $PWD/sonarqube/certs/
-mkdir $PWD/sonarqube/store/
-
-curl -o $PWD/sonarqube/certs/RH-IT-Root-CA.crt --insecure $ROOT_CA_CERT_URL
-
-$JAVA_HOME/bin/keytool \
-  -keystore /$PWD/sonarqube/store/RH-IT-Root-CA.keystore \
-  -import \
-  -alias RH-IT-Root-CA \
-  -file /$PWD/sonarqube/certs/RH-IT-Root-CA.crt \
-  -storepass redhat \
-  -noprompt
-
-export SONAR_SCANNER_OPTS="-Djavax.net.ssl.trustStore=$PWD/sonarqube/store/RH-IT-Root-CA.keystore -Djavax.net.ssl.trustStorePassword=redhat"
-
-
 export SONAR_SCANNER_OS="linux"
 if [[ "$OSTYPE" == "darwin"* ]]; then
     export SONAR_SCANNER_OS="macosx"
 fi
 
-export SONAR_SCANNER_CLI_VERSION="4.7.0.2747"
-export SONAR_SCANNER_DOWNLOAD_NAME="sonar-scanner-cli-$SONAR_SCANNER_CLI_VERSION-$SONAR_SCANNER_OS"
-export SONAR_SCANNER_NAME="sonar-scanner-$SONAR_SCANNER_CLI_VERSION-$SONAR_SCANNER_OS"
-
-curl -o $PWD/sonarqube/download/$SONAR_SCANNER_DOWNLOAD_NAME.zip --insecure $SONARQUBE_CLI_URL
-
-unzip -d $PWD/sonarqube/extract/ $PWD/sonarqube/download/$SONAR_SCANNER_DOWNLOAD_NAME.zip
-
-export PATH="$PWD/sonarqube/extract/$SONAR_SCANNER_NAME/bin:$PATH"
-
 COMMIT_SHORT=$(git rev-parse --short=7 HEAD)
 
-sonar-scanner \
-  -Dsonar.projectKey=console.redhat.com:insights-host-inventory \
-  -Dsonar.sources=$PWD \
-  -Dsonar.host.url=$SONARQUBE_REPORT_URL \
-  -Dsonar.projectVersion=$COMMIT_SHORT \
-  -Dsonar.login=$SONARQUBE_TOKEN
+docker run \
+  -v ${PWD}:/usr/src \
+  -e SONAR_HOST_URL=https://sonarqube.corp.redhat.com/ \
+  -e SONAR_SCANNER_HOME=/opt/sonar-scanner \
+  -e SONAR_USER_HOME=/opt/sonar-scanner/.sonar \
+  -e SONAR_SCANNER_OPTS="-Xmx512m -Dsonar.projectKey=console.redhat.com:insights-host-inventory -Dsonar.projectVersion=${COMMIT_SHORT} -Dsonar.sources=/usr/src" \
+  -e SONAR_TOKEN=$SONARQUBE_TOKEN \
+  images.paas.redhat.com/alm/sonar-scanner-alpine \
+  sonar-scanner
 
 mkdir -p $WORKSPACE/artifacts
 cat << EOF > ${WORKSPACE}/artifacts/junit-dummy.xml
