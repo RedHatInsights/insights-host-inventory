5f79210f7740744da34d8a8a55fd244ad35bfdfc
Asa Price <aprice@redhat.com>
2025-05-07 15:53:13 -0400

    Separate ungrouped-host group script into 2

--- tests/test_ungrouped_group_scripts.py
+++ tests/test_ungrouped_group_scripts.py
@@ -3,14 +3,38 @@ from unittest import mock
 import pytest
 
 from app.models import db
-from create_ungrouped_host_groups import run as run_script
+from assign_ungrouped_hosts_to_groups import run as run_assignment_script
+from create_ungrouped_host_groups import run as run_creation_script
 from delete_ungrouped_host_groups import run as run_undo_script
 from tests.helpers.test_utils import SYSTEM_IDENTITY
 
 
-@pytest.mark.parametrize("existing_ungrouped", (True, False))
+def test_creation_happy_path(
+    flask_app,
+    db_create_host,
+    db_get_ungrouped_group,
+    db_get_groups_for_host,
+):
+    ungrouped_host_id = db_create_host().id
+
+    # There should not be an ungrouped group yet
+    assert db_get_ungrouped_group("test") is None
+
+    run_creation_script(
+        logger=mock.MagicMock(),
+        session=db.session,
+        application=flask_app,
+    )
+
+    # The ungrouped group should exist
+    assert db_get_ungrouped_group("test") is not None
+
+    # The host should still be ungrouped (unchanged)
+    assert db_get_groups_for_host(ungrouped_host_id) == []
+
+
 @pytest.mark.parametrize("num_ungrouped_hosts", (0, 1, 5, 6))
-def test_happy_path(
+def test_assignment_happy_path(
     flask_app,
     mocker,
     db_create_host,
@@ -18,14 +42,13 @@ def test_happy_path(
     db_get_hosts_for_group,
     db_get_groups_for_host,
     event_producer_mock,
-    existing_ungrouped,
     num_ungrouped_hosts,
 ):
     EXISTING_GROUP_NAME = "existing group"
 
     # Create some ungrouped hosts & 2 grouped hosts
     ungrouped_host_ids = [db_create_host().id for _ in range(num_ungrouped_hosts)]
-    grouped_group_id = db_create_group_with_hosts(EXISTING_GROUP_NAME, 2, ungrouped=existing_ungrouped).id
+    grouped_group_id = db_create_group_with_hosts(EXISTING_GROUP_NAME, 2, ungrouped=True).id
     grouped_host_ids = [host.id for host in db_get_hosts_for_group(grouped_group_id)]
     db.session.commit()
 
@@ -36,9 +59,9 @@ def test_happy_path(
         assert db_get_groups_for_host(host_id)[0].name == EXISTING_GROUP_NAME
 
     # Force a smaller batch size so the test doesn't take ages
-    mocker.patch("create_ungrouped_host_groups.BATCH_SIZE", 5)
+    mocker.patch("assign_ungrouped_hosts_to_groups.BATCH_SIZE", 5)
 
-    run_script(
+    run_assignment_script(
         logger=mock.MagicMock(),
         session=db.session,
         event_producer=event_producer_mock,
@@ -48,9 +71,8 @@ def test_happy_path(
     # All hosts that used to be ungrouped should now be in the "ungrouped" workspace
     # If there was already an existing "ungrouped" group, they should be in that group.
     # Otherwise, they should be in a new "ungrouped" group named "ungrouped".
-    ungrouped_group_name = EXISTING_GROUP_NAME if existing_ungrouped else "Ungrouped Hosts"
     for host_id in ungrouped_host_ids:
-        assert db_get_groups_for_host(host_id)[0].name == ungrouped_group_name
+        assert db_get_groups_for_host(host_id)[0].name == EXISTING_GROUP_NAME
         assert db_get_groups_for_host(host_id)[0].ungrouped is True
         assert db_get_groups_for_host(host_id)[0].account == SYSTEM_IDENTITY["account_number"]
 
