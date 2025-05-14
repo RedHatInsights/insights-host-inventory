4b60596e13b4dd39e1fb1fef63381fb3ebd4a962
thearifismail <arifismail@yahoo.com>
2025-05-14 14:28:03 -0400

    Get secrets by name
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED

--- docs/get_hbi_rbac_db_creds.sh
+++ docs/get_hbi_rbac_db_creds.sh
@@ -12,8 +12,7 @@ echo -e "Using namespace $PROJECT_NAME\n"
 
 # host inventory
 HBI_DATABASE_SECRET_NAME="host-inventory-db"
-HAS_INVENTORY_SECRET=$(kubectl get secrets -o json -n $PROJECT_NAME | jq ".items[] | select(.metadata.name==\"$HBI_DATABASE_SECRET_NAME\")")
-if [ -n "$HAS_INVENTORY_SECRET" ]; then
+if kubectl get secret "$HBI_DATABASE_SECRET_NAME" -n "$PROJECT_NAME" &> /dev/null; then
   INVENTORY_DB_USER=$(kubectl -n "$PROJECT_NAME" get secret/"$HBI_DATABASE_SECRET_NAME" -o custom-columns=:data.username | base64 -d)
   INVENTORY_DB_PASS=$(kubectl -n "$PROJECT_NAME" get secret/"$HBI_DATABASE_SECRET_NAME" -o custom-columns=:data.password | base64 -d)
   INVENTORY_DB_NAME=$(kubectl -n "$PROJECT_NAME" get secret/"$HBI_DATABASE_SECRET_NAME" -o custom-columns=:data.name | base64 -d)
@@ -32,13 +31,12 @@ else
 fi
 
 # RBAC
-RBAC_SECRET_NAME="rbac-db"
-HAS_RBAC_SECRET=$(kubectl get secrets -o json  -n $PROJECT_NAME | jq ".items[] | select(.metadata.name==\"$RBAC_SECRET_NAME\")")
-if [ -n "$HAS_RBAC_SECRET" ]; then
-  RBAC_USER=$(kubectl -n "$PROJECT_NAME" get secret/"$RBAC_SECRET_NAME" -o custom-columns=:data.username | base64 -d)
-  RBAC_PASSWORD=$(kubectl -n "$PROJECT_NAME" get secret/"$RBAC_SECRET_NAME" -o custom-columns=:data.password | base64 -d)
-  RBAC_NAME=$(kubectl -n "$PROJECT_NAME" get secret/"$RBAC_SECRET_NAME" -o custom-columns=:data.name | base64 -d)
-  RBAC_HOSTNAME=$(kubectl -n "$PROJECT_NAME" get secret/"$RBAC_SECRET_NAME" -o custom-columns=:data.hostname | base64 -d)
+RBAC_DATABASE_SECRET_NAME="rbac-db"
+if kubectl get secret "$RBAC_DATABASE_SECRET_NAME" -n "$PROJECT_NAME" &> /dev/null; then
+  RBAC_USER=$(kubectl -n "$PROJECT_NAME" get secret/"$RBAC_DATABASE_SECRET_NAME" -o custom-columns=:data.username | base64 -d)
+  RBAC_PASSWORD=$(kubectl -n "$PROJECT_NAME" get secret/"$RBAC_DATABASE_SECRET_NAME" -o custom-columns=:data.password | base64 -d)
+  RBAC_NAME=$(kubectl -n "$PROJECT_NAME" get secret/"$RBAC_DATABASE_SECRET_NAME" -o custom-columns=:data.name | base64 -d)
+  RBAC_HOSTNAME=$(kubectl -n "$PROJECT_NAME" get secret/"$RBAC_DATABASE_SECRET_NAME" -o custom-columns=:data.hostname | base64 -d)
   echo "RBAC_USER: $RBAC_USER"
   echo "RBAC_PASSWORD: $RBAC_PASSWORD"
   echo "RBAC_NAME: $RBAC_NAME"
@@ -48,4 +46,6 @@ if [ -n "$HAS_RBAC_SECRET" ]; then
   export RBAC_PASSWORD
   export RBAC_NAME
   export RBAC_HOSTNAME
+else
+  echo "$RBAC_DATABASE_SECRET_NAME secret not found"
 fi
