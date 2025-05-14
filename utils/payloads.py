2c394fa89f19b86bbc1e9ce89d3082dd8025ca8c
Joao Paulo Ramos <jramos@redhat.com>
2025-05-14 09:56:21 -0300

    chore: add a way of creating multiple hosts for multiple org_ids

--- utils/payloads.py
+++ utils/payloads.py
@@ -7,9 +7,11 @@ from datetime import datetime
 from datetime import timedelta
 from datetime import timezone
 
+NUM_OF_DIFF_ORG_IDS = os.environ.get("NUM_OF_DIFF_ORG_IDS", "")
+
 # complete system identity
 IDENTITY = {
-    "org_id": "test",
+    "org_id": f"test{NUM_OF_DIFF_ORG_IDS}",
     "type": "System",
     "auth_type": "cert-auth",
     "system": {"cn": "1b36b20f-7fa0-4454-a6d2-008294e06378", "cert_type": "system"},
