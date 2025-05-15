410ee34c88b0dd316bbdf232f1d62bf1978c0e14
Muhammad Arif <40067075+thearifismail@users.noreply.github.com>
2025-05-15 14:48:47 -0400

    RHINENG-17952: Add instructions for using local code with Ephemeral na… (#2532)
    * RINENG-17952: Add instructions for using local code with Ephemeral namespaces
    
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED
    
    * RHINENG-17952: Add kessel user test identity for using rbac with hbi (#2521)
    
    * Add kessel user identity using rbac with hbi
    
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED
    
    * Update utils/payloads.py
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>
    Co-authored-by: Adarsh Dubey <addubey@redhat.com>
    
    * Use distinct env var for mq-workspaces pod count (#2536)
    
    * Add workspace_type filter, adjust resource_types behavior (#2533)
    
    Co-authored-by: Fabrícia Diniz <104025448+FabriciaDinizRH@users.noreply.github.com>
    
    * Correct permissions for script
    
    * feat(RHINENG-19732): Add account_number to workspace MQ schema (#2537)
    
    * Add account_number to workspace MQ schema
    
    * Address suggestions
    
    * responded to review comments
    
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED
    
    * Get secrets by name
    
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED
    
    * Remove duplicat kafka command
    
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED
    
    * Remove duplicat kafka command
    
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED
    
    * Update docs/debug_local_code_targeting_ephemeral_namespace.md
    
    Co-authored-by: Joao Paulo Ramos <joao.oramos11@gmail.com>
    
    ---------
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>
    Co-authored-by: Adarsh Dubey <addubey@redhat.com>
    Co-authored-by: Asa Price <aprice@redhat.com>
    Co-authored-by: Fabrícia Diniz <104025448+FabriciaDinizRH@users.noreply.github.com>
    Co-authored-by: Joao Paulo Ramos <joao.oramos11@gmail.com>

---
+++ docs/get_hbi_rbac_db_creds.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# this scripts helps with quickly getting credentials for accessing k8s resources.
+
+PROJECT_NAME=$1
+
+if [ -z "$PROJECT_NAME" ]; then
+  echo "openshift project or Kubernetes namespace required as input argument."
+  exit 1
+fi
+
+echo -e "Using namespace $PROJECT_NAME\n"
+
+# host inventory
+HBI_DATABASE_SECRET_NAME="host-inventory-db"
+if kubectl get secret "$HBI_DATABASE_SECRET_NAME" -n "$PROJECT_NAME" &> /dev/null; then
+  INVENTORY_DB_USER=$(kubectl -n "$PROJECT_NAME" get secret/"$HBI_DATABASE_SECRET_NAME" -o custom-columns=:data.username | base64 -d)
+  INVENTORY_DB_PASS=$(kubectl -n "$PROJECT_NAME" get secret/"$HBI_DATABASE_SECRET_NAME" -o custom-columns=:data.password | base64 -d)
+  INVENTORY_DB_NAME=$(kubectl -n "$PROJECT_NAME" get secret/"$HBI_DATABASE_SECRET_NAME" -o custom-columns=:data.name | base64 -d)
+  INVENTORY_DB_HOST=$(kubectl -n "$PROJECT_NAME" get secret/"$HBI_DATABASE_SECRET_NAME" -o custom-columns=:data.hostname | base64 -d)
+  echo "INVENTORY_DB_USER: $INVENTORY_DB_USER"
+  echo "INVENTORY_DB_PASS: $INVENTORY_DB_PASS"
+  echo "INVENTORY_DB_NAME: $INVENTORY_DB_NAME"
+  echo "INVENTORY_DB_HOST: $INVENTORY_DB_HOST"
+  echo -e ""
+  export INVENTORY_DB_USER
+  export INVENTORY_DB_PASS
+  export INVENTORY_DB_NAME
+  export INVENTORY_DB_HOST
+else
+  echo "$HBI_DATABASE_SECRET_NAME secret not found"
+fi
+
+# RBAC
+RBAC_DATABASE_SECRET_NAME="rbac-db"
+if kubectl get secret "$RBAC_DATABASE_SECRET_NAME" -n "$PROJECT_NAME" &> /dev/null; then
+  RBAC_USER=$(kubectl -n "$PROJECT_NAME" get secret/"$RBAC_DATABASE_SECRET_NAME" -o custom-columns=:data.username | base64 -d)
+  RBAC_PASSWORD=$(kubectl -n "$PROJECT_NAME" get secret/"$RBAC_DATABASE_SECRET_NAME" -o custom-columns=:data.password | base64 -d)
+  RBAC_NAME=$(kubectl -n "$PROJECT_NAME" get secret/"$RBAC_DATABASE_SECRET_NAME" -o custom-columns=:data.name | base64 -d)
+  RBAC_HOSTNAME=$(kubectl -n "$PROJECT_NAME" get secret/"$RBAC_DATABASE_SECRET_NAME" -o custom-columns=:data.hostname | base64 -d)
+  echo "RBAC_USER: $RBAC_USER"
+  echo "RBAC_PASSWORD: $RBAC_PASSWORD"
+  echo "RBAC_NAME: $RBAC_NAME"
+  echo "RBAC_HOSTNAME: $RBAC_HOSTNAME"
+  echo -e ""
+  export RBAC_USER
+  export RBAC_PASSWORD
+  export RBAC_NAME
+  export RBAC_HOSTNAME
+else
+  echo "$RBAC_DATABASE_SECRET_NAME secret not found"
+fi
---
+++ docs/set_hbi_rbac_ports.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+pkill -f "kubectl port-forward"
+
+PROJECT_NAME=$1
+
+if [ -z "$PROJECT_NAME" ]; then
+  echo "openshift project or Kubernetes namespace required as input argument."
+  exit 1
+fi
+
+echo "Using namespace $PROJECT_NAME"
+
+# kafka
+ENV_EPHEM_KAFKA_NAME=$(kubectl get kafka -o custom-columns=:metadata.name -n "$PROJECT_NAME" | grep env-ephem |xargs)
+ENV_EPHEM_KAFKA_SVC="svc/$ENV_EPHEM_KAFKA_NAME-kafka-bootstrap"
+RBAC_KAFKA_NAME=$(kubectl get kafka -o custom-columns=:metadata.name -n "$PROJECT_NAME" | grep rbac |xargs)
+RBAC_EPHEM_KAFKA_SVC="svc/$ENV_EPHEM_KAFKA_NAME-kafka-bootstrap"
+ENV_EPHEM_CONNECT_NAME=$(kubectl get kafkaconnect -o custom-columns=:metadata.name -n "$PROJECT_NAME" | grep env-ephem | xargs)
+ENV_EPHEM_CONNECT_SVC="svc/$ENV_EPHEM_CONNECT_NAME-connect-api"
+
+# host-inventory
+HBI_DB_SVC="svc/host-inventory-db"
+HBI_SVC="svc/host-inventory-service"
+
+# rbac
+RBAC_DB_SVC="svc/rbac-db"
+RBAC_SVC="svc/rbac-service"
+
+kubectl port-forward "$ENV_EPHEM_CONNECT_SVC" 8083:8083 -n "$PROJECT_NAME" >/dev/null 2>&1 &
+kubectl port-forward "$ENV_EPHEM_KAFKA_SVC" 9092:9092 -n "$PROJECT_NAME" >/dev/null 2>&1 &
+kubectl port-forward "$ENV_EPHEM_KAFKA_SVC" 29092:9092 -n "$PROJECT_NAME" >/dev/null 2>&1 &
+
+kubectl port-forward "$RBAC_DB_SVC" 5433:5432 -n "$PROJECT_NAME" >/dev/null 2>&1 &
+kubectl port-forward "$RBAC_SVC" 8111:8000 -n "$PROJECT_NAME" >/dev/null 2>&1 &
+
+kubectl port-forward "$HBI_DB_SVC" 5432:5432 -n "$PROJECT_NAME" >/dev/null 2>&1 &
+kubectl port-forward "$HBI_SVC" 8000:8000 -n "$PROJECT_NAME" >/dev/null 2>&1 &
+
+pgrep -fla "kubectl port-forward"
