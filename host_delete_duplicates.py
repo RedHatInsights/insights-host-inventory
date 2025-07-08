a0c041423edf22cb1dac16148e306cec78f54a62
Frantisek Stavela <fstavela@redhat.com>
2025-07-08 14:02:57 +0200

    Refactor 'delete duplicates' job
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED

--- host_delete_duplicates.py
+++ host_delete_duplicates.py
@@ -2,49 +2,29 @@ import sys
 from functools import partial
 from logging import Logger
 
-from prometheus_client import CollectorRegistry
-from prometheus_client import push_to_gateway
-from sqlalchemy import create_engine
+from connexion import FlaskApp
 from sqlalchemy.orm import Session
-from sqlalchemy.orm import sessionmaker
 
 from app.config import Config
 from app.environment import RuntimeEnvironment
-from app.logging import configure_logging
 from app.logging import get_logger
 from app.logging import threadctx
 from app.queue.event_producer import EventProducer
+from jobs.common import excepthook
+from jobs.common import init_db
+from jobs.common import job_setup
 from lib.db import multi_session_guard
 from lib.handlers import ShutdownHandler
 from lib.handlers import register_shutdown
 from lib.host_remove_duplicates import delete_duplicate_hosts
 from lib.metrics import delete_duplicate_host_count
 
-__all__ = ("main", "run")
+__all__ = ("run",)
 
 PROMETHEUS_JOB = "duplicate-hosts-remover"
 LOGGER_NAME = "duplicate-hosts-remover"
 COLLECTED_METRICS = (delete_duplicate_host_count,)
 RUNTIME_ENVIRONMENT = RuntimeEnvironment.JOB
-
-
-def _init_config() -> Config:
-    config = Config(RUNTIME_ENVIRONMENT)
-    config.log_configuration()
-    return config
-
-
-def _init_db(config: Config) -> sessionmaker:
-    engine = create_engine(config.db_uri)
-    return sessionmaker(bind=engine)
-
-
-def _prometheus_job(namespace: str) -> str:
-    return f"{PROMETHEUS_JOB}-{namespace}" if namespace else PROMETHEUS_JOB
-
-
-def _excepthook(logger: Logger, type, value, traceback) -> None:  # noqa: ARG001, needed by sys.excepthook
-    logger.exception("Host duplicate remover failed", exc_info=value)
 
 
 def run(
@@ -54,33 +34,39 @@ def run(
     hosts_session: Session,
     misc_session: Session,
     event_producer: EventProducer,
+    notifications_event_producer: EventProducer,
     shutdown_handler: ShutdownHandler,
-) -> int:
-    num_deleted = delete_duplicate_hosts(
-        org_ids_session,
-        hosts_session,
-        misc_session,
-        config.script_chunk_size,
-        logger,
-        event_producer,
-        shutdown_handler.shut_down,
+    application: FlaskApp,
+) -> None:
+    with application.app.app_context():
+        threadctx.request_id = None
+        try:
+            num_deleted = delete_duplicate_hosts(
+                org_ids_session,
+                hosts_session,
+                misc_session,
+                config.script_chunk_size,
+                logger,
+                event_producer,
+                notifications_event_producer,
+                shutdown_handler.shut_down,
+            )
+            logger.info(f"Total number of hosts deleted: {num_deleted}")
+
+        except InterruptedError:
+            logger.info(f"{PROMETHEUS_JOB} was interrupted.")
+
+
+if __name__ == "__main__":
+    logger = get_logger(LOGGER_NAME)
+    job_type = "Delete duplicate hosts"
+    sys.excepthook = partial(excepthook, logger, job_type)
+
+    config, _, event_producer, notifications_event_producer, shutdown_handler, application = job_setup(
+        COLLECTED_METRICS, PROMETHEUS_JOB
     )
-    logger.info(f"Total number of hosts deleted: {num_deleted}")
-    return num_deleted
 
-
-def main(logger: Logger) -> None:
-    config = _init_config()
-    registry = CollectorRegistry()
-
-    for metric in COLLECTED_METRICS:
-        registry.register(metric)
-
-    job = _prometheus_job(config.kubernetes_namespace)
-    prometheus_shutdown = partial(push_to_gateway, config.prometheus_pushgateway, job, registry)
-    register_shutdown(prometheus_shutdown, "Pushing metrics")
-
-    SessionMaker = _init_db(config)
+    SessionMaker = init_db(config)
     org_ids_session = SessionMaker()
     hosts_session = SessionMaker()
     misc_session = SessionMaker()
@@ -88,21 +74,15 @@ def main(logger: Logger) -> None:
     register_shutdown(hosts_session.get_bind().dispose, "Closing database")
     register_shutdown(misc_session.get_bind().dispose, "Closing database")
 
-    event_producer = EventProducer(config, config.event_topic)
-    register_shutdown(event_producer.close, "Closing producer")
-
-    shutdown_handler = ShutdownHandler()
-    shutdown_handler.register()
-
     with multi_session_guard([org_ids_session, hosts_session, misc_session]):
-        run(config, logger, org_ids_session, hosts_session, misc_session, event_producer, shutdown_handler)
-
-
-if __name__ == "__main__":
-    configure_logging()
-
-    logger = get_logger(LOGGER_NAME)
-    sys.excepthook = partial(_excepthook, logger)
-
-    threadctx.request_id = None
-    main(logger)
+        run(
+            config,
+            logger,
+            org_ids_session,
+            hosts_session,
+            misc_session,
+            event_producer,
+            notifications_event_producer,
+            shutdown_handler,
+            application,
+        )
