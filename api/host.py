aa8d1b11b37783fa6db6b486afc7f75938ef1ce5
Fabricia Diniz <agomesdi@redhat.com>
2025-07-11 02:14:19 +0100

    feat(RHINENG-19125): add system_type filter

--- api/host.py
+++ api/host.py
@@ -86,6 +86,7 @@ def get_host_list(
     order_how=None,
     staleness=None,
     registered_with=None,
+    system_type=None,
     filter=None,
     fields=None,
     rbac_filter=None,
@@ -108,6 +109,7 @@ def get_host_list(
             order_by,
             order_how,
             registered_with,
+            system_type,
             filter,
             fields,
         ]
@@ -149,6 +151,7 @@ def get_host_list(
             order_how,
             staleness,
             registered_with,
+            system_type,
             filter,
             fields,
             rbac_filter,
@@ -223,6 +226,7 @@ def delete_hosts_by_filter(
             updated_end,
             group_name,
             registered_with,
+            None,  # system_type is not used in this query
             staleness,
             tags,
             filter,
