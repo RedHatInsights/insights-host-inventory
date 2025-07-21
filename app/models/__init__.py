e1781855f2e77b7ec4797b4be8b5d3014cf38bb6
Muhammad Arif <aarif@redhat.com>
2025-07-21 17:34:11 -0400

    Add outbox table migration

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -22,6 +22,7 @@ from app.models.group import Group
 from app.models.host import Host
 from app.models.host import LimitedHost
 from app.models.host_group_assoc import HostGroupAssoc
+from app.models.outbox import Outbox
 from app.models.host_inventory_metadata import HostInventoryMetadata
 from app.models.schemas import CanonicalFactsSchema
 from app.models.schemas import DiskDeviceSchema
@@ -73,6 +74,7 @@ __all__ = [
     "Host",
     "Group",
     "HostGroupAssoc",
+    "Outbox",
     "Staleness",
     "HostInventoryMetadata",
     "DiskDeviceSchema",
---
+++ app/models/outbox.py
@@ -0,0 +1,38 @@
+import uuid
+
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.dialects.postgresql import UUID
+
+from app.models.constants import INVENTORY_SCHEMA
+from app.models.database import db
+from app.models.utils import _time_now
+
+
+class Outbox(db.Model):
+    __tablename__ = "outbox"
+    __table_args__ = (
+        {"schema": INVENTORY_SCHEMA},
+    )
+
+    def __init__(
+        self,
+        aggregate_type="hbi.hosts",
+        aggregate_id=None,
+        type=None,
+        payload=None,
+        id=None,
+    ):
+        self.id = id or uuid.uuid4()
+        self.aggregate_type = aggregate_type
+        self.aggregate_id = aggregate_id
+        self.type = type
+        self.payload = payload or {}
+
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    aggregate_type = db.Column(db.String(255), nullable=False, default="hbi.hosts")
+    aggregate_id = db.Column(UUID(as_uuid=True), nullable=False)
+    type = db.Column(db.String(255), nullable=False)
+    payload = db.Column(JSONB)
+
+    def __repr__(self):
+        return f"<Outbox(id={self.id}, aggregate_type={self.aggregate_type}, aggregate_id={self.aggregate_id}, type={self.type})>" 
