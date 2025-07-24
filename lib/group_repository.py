4919f2558b29e1bdedadf0b104e0be96476bc0e7
Muhammad Arif <aarif@redhat.com>
2025-07-24 18:21:50 -0400

    Fixed detached session enoough that the hosts table shows the new group

--- lib/group_repository.py
+++ lib/group_repository.py
@@ -257,20 +257,23 @@ def add_group_with_hosts(
     staleness: AttrDict,
     event_producer: EventProducer,
 ) -> Group:
+    created_group_id = None
     with session_guard(db.session):
         # Create group
         created_group = add_group(group_name, identity.org_id, account, group_id, ungrouped)
+        # Capture the group ID before the session is closed to avoid DetachedInstanceError
+        created_group_id = created_group.id
 
         # Add hosts to group
         if host_id_list:
-            _add_hosts_to_group(created_group.id, host_id_list, identity.org_id)
+            _add_hosts_to_group(created_group_id, host_id_list, identity.org_id)
 
     # gets the ID of the group after it has been committed
     created_group = Group.query.filter((Group.name == group_name) & (Group.org_id == identity.org_id)).one_or_none()
 
     # Produce update messages once the DB session has been closed
     serialized_groups, host_id_list = _update_hosts_for_group_changes(
-        host_id_list, group_id_list=[created_group.id], identity=identity
+        host_id_list, group_id_list=[created_group_id], identity=identity
     )
     _process_host_changes(host_id_list, serialized_groups, staleness, identity, event_producer)
 
