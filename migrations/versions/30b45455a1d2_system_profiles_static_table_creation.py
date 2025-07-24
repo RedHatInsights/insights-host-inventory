ba7b0be84eb4f8d7127c83e5ce7f74e01e277c17
Fabricia Diniz <agomesdi@redhat.com>
2025-07-24 12:11:41 +0100

    feat(RHINENG-18032): implement system_profiles_static table

---
+++ migrations/versions/30b45455a1d2_system_profiles_static_table_creation.py
@@ -0,0 +1,106 @@
+"""system_profiles_static table creation
+
+Revision ID: 30b45455a1d2
+Revises: 61c1b152246a
+Create Date: 2025-07-23 15:59:07.694257
+
+"""
+
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy.dialects import postgresql
+
+# revision identifiers, used by Alembic.
+revision = "30b45455a1d2"
+down_revision = "61c1b152246a"
+branch_labels = None
+depends_on = None
+
+
+def validate_inputs(num_partitions: int):
+    """Validate input parameters"""
+    if not 1 <= num_partitions <= 32:
+        raise ValueError(f"Invalid number of partitions: {num_partitions}. Must be between 1 and 32.")
+
+
+def upgrade():
+    op.create_table(
+        "system_profiles_static",
+        # --- PK COLUMNS ---
+        sa.Column("org_id", sa.String(36), nullable=False),
+        sa.Column("host_id", sa.UUID(as_uuid=True), nullable=False),
+        # --- STATIC FIELDS ---
+        sa.Column("arch", sa.String(length=50), nullable=True),
+        sa.Column("basearch", sa.String(length=50), nullable=True),
+        sa.Column("bios_release_date", sa.String(length=50), nullable=True),
+        sa.Column("bios_vendor", sa.String(length=100), nullable=True),
+        sa.Column("bios_version", sa.String(length=100), nullable=True),
+        sa.Column("bootc_status", postgresql.JSONB(astext_type=sa.Text()), nullable=True),
+        sa.Column("cloud_provider", sa.String(length=100), nullable=True),
+        sa.Column("conversions", postgresql.JSONB(astext_type=sa.Text()), nullable=True),
+        sa.Column("cores_per_socket", sa.Integer(), nullable=True),
+        sa.Column("cpu_model", sa.String(length=100), nullable=True),
+        sa.Column("disk_devices", postgresql.ARRAY(postgresql.JSONB()), nullable=True),
+        sa.Column("dnf_modules", postgresql.ARRAY(postgresql.JSONB()), nullable=True),
+        sa.Column("enabled_services", postgresql.ARRAY(sa.String(length=512)), nullable=True),
+        sa.Column("gpg_pubkeys", postgresql.ARRAY(sa.String(length=512)), nullable=True),
+        sa.Column("greenboot_fallback_detected", sa.Boolean(), server_default="FALSE", nullable=True),
+        sa.Column("greenboot_status", sa.String(length=5), nullable=True),
+        sa.Column("host_type", sa.String(length=4), nullable=True),
+        sa.Column("image_builder", postgresql.JSONB(astext_type=sa.Text()), nullable=True),
+        sa.Column("infrastructure_type", sa.String(length=100), nullable=True),
+        sa.Column("infrastructure_vendor", sa.String(length=100), nullable=True),
+        sa.Column("insights_client_version", sa.String(length=50), nullable=True),
+        sa.Column("installed_packages_delta", postgresql.ARRAY(sa.String(length=512)), nullable=True),
+        sa.Column("installed_services", postgresql.ARRAY(sa.String(length=512)), nullable=True),
+        sa.Column("intersystems", postgresql.JSONB(astext_type=sa.Text()), nullable=True),
+        sa.Column("is_marketplace", sa.Boolean(), server_default="FALSE", nullable=True),
+        sa.Column("katello_agent_running", sa.Boolean(), server_default="FALSE", nullable=True),
+        sa.Column("number_of_cpus", sa.Integer(), nullable=True),
+        sa.Column("number_of_sockets", sa.Integer(), nullable=True),
+        sa.Column("operating_system", postgresql.JSONB(astext_type=sa.Text()), nullable=True),
+        sa.Column("os_kernel_version", sa.String(length=20), nullable=True),
+        sa.Column("os_release", sa.String(length=100), nullable=True),
+        sa.Column("owner_id", postgresql.UUID(as_uuid=True), nullable=True),
+        sa.Column("public_dns", postgresql.ARRAY(sa.String(length=100)), nullable=True),
+        sa.Column("public_ipv4_addresses", postgresql.ARRAY(sa.String(length=15)), nullable=True),
+        sa.Column("releasever", sa.String(length=100), nullable=True),
+        sa.Column("rhc_client_id", postgresql.UUID(as_uuid=True), nullable=True),
+        sa.Column("rhc_config_state", postgresql.UUID(as_uuid=True), nullable=True),
+        sa.Column("rhel_ai", postgresql.JSONB(astext_type=sa.Text()), nullable=True),
+        sa.Column("rhsm", postgresql.JSONB(astext_type=sa.Text()), nullable=True),
+        sa.Column("rpm_ostree_deployments", postgresql.ARRAY(postgresql.JSONB()), nullable=True),
+        sa.Column("satellite_managed", sa.Boolean(), server_default="FALSE", nullable=True),
+        sa.Column("selinux_config_file", sa.String(length=128), nullable=True),
+        sa.Column("selinux_current_mode", sa.String(length=10), nullable=True),
+        sa.Column("subscription_auto_attach", sa.String(length=100), nullable=True),
+        sa.PrimaryKeyConstraint("owner_id", name=op.f("pk_system_profiles_static")),
+        # --- CONSTRAINTS ---
+        sa.ForeignKeyConstraint(
+            ["org_id", "host_id"], ["hbi.hosts.org_id", "hbi.hosts.id"], name=op.f("fk_system_profiles_static_hosts")
+        ),
+        sa.PrimaryKeyConstraint("org_id", "host_id", name=op.f("pk_system_profiles_static")),
+        schema="hbi",
+        postgresql_partition_by="HASH (org_id)",
+    )
+
+    # --- PARTITION CREATION ---
+    op.execute(
+        """
+        DO $$
+        BEGIN
+            FOR i IN 0..31 LOOP
+                EXECUTE format('CREATE TABLE hbi.system_profiles_static_p%s PARTITION
+                OF hbi.system_profiles_static FOR VALUES WITH (MODULUS 32, REMAINDER %s);', i, i);
+            END LOOP;
+        END;
+        $$;
+        """
+    )
+
+
+def downgrade():
+    """
+    Removes the system_profiles_static table and its partitions.
+    """
+    op.drop_table("system_profiles_static", schema="hbi")
