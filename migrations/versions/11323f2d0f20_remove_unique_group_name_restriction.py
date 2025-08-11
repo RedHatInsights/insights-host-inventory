1be904cc41f318cf0634592b4fe5a88c5e48b839
Muhammad Arif <aarif@redhat.com>
2025-08-11 16:51:21 -0400

    Put back the correct Makefile tasks

--- migrations/versions/11323f2d0f20_remove_unique_group_name_restriction.py
+++ migrations/versions/11323f2d0f20_remove_unique_group_name_restriction.py
@@ -1,16 +1,17 @@
-"""remove group name uniqunesness restriction
+"""remove_unique_group_name_restriction
 
-Revision ID: 6cba58098c84
-Revises: 61c1b152246a
-Create Date: 2025-08-11 16:13:34.220316
+Revision ID: 11323f2d0f20
+Revises: 6714f3063d90
+Create Date: 2025-08-11 16:47:35.046777
 
 """
 from alembic import op
 from sqlalchemy import text
 
+
 # revision identifiers, used by Alembic.
-revision = '6cba58098c84'
-down_revision = '61c1b152246a'
+revision = '11323f2d0f20'
+down_revision = '6714f3063d90'
 branch_labels = None
 depends_on = None
 
@@ -19,7 +20,6 @@ def upgrade():
     with op.get_context().autocommit_block():
         op.execute(text("CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_groups_org_id_name_ignorecase ON hbi.groups (lower(name), org_id);"))
         op.execute(text("DROP INDEX CONCURRENTLY IF EXISTS hbi.idx_groups_org_id_name_nocase;"))
-
 
 
 def downgrade():
