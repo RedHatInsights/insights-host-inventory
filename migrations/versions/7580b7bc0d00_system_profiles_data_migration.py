b48d147878ae45873a095bc02cf0e9dda628d4ab
Rodrigo Antunes <rantunes@redhat.com>
2025-08-11 13:30:05 -0300

    Sync with master branch

--- migrations/versions/7580b7bc0d00_system_profiles_data_migration.py
+++ migrations/versions/7580b7bc0d00_system_profiles_data_migration.py
@@ -1,6 +1,6 @@
 """system_profiles_data_migration
 Revision ID: 7580b7bc0d00
-Revises: 96e12ae08c05
+Revises: 6714f3063d90
 Create Date: 2025-07-31 08:21:15.437099
 """
 
@@ -12,7 +12,7 @@ from app.models.constants import INVENTORY_SCHEMA
 
 # revision identifiers, used by Alembic.
 revision = "7580b7bc0d00"
-down_revision = "96e12ae08c05"
+down_revision = "6714f3063d90"
 branch_labels = None
 depends_on = None
 
