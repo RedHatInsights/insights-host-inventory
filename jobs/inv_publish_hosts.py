7ad36449d872f4d3e1286bece52517ffa5437715
Rodrigo Antunes <rantunes@redhat.com>
2025-08-12 14:06:08 -0300

    chore(RHINENG-19061): Add a list of inactive replication slots to drop

--- jobs/inv_publish_hosts.py
+++ jobs/inv_publish_hosts.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 import sys
 from functools import partial
 
@@ -22,35 +22,100 @@ __all__ = ("main", "run")
 
 LOGGER_NAME = "hosts-syndicator"
 PROMETHEUS_JOB = "hosts-syndicator"
+RUNTIME_ENVIRONMENT = RuntimeEnvironment.JOB
+
+# Publications and columns
+PUBLICATION_NAME = "hbi_hosts_pub_v1_0_2"
+HOSTS_PUBLICATION_COLUMNS = [
+    "org_id",
+    "id",
+    "account",
+    "display_name",
+    "ansible_host",
+    "created_on",
+    "modified_on",
+    "facts",
+    "tags_alt",
+    "groups",
+    "last_check_in",
+    "stale_timestamp",
+    "deletion_timestamp",
+    "stale_warning_timestamp",
+    "reporter",
+    "per_reporter_staleness",
+    "insights_id",
+    "subscription_manager_id",
+    "satellite_id",
+    "fqdn",
+    "bios_uuid",
+    "ip_addresses",
+    "mac_addresses",
+    "provider_id",
+    "provider_type"
+]
+SP_DYNAMIC_PUBLICATION_COLUMNS = [
+    "org_id",
+    "host_id",
+    "insights_id",
+    "installed_packages",
+    "installed_products",
+    "workloads"
+]
+SP_STATIC_PUBLICATION_COLUMNS = (
+    "org_id",
+    "host_id",
+    "insights_id",
+    "arch",
+    "bootc_status",
+    "dnf_modules",
+    "host_type",
+    "image_builder",
+    "operating_system",
+    "owner_id",
+    "releasever",
+    "rhc_client_id",
+    "rhsm",
+    "satellite_managed",
+    "system_update_method",
+    "yum_repos"
+)
+
+# Publications to drop
+DROP_PUBLICATIONS = [
+    "hbi_hosts_pub",
+    "hbi_hosts_pub_v1_0_0",
+    "hbi_hosts_pub_v1_0_1",
+]
+
+# Replication slots to drop
+DROP_REPLICATION_SLOTS = ["roadmap_hosts_sub"]
+
+CHECK_PUBLICATION_SQL = sa_text("""
+    SELECT EXISTS(
+        SELECT 1 FROM pg_catalog.pg_publication WHERE pubname = :pubname
+    )
+""")
+CHECK_REPLICATION_SLOTS_SQL = sa_text("SELECT slot_name, active FROM pg_replication_slots")
+
+CREATE_PUBLICATION_SQL = sa_text(f"""
+    CREATE PUBLICATION {PUBLICATION_NAME}
+    FOR TABLE
+      hbi.hosts ({','.join(HOSTS_PUBLICATION_COLUMNS)}),
+      hbi.system_profiles_dynamic ({','.join(SP_DYNAMIC_PUBLICATION_COLUMNS)}),
+      hbi.system_profiles_static ({','.join(SP_STATIC_PUBLICATION_COLUMNS)})
+    WHERE (insights_id != '00000000-0000-0000-0000-000000000000')
+    WITH (publish_via_partition_root = true);
+""")
+
+DROP_PUBLICATION_SQL = "DROP PUBLICATION IF EXISTS "
+
+DROP_REPLICATION_SLOTS_SQL = sa_text("SELECT pg_drop_replication_slot(:slot_name)")
+
 COLLECTED_METRICS = (
     hosts_syndication_fail_count,
     hosts_syndication_success_count,
     hosts_syndication_time,
 )
-
-RUNTIME_ENVIRONMENT = RuntimeEnvironment.JOB
-
-PUBLICATION_NAME = "hbi_hosts_pub_v1_0_2"
-HOSTS_PUBLICATION_COLUMNS = "org_id,id,account,display_name,created_on,modified_on,facts,\
-ansible_host,insights_id,subscription_manager_id,satellite_id,fqdn,bios_uuid,ip_addresses,mac_addresses,\
-provider_id,provider_type,stale_timestamp,reporter,per_reporter_staleness,\
-groups,tags_alt,last_check_in,stale_warning_timestamp,deletion_timestamp"
-SP_DYNAMIC_PUBLICATION_COLUMNS = "org_id,host_id,installed_packages,installed_products,workloads"
-SP_STATIC_PUBLICATION_COLUMNS = "org_id,host_id,arch,bootc_status,dnf_modules,host_type,image_builder,\
-operating_system,owner_id,releasever,rhc_client_id,rhsm,satellite_managed,system_update_method,yum_repos"
-CHECK_PUBLICATION = f"SELECT EXISTS(SELECT * FROM pg_catalog.pg_publication WHERE pubname = '{PUBLICATION_NAME}')"
-CREATE_PUBLICATION = (
-    f"CREATE PUBLICATION {PUBLICATION_NAME} "
-    f"FOR TABLE "
-    f"hbi.hosts ({HOSTS_PUBLICATION_COLUMNS}), "
-    f"hbi.system_profiles_dynamic ({SP_DYNAMIC_PUBLICATION_COLUMNS}), "
-    f"hbi.system_profiles_static ({SP_STATIC_PUBLICATION_COLUMNS}) "
-    f"WHERE (insights_id != '00000000-0000-0000-0000-000000000000') "
-    f"WITH (publish_via_partition_root = true);"
-)
-CHECK_REPLICATION_SLOTS = "SELECT slot_name, active FROM pg_replication_slots"
-DROP_PUBLICATIONS = ["hbi_hosts_pub", "hbi_hosts_pub_v1_0_0", "hbi_hosts_pub_v1_0_1"]
-DROP_PUBLICATION = "DROP PUBLICATION IF EXISTS "
 
 
 def _init_config():
@@ -68,46 +133,54 @@ def _prometheus_job(namespace):
     return f"{PROMETHEUS_JOB}-{namespace}" if namespace else PROMETHEUS_JOB
 
 
-def _excepthook(logger, type, value, traceback):  # noqa: ARG001, needed by sys.excepthook
-    logger.exception("Inventory migration failed", exc_info=value)
+def _excepthook(logger, exc_type, value, traceback):  # noqa
+    logger.exception("Inventory migration failed", exc_info=(exc_type, value, traceback))
+
+
+def setup_publication(session, logger):
+    # 1) drop any legacy publications
+    for pub in DROP_PUBLICATIONS:
+        logger.info(f"Dropping publication: {pub}")
+        session.execute(sa_text(DROP_PUBLICATION_SQL + pub))
+
+    # 2) check if the publication already exists
+    exists = session.execute(CHECK_PUBLICATION_SQL, {"pubname": PUBLICATION_NAME}).scalar()
+    if exists:
+        logger.info(f'Publication "{PUBLICATION_NAME}" already exists.')
+        return
+
+    # 3) create the publication
+    logger.info(f'Creating publication "{PUBLICATION_NAME}".')
+    session.execute(CREATE_PUBLICATION_SQL)
+
+    # 4) sanity-check replication slots
+    bad = []
+    for slot_name, active in session.execute(CHECK_REPLICATION_SLOTS_SQL):
+        if not active:
+            if slot_name in DROP_REPLICATION_SLOTS:
+                logger.info(f"Dropping inactive slot: {slot_name}")
+                session.execute(DROP_REPLICATION_SLOTS_SQL, {"slot_name": slot_name})
+            else:
+                bad.append(slot_name)
+    if bad:
+        raise RuntimeError(f"Inactive slots left open: {bad}")
+
+    logger.info(f'Publication "{PUBLICATION_NAME}" created!')
 
 
 def run(logger, session, application):
     with application.app.app_context():
-        # Drop publications by name
-        for pub in DROP_PUBLICATIONS:
-            logger.info(f"Dropping publication using the following SQL statement:\n\t{DROP_PUBLICATION + pub}")
-            session.execute(sa_text(DROP_PUBLICATION + pub))
-
-        logger.info(f"Checking for publication using the following SQL statement:\n\t{CHECK_PUBLICATION}")
-        result = session.execute(sa_text(CHECK_PUBLICATION))
-        if result.cursor.fetchone()[0]:
-            logger.info(f'Publication "{PUBLICATION_NAME}" found!')
+        try:
+            setup_publication(session, logger)
+        except Exception as e:
+            session.rollback()
+            hosts_syndication_fail_count.inc()
+            logger.error(f"Error during publication setup: {e}")
+            raise
         else:
-            logger.info(f'Creating publication "{PUBLICATION_NAME}" using \n\t{CREATE_PUBLICATION}')
-            try:
-                session.execute(sa_text(CREATE_PUBLICATION))
-
-                # check for inactive replication_slots
-                replication_slots = session.execute(sa_text(CHECK_REPLICATION_SLOTS)).all()
-                inactive = 0
-                for slot in replication_slots:
-                    slot_name, active = slot
-                    if not active:
-                        inactive += 1
-                        logger.error(f"Replication slot named {slot_name} is not active")
-                    if inactive > 0:
-                        exit(1)
-            except Exception as e:
-                session.rollback()
-                logger.error(f'Error encountered when creating the publication "{PUBLICATION_NAME}" \n\t{e}')
-                hosts_syndication_fail_count.inc()
-                raise e
-            else:
-                session.commit()
-                logger.info(f'Publication "{PUBLICATION_NAME}" created!!!')
-
-        hosts_syndication_success_count.inc()
+            session.commit()
+            hosts_syndication_success_count.inc()
+            logger.info("Publication setup completed successfully.")
 
 
 def main(logger):
@@ -117,6 +190,7 @@ def main(logger):
     registry = CollectorRegistry()
     for metric in COLLECTED_METRICS:
         registry.register(metric)
+
     job = _prometheus_job(config.kubernetes_namespace)
     prometheus_shutdown = partial(push_to_gateway, config.prometheus_pushgateway, job, registry)
     register_shutdown(prometheus_shutdown, "Pushing metrics")
@@ -127,6 +201,7 @@ def main(logger):
 
     shutdown_handler = ShutdownHandler()
     shutdown_handler.register()
+
     run(logger, session, application)
 
 
