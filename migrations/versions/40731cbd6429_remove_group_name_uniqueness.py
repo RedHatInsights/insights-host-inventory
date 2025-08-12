0095f7b0838de3366dc297cd629e013fd11b3ea5
Muhammad Arif <aarif@redhat.com>
2025-08-12 15:19:05 -0400

    make new index idx_groups_org_id_name_ignorecase unique

--- migrations/versions/40731cbd6429_remove_group_name_uniqueness.py
+++ migrations/versions/40731cbd6429_remove_group_name_uniqueness.py
@@ -1,8 +1,8 @@
 """remove group name uniqueness
 
-Revision ID: 05b5af5bde02
+Revision ID: 40731cbd6429
 Revises: 6714f3063d90
-Create Date: 2025-08-12 12:33:43.325674
+Create Date: 2025-08-12 14:59:36.772127
 
 """
 
@@ -10,7 +10,7 @@ from alembic import op
 from sqlalchemy import text
 
 # revision identifiers, used by Alembic.
-revision = "05b5af5bde02"
+revision = "40731cbd6429"
 down_revision = "6714f3063d90"
 branch_labels = None
 depends_on = None
@@ -20,7 +20,7 @@ def upgrade():
     with op.get_context().autocommit_block():
         op.execute(
             text("""
-            CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_groups_org_id_name_ignorecase
+            CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS idx_groups_org_id_name_ignorecase
             ON hbi.groups (lower(name), org_id);
         """)
         )
@@ -31,7 +31,8 @@ def downgrade():
     with op.get_context().autocommit_block():
         op.execute(
             text("""
-        CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_groups_org_id_name_nocase ON hbi.groups (lower(name), org_id);
+            CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS idx_groups_org_id_name_nocase
+            ON hbi.groups (lower(name), org_id);
         """)
         )
         op.execute(text("DROP INDEX CONCURRENTLY IF EXISTS hbi.idx_groups_org_id_name_ignorecase;"))
