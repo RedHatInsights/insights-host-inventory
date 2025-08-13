90826bc65ba2d7ad2cc71f9230476f1e5fdcb5a1
zhamilya <@computercamplove>
2025-08-13 14:15:51 +0200

    comment config

--- app/config.py
+++ app/config.py
@@ -304,12 +304,6 @@ class Config:
             "CONVENTIONAL_TIME_TO_DELETE_SECONDS", days_to_seconds(14)
         )
 
-        self.immutable_time_to_stale_seconds = self.conventional_time_to_stale_seconds
-
-        self.immutable_time_to_stale_warning_seconds = self.conventional_time_to_stale_warning_seconds
-
-        self.immutable_time_to_delete_seconds = self.conventional_time_to_delete_seconds
-
         self.use_sub_man_id_for_host_id = os.environ.get("USE_SUBMAN_ID", "false").lower() == "true"
         self.host_delete_chunk_size = int(os.getenv("HOST_DELETE_CHUNK_SIZE", "1000"))
         self.script_chunk_size = int(os.getenv("SCRIPT_CHUNK_SIZE", "500"))
