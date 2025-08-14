8e3d309ad09aaec85aa764a18662db1151bbaca0
Rodrigo Antunes <rantunes@redhat.com>
2025-08-14 16:39:29 -0300

    Check the index existence before updating replica indentity

--- migrations/versions/595dbad97e20_update_replica_identity_on_table_.py
+++ migrations/versions/595dbad97e20_update_replica_identity_on_table_.py
@@ -33,29 +33,46 @@ cascade to child partitions, so each partition must be updated individually.
 def upgrade():
     validate_num_partitions(TABLE_NUM_PARTITIONS)
 
-    for i in range(TABLE_NUM_PARTITIONS):
-        if MIGRATION_MODE == "managed":
-            # In managed environments, this index was created concurrently on the hosts table partitions,
-            # unlike automated environments, where index creation cascades automatically from the parent table.
-            # This is why the partition indexes for the hosts table ended up having two different naming schemes.
-            hosts_index_name = f"hosts_p{i}_replica_identity_idx"
+    def set_replica_identity_if_index_exists(schema: str, table: str, index: str):
+        result = (
+            op.get_bind()
+            .execute(
+                text("""
+                SELECT 1
+                FROM pg_indexes
+                WHERE schemaname = :schema
+                  AND tablename = :table
+                  AND indexname = :index
+            """),
+                {"schema": schema, "table": table, "index": index},
+            )
+            .fetchone()
+        )
+
+        if result:
+            op.execute(text(f"ALTER TABLE {schema}.{table} REPLICA IDENTITY USING INDEX {index};"))
         else:
-            hosts_index_name = f"hosts_p{i}_org_id_id_insights_id_idx"
+            print(f"Index '{index}' does not exist on table '{schema}.{table}'. Skipping replica identity update.")
 
-        hosts_partition_name = f"{INVENTORY_SCHEMA}.hosts_p{i}"
-        op.execute(text(f"ALTER TABLE {hosts_partition_name} REPLICA IDENTITY USING INDEX {hosts_index_name};"))
-
-        sp_static_index_name = f"system_profiles_static_p{i}_org_id_host_id_insights_id_idx"
-        sp_static_partition_name = f"{INVENTORY_SCHEMA}.system_profiles_static_p{i}"
-        op.execute(
-            text(f"ALTER TABLE {sp_static_partition_name} REPLICA IDENTITY USING INDEX {sp_static_index_name};")
+    for i in range(TABLE_NUM_PARTITIONS):
+        # In managed environments, this index was created concurrently on the hosts table partitions,
+        # unlike automated environments, where index creation cascades automatically from the parent table.
+        # This is why the partition indexes for the hosts table ended up having two different naming schemes.
+        hosts_index_name = (
+            f"hosts_p{i}_replica_identity_idx"
+            if MIGRATION_MODE == "managed"
+            else f"hosts_p{i}_org_id_id_insights_id_idx"
         )
+        hosts_table = f"hosts_p{i}"
+        set_replica_identity_if_index_exists(INVENTORY_SCHEMA, hosts_table, hosts_index_name)
 
-        sp_dynamic_index_name = f"system_profiles_dynamic_p{i}_org_id_host_id_insights_id_idx"
-        sp_dynamic_partition_name = f"{INVENTORY_SCHEMA}.system_profiles_dynamic_p{i}"
-        op.execute(
-            text(f"ALTER TABLE {sp_dynamic_partition_name} REPLICA IDENTITY USING INDEX {sp_dynamic_index_name};")
-        )
+        sp_static_table = f"system_profiles_static_p{i}"
+        sp_static_index = f"{sp_static_table}_org_id_host_id_insights_id_idx"
+        set_replica_identity_if_index_exists(INVENTORY_SCHEMA, sp_static_table, sp_static_index)
+
+        sp_dynamic_table = f"system_profiles_dynamic_p{i}"
+        sp_dynamic_index = f"{sp_dynamic_table}_org_id_host_id_insights_id_idx"
+        set_replica_identity_if_index_exists(INVENTORY_SCHEMA, sp_dynamic_table, sp_dynamic_index)
 
 
 def downgrade():
