990dda68c1ace89bffa932ff7e6b117d58d2c1af
Muhammad Arif <aarif@redhat.com>
2025-09-03 16:22:57 -0400

    Create signal table for use by the debezium connector

---
+++ app/models/debezium_signal.py
@@ -0,0 +1,26 @@
+import uuid
+
+from app.models.constants import INVENTORY_SCHEMA
+from app.models.database import db
+
+
+# This table is used to store the signal from the debezium connector.
+# The table will be registered to the debezium connector as a signal table.
+# The host-ivnentory does need to do anything, except have this table created.
+class DebeziumSignal(db.Model):
+    __tablename__ = "debezium_signal"
+    __table_args__ = ({"schema": INVENTORY_SCHEMA},)
+
+    def __init__(
+        self,
+        type_column,
+        data,
+        id=None,
+    ):
+        self.id = id or uuid.uuid4()
+        self.type_column = type_column
+        self.data = data
+
+    id = db.Column(db.String(36), primary_key=True, default=uuid.uuid4)
+    type_column = db.Column(db.String(50), nullable=False, name="type", default="incremental")
+    data = db.Column(db.String, nullable=True)
---
+++ migrations/versions/0305bb6cb3b7_add_debezium_signal_table.py
@@ -0,0 +1,29 @@
+"""Add debezium signal table
+
+Revision ID: 0305bb6cb3b7
+Revises: 91110801bc8e
+Create Date: 2025-09-03 15:56:31.038074
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+# revision identifiers, used by Alembic.
+revision = '0305bb6cb3b7'
+down_revision = '91110801bc8e'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table('debezium_signal',
+        sa.Column('id', sa.String(length=36), nullable=False, primary_key=True),
+        sa.Column('type', sa.String(length=50), nullable=False),
+        sa.Column('data', sa.String(), nullable=True),
+        schema='hbi'
+    )
+
+
+def downgrade():
+    op.drop_table('debezium_signal', schema='hbi')
