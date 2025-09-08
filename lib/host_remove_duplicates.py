061158df1f2b9ef4e18539c700cb8b5022512930
Frantisek Stavela <fstavela@redhat.com>
2025-09-08 15:32:02 +0200

    Fix OOM issue in duplicate hosts remover
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED

--- lib/host_remove_duplicates.py
+++ lib/host_remove_duplicates.py
@@ -45,6 +45,39 @@ def find_hosts_by_multiple_facts(canonical_facts: dict[str, str], query: Query) 
     )
 
 
+def delete_hosts_by_ids(
+    host_ids: set[str],
+    *,
+    org_id: str,
+    session: Session,
+    dry_run: bool,
+    logger: Logger,
+    event_producer: EventProducer,
+    notifications_event_producer: EventProducer,
+    chunk_size: int,
+    interrupt: Callable[[], bool],
+) -> int:
+    if dry_run:
+        logger.info(f"Found {len(host_ids)} duplicates in the current batch")
+        return len(host_ids)
+
+    hosts_by_ids_query = session.query(Host).filter(Host.org_id == org_id, Host.id.in_(host_ids))
+    deleted_count = len(
+        list(
+            delete_hosts(
+                hosts_by_ids_query,
+                event_producer,
+                notifications_event_producer,
+                chunk_size,
+                interrupt,
+                control_rule="DEDUP",
+            )
+        )
+    )
+    logger.info(f"Deleted {deleted_count} duplicates in the current batch")
+    return deleted_count
+
+
 def delete_duplicate_hosts(
     org_ids_session: Session,
     hosts_session: Session,
@@ -58,65 +91,65 @@ def delete_duplicate_hosts(
 ) -> int:
     total_deleted = 0
     hosts_query = hosts_session.query(Host)
-    org_id_query = org_ids_session.query(Host.org_id)
+    org_id_query = org_ids_session.query(Host.org_id).distinct(Host.org_id).order_by(Host.org_id)
 
     logger.info(f"Total number of hosts in inventory: {hosts_query.count()}")
-    logger.info(f"Total number of org_ids in inventory: {org_id_query.distinct(Host.org_id).count()}")
+    logger.info(f"Total number of org_ids in inventory: {org_id_query.count()}")
 
-    for org_id in org_id_query.distinct(Host.org_id).yield_per(chunk_size):
-        actual_org_id = org_id[0]  # query.distinct() returns a tuple of all queried columns
-        logger.info(f"Processing org_id: {actual_org_id}")
+    org_id_list = org_id_query.limit(chunk_size).all()
+    while len(org_id_list) > 0 and not interrupt():
+        for org_id in org_id_list:
+            actual_org_id = org_id[0]  # query.distinct() returns a tuple of all queried columns
+            logger.info(f"Processing org_id: {actual_org_id}")
 
-        unique_host_ids = set()
-        duplicate_host_ids = set()
-        misc_query = misc_session.query(Host).filter(Host.org_id == actual_org_id)
+            duplicates_per_org = 0
+            misc_query = misc_session.query(Host).filter(Host.org_id == actual_org_id)
 
-        def unique(host_list: list[Host]) -> None:
-            unique_host_ids.add(host_list[0].id)
-            logger.info(f"{host_list[0].id} is unique, total: {len(unique_host_ids)}")
-            if len(host_list) > 1:
-                for host_id in [h.id for h in host_list[1:] if h.id not in unique_host_ids]:
-                    duplicate_host_ids.add(host_id)
-                    logger.info(f"{host_id} is a potential duplicate, total: {len(duplicate_host_ids)}")
-
-        for host in (
-            hosts_query.filter(Host.org_id == actual_org_id).order_by(Host.last_check_in.desc()).yield_per(chunk_size)
-        ):
-            canonical_facts = host.canonical_facts
-            logger.info(f"find by canonical facts: {canonical_facts}")
-            matching_hosts = find_matching_hosts(canonical_facts, misc_query)
-
-            unique(matching_hosts)
-
-        hosts_session.expunge_all()
-        misc_session.expunge_all()
-
-        # delete duplicate hosts
-        if dry_run:
-            logger.info(f"Found {len(duplicate_host_ids)} duplicates for org_id: {actual_org_id}")
-            total_deleted += len(duplicate_host_ids)
-        else:
-            hosts_by_ids_query = misc_session.query(Host).filter(
-                Host.org_id == actual_org_id, Host.id.in_(duplicate_host_ids)
+            host_list = (
+                hosts_query.filter(Host.org_id == actual_org_id)
+                .order_by(Host.last_check_in.desc())
+                .limit(chunk_size)
+                .all()
             )
-            deleted_count = len(
-                list(
-                    delete_hosts(
-                        hosts_by_ids_query,
-                        event_producer,
-                        notifications_event_producer,
-                        chunk_size,
-                        interrupt,
-                        control_rule="DEDUP",
+            while len(host_list) > 0 and not interrupt():
+                # Process a batch of hosts
+                duplicate_host_ids = set()
+                for host in host_list:
+                    canonical_facts = host.canonical_facts
+                    logger.info(f"Find by canonical facts: {canonical_facts}")
+                    matching_hosts = find_matching_hosts(canonical_facts, misc_query)
+
+                    logger.info(f"Found {len(matching_hosts)} matching hosts ({len(matching_hosts) - 1} duplicates)")
+                    if len(matching_hosts) > 1:
+                        duplicate_host_ids.update([host.id for host in matching_hosts[1:]])
+
+                if duplicate_host_ids:
+                    deleted_count = delete_hosts_by_ids(
+                        duplicate_host_ids,
+                        org_id=actual_org_id,
+                        session=misc_session,
+                        dry_run=dry_run,
+                        logger=logger,
+                        event_producer=event_producer,
+                        notifications_event_producer=notifications_event_producer,
+                        chunk_size=chunk_size,
+                        interrupt=interrupt,
                     )
+                    duplicates_per_org += deleted_count
+
+                host_list = (
+                    hosts_query.filter(Host.org_id == actual_org_id, Host.last_check_in < host_list[-1].last_check_in)
+                    .order_by(Host.last_check_in.desc())
+                    .limit(chunk_size)
+                    .all()
                 )
-            )
-            logger.info(f"Deleted {deleted_count} hosts for org_id: {actual_org_id}")
-            total_deleted += deleted_count
 
-        misc_session.expunge_all()
+                misc_session.expunge_all()
 
-        if interrupt():
-            break
+            logger.info(f"Found {duplicates_per_org} duplicates for org_id: {actual_org_id}")
+            total_deleted += duplicates_per_org
+            hosts_session.expunge_all()
+
+        org_id_list = org_id_query.filter(Host.org_id > actual_org_id).limit(chunk_size).all()
 
     return total_deleted
