d8dec1baadaeb0743c320a089d05372e4d9184eb
zhamilya <@computercamplove>
2025-09-18 15:04:21 +0200

    fix

--- migrations/versions/bf78431e36c3_remove_immutable__fileds_from_staleness.py
+++ migrations/versions/bf78431e36c3_remove_immutable__fileds_from_staleness.py
@@ -5,15 +5,16 @@ Revises: 7439696a760f
 Create Date: 2025-09-18 14:28:37.426553
 
 """
-from alembic import op
+
 import sqlalchemy as sa
-from sqlalchemy.dialects import postgresql
-from app.models.constants import INVENTORY_SCHEMA
+from alembic import op
+
 from app.culling import days_to_seconds
+from app.models.constants import INVENTORY_SCHEMA
 
 # revision identifiers, used by Alembic.
-revision = 'bf78431e36c3'
-down_revision = '7439696a760f'
+revision = "bf78431e36c3"
+down_revision = "7439696a760f"
 branch_labels = None
 depends_on = None
 
@@ -25,6 +26,16 @@ def upgrade():
 
 
 def downgrade():
-    op.add_column("staleness", sa.Column("immutable_time_to_stale", sa.Integer(104400), nullable=False), schema="hbi")
-    op.add_column("staleness", sa.Column("immutable_time_to_stale_warning", sa.Integer(days_to_seconds(7)), nullable=False), schema="hbi")
-    op.add_column("staleness", sa.Column("immutable_time_to_delete", sa.Integer(days_to_seconds(14)), nullable=False), schema="hbi")
+    op.add_column(
+        "staleness", sa.Column("immutable_time_to_stale", sa.Integer(104400), nullable=False), schema=INVENTORY_SCHEMA
+    )
+    op.add_column(
+        "staleness",
+        sa.Column("immutable_time_to_stale_warning", sa.Integer(days_to_seconds(7)), nullable=False),
+        schema=INVENTORY_SCHEMA,
+    )
+    op.add_column(
+        "staleness",
+        sa.Column("immutable_time_to_delete", sa.Integer(days_to_seconds(14)), nullable=False),
+        schema=INVENTORY_SCHEMA,
+    )
