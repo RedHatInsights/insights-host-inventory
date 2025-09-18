83c6cb690b83d6de04ee363ef1796a9159852a61
Muhammad Arif <aarif@redhat.com>
2025-09-18 17:51:01 -0400

    Updates by linting

--- migrations/versions/94a16dd3106c_signal_table_columns_update.py
+++ migrations/versions/94a16dd3106c_signal_table_columns_update.py
@@ -5,66 +5,72 @@ Revises: 0d4ee3bf6b94
 Create Date: 2025-09-18 15:16:34.854784
 
 """
-from alembic import op
+
 import sqlalchemy as sa
-from sqlalchemy.dialects import postgresql
+from alembic import op
 
 # revision identifiers, used by Alembic.
-revision = '94a16dd3106c'
-down_revision = '0d4ee3bf6b94'
+revision = "94a16dd3106c"
+down_revision = "0d4ee3bf6b94"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
     op.alter_column(
-        "debezium_signal", "id",
+        "debezium_signal",
+        "id",
         existing_type=sa.String(36),
         type_=sa.String(50),
-        existing_nullable=False, # Adjust based on your column's nullability
+        existing_nullable=False,  # Adjust based on your column's nullability
         primary_key=True,
         schema="hbi",
     )
 
     op.alter_column(
-        "debezium_signal", "type",
+        "debezium_signal",
+        "type",
         existing_type=sa.String(50),
         type_=sa.String(32),
-        existing_nullable=False, # Adjust based on your column's nullability
+        existing_nullable=False,  # Adjust based on your column's nullability
         schema="hbi",
     )
 
     op.alter_column(
-        "debezium_signal", "data",
+        "debezium_signal",
+        "data",
         existing_type=sa.String(),
         type_=sa.String(2048),
-        existing_nullable=True, # Adjust based on your column's nullability
+        existing_nullable=True,  # Adjust based on your column's nullability
         schema="hbi",
     )
 
 
 def downgrade():
     op.alter_column(
-        "debezium_signal", "id",
+        "debezium_signal",
+        "id",
         existing_type=sa.String(50),
         type_=sa.String(36),
-        existing_nullable=False, # Adjust based on your column's nullability
+        existing_nullable=False,  # Adjust based on your column's nullability
         primary_key=True,
         schema="hbi",
     )
 
     op.alter_column(
-        "debezium_signal", "type",
+        "debezium_signal",
+        "type",
         existing_type=sa.String(32),
         type_=sa.String(50),
-        existing_nullable=False, # Adjust based on your column's nullability
+        existing_nullable=False,  # Adjust based on your column's nullability
         schema="hbi",
     )
 
     op.alter_column(
-        "debezium_signal", "data",
+        "debezium_signal",
+        "data",
         existing_type=sa.String(2048),
         type_=sa.String(),
-        existing_nullable=True, # Adjust based on your column's nullability
+        existing_nullable=True,  # Adjust based on your column's nullability
         schema="hbi",
     )
