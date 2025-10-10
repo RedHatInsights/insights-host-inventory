eb5896e50dd13459ed6d92804444319ae8fb37b6
Asa Price <aprice@redhat.com>
2025-10-10 11:24:38 -0400

    Fix host.groups serialization in PATCH /groups logic

--- tests/test_api_groups_delete.py
+++ tests/test_api_groups_delete.py
@@ -43,16 +43,21 @@ def test_delete_group_ids(db_create_group, db_get_group_by_id, api_delete_groups
         assert not db_get_group_by_id(group_id)
 
 
+@pytest.mark.parametrize("kessel_enabled", [True, False])
 def test_remove_hosts_from_existing_group(
     db_create_group,
     db_create_host,
     db_get_hosts_for_group,
     db_create_host_group_assoc,
+    db_get_ungrouped_group,
     api_remove_hosts_from_group,
     event_producer,
     mocker,
+    kessel_enabled,
 ):
     mocker.patch.object(event_producer, "write_event")
+    mocker.patch("api.group.get_flag_value", return_value=kessel_enabled)
+    mocker.patch("lib.group_repository.get_flag_value", return_value=kessel_enabled)
     # Create a group and 3 hosts
     group_id = db_create_group("test_group").id
     host_id_list = [str(db_create_host().id) for _ in range(3)]
@@ -66,20 +71,23 @@ def test_remove_hosts_from_existing_group(
     assert len(hosts_before) == 3
 
     # Remove the first two hosts from the group
-    response_status, _ = api_remove_hosts_from_group(group_id, [host for host in host_id_list[0:2]])
+    response_status, _ = api_remove_hosts_from_group(group_id, list(host_id_list[:2]))
     assert response_status == 204
 
     # Confirm that the group now only contains the last host
     hosts_after = db_get_hosts_for_group(group_id)
     assert len(hosts_after) == 1
     assert str(hosts_after[0].id) == host_id_list[2]
-
     assert event_producer.write_event.call_count == 2
+
     for call_arg in event_producer.write_event.call_args_list:
         event = json.loads(call_arg[0][0])
         host = event["host"]
-        assert host["id"] in host_id_list[0:2]
-        assert len(host["groups"]) == 0
+        assert host["id"] in host_id_list[:2]
+        if kessel_enabled:
+            assert host["groups"][0]["id"] == str(db_get_ungrouped_group(hosts_after[0].org_id).id)
+        else:
+            assert len(host["groups"]) == 0
         assert event["platform_metadata"] == {"b64_identity": to_auth_header(Identity(obj=USER_IDENTITY))}
 
 
