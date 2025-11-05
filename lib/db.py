282cad71a9d3d9643b8c5f38bc8b053230a8f00b
Asa Price <aprice@redhat.com>
2025-11-05 10:46:03 -0500

    Remove unused code

--- lib/db.py
+++ lib/db.py
@@ -1,27 +1,6 @@
 from contextlib import contextmanager
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-from app.config import Config
-from app.environment import RuntimeEnvironment
 from app.models import db
-
-
-# TODO: This is a temporary solution to create a separate database session for independent database operations.
-@contextmanager
-def get_independent_db_session():
-    """Create a separate database session for independent database operations."""
-    # Create a new engine and session factory
-    config = Config(RuntimeEnvironment.TEST)
-    engine = create_engine(config.db_uri)
-    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-    session = SessionLocal()
-    try:
-        yield session
-    finally:
-        session.close()
 
 
 @contextmanager
