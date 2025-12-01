7924f0b2a41bc200eb55132c9235fe2cfb2b18e4
Frantisek Stavela <fstavela@redhat.com>
2025-12-01 10:47:41 +0100

    test: Migrate IQE host reaper tests to unit tests

--- tests/test_jobs/test_host_reaper.py
+++ tests/test_jobs/test_host_reaper.py
@@ -35,7 +35,6 @@ from tests.helpers.mq_utils import assert_delete_event_is_valid
 from tests.helpers.mq_utils import assert_delete_notification_is_valid
 from tests.helpers.test_utils import USER_IDENTITY
 from tests.helpers.test_utils import generate_uuid
-from tests.helpers.test_utils import get_staleness_timestamps
 
 CUSTOM_STALENESS_DELETE = {
     "conventional_time_to_stale": CONVENTIONAL_TIME_TO_STALE_SECONDS,
@@ -51,6 +50,7 @@ CUSTOM_STALENESS_NO_HOSTS_TO_DELETE = {
 
 
 @pytest.mark.host_reaper
+@pytest.mark.parametrize("host_type", ["conventional", "edge"])
 @pytest.mark.parametrize(
     "is_host_grouped",
     (True, False),
@@ -72,6 +72,7 @@ def test_culled_host_is_removed(
     db_get_host: Callable[[UUID | str], Host | None],
     db_create_group: Callable[..., Group],
     db_create_host_group_assoc: Callable[..., HostGroupAssoc],
+    host_type: str,
     is_host_grouped: bool,
     reporter: str,
     should_be_removed: bool,
@@ -80,82 +81,51 @@ def test_culled_host_is_removed(
         mock_datetime.now.return_value = datetime(year=2023, month=4, day=2, hour=1, minute=1, second=1, tzinfo=UTC)
         mock_datetime.side_effect = lambda *args, **kw: datetime(*args, **kw)
 
-        staleness_timestamps = get_staleness_timestamps()
-
-        host = minimal_db_host(stale_timestamp=staleness_timestamps["culled"], reporter=reporter)
+        system_profile = {"host_type": host_type} if host_type == "edge" else {}
+        host = minimal_db_host(reporter=reporter, system_profile_facts=system_profile)
         created_host = db_create_host(host=host)
 
         if is_host_grouped:
             group = db_create_group("test_group")
             db_create_host_group_assoc(created_host.id, group.id)
 
-        assert db_get_host(created_host.id)
-
-        threadctx.request_id = None
-        host_reaper_run(
-            inventory_config,
-            mock.Mock(),
-            db.session,
-            event_producer=event_producer_mock,
-            notification_event_producer=notification_event_producer_mock,
-            shutdown_handler=mock.Mock(**{"shut_down.return_value": False}),
-            application=flask_app,
-        )
-
-        if should_be_removed:
-            assert not db_get_host(created_host.id)
-            assert_delete_event_is_valid(
-                event_producer=event_producer_mock,
-                host=created_host,
-                timestamp=event_datetime_mock,
-                identity=None,
-            )
-            assert_delete_notification_is_valid(
-                notification_event_producer=notification_event_producer_mock, host=created_host
-            )
-        else:
-            assert db_get_host(created_host.id)
-            assert event_producer_mock.event is None
-            assert notification_event_producer_mock.event is None
-
-
-@pytest.mark.host_reaper
-def test_culled_edge_host_is_not_removed(
-    flask_app: FlaskApp,
-    event_producer_mock: MockEventProducer,
-    notification_event_producer_mock: MockEventProducer,
-    inventory_config: Config,
-    db_create_host: Callable[..., Host],
-    db_get_host: Callable[[UUID | str], Host | None],
-) -> None:
-    staleness_timestamps = get_staleness_timestamps()
-
-    host = minimal_db_host(
-        stale_timestamp=staleness_timestamps["culled"],
-        reporter="some reporter",
-        system_profile_facts={"host_type": "edge"},
-    )
-    created_host_id = db_create_host(host=host).id
-
-    assert db_get_host(created_host_id)
+    assert db_get_host(created_host.id)
 
     threadctx.request_id = None
     host_reaper_run(
         inventory_config,
         mock.Mock(),
         db.session,
-        event_producer_mock,
-        notification_event_producer_mock,
+        event_producer=event_producer_mock,
+        notification_event_producer=notification_event_producer_mock,
         shutdown_handler=mock.Mock(**{"shut_down.return_value": False}),
         application=flask_app,
     )
 
-    assert db_get_host(created_host_id)
-    assert event_producer_mock.event is None
-    assert notification_event_producer_mock.event is None
+    if should_be_removed:
+        assert not db_get_host(created_host.id)
+        assert_delete_event_is_valid(
+            event_producer=event_producer_mock,
+            host=created_host,
+            timestamp=event_datetime_mock,
+            identity=None,
+        )
+        assert_delete_notification_is_valid(
+            notification_event_producer=notification_event_producer_mock, host=created_host
+        )
+    else:
+        assert db_get_host(created_host.id)
+        assert event_producer_mock.event is None
+        assert notification_event_producer_mock.event is None
 
 
 @pytest.mark.host_reaper
+@pytest.mark.parametrize("host_type", ["conventional", "edge"])
+@pytest.mark.parametrize(
+    "is_host_grouped",
+    (True, False),
+)
+@pytest.mark.parametrize("reporter", ["puptoo", "rhsm-system-profile-bridge"])
 def test_non_culled_host_is_not_removed(
     flask_app: FlaskApp,
     event_producer_mock: MockEventProducer,
@@ -163,18 +133,32 @@ def test_non_culled_host_is_not_removed(
     inventory_config: Config,
     db_create_host: Callable[..., Host],
     db_get_hosts: Callable[..., Query],
+    db_create_group: Callable[..., Group],
+    db_create_host_group_assoc: Callable[..., HostGroupAssoc],
+    host_type: str,
+    is_host_grouped: bool,
+    reporter: str,
 ) -> None:
-    staleness_timestamps = get_staleness_timestamps()
     created_hosts = []
 
-    for stale_timestamp in (
-        staleness_timestamps["stale_warning"],
-        staleness_timestamps["stale"],
-        staleness_timestamps["fresh"],
+    for time_delta in (
+        0,  # Fresh host
+        CONVENTIONAL_TIME_TO_STALE_SECONDS,  # Stale host
+        CONVENTIONAL_TIME_TO_STALE_WARNING_SECONDS,  # Stale warning host
     ):
-        host = minimal_db_host(stale_timestamp=stale_timestamp, reporter="some reporter")
-        created_host = db_create_host(host=host)
-        created_hosts.append(created_host)
+        with patch("app.models.utils.datetime") as mock_datetime:
+            mock_datetime.now.return_value = datetime.now(UTC) - timedelta(seconds=time_delta)
+            mock_datetime.side_effect = lambda *args, **kw: datetime(*args, **kw)
+
+            system_profile = {"host_type": host_type} if host_type == "edge" else {}
+            host = minimal_db_host(reporter=reporter, system_profile_facts=system_profile)
+            created_host = db_create_host(host=host)
+
+            if is_host_grouped:
+                group = db_create_group("test_group")
+                db_create_host_group_assoc(created_host.id, group.id)
+
+            created_hosts.append(created_host)
 
     created_host_ids = sorted(host.id for host in created_hosts)
     retrieved_hosts = db_get_hosts(created_host_ids)
@@ -211,36 +195,35 @@ def test_reaper_shutdown_handler(
         mock_datetime.now.return_value = datetime(year=2023, month=4, day=2, hour=1, minute=1, second=1)
         mock_datetime.side_effect = lambda *args, **kw: datetime(*args, **kw)
 
-        staleness_timestamps = get_staleness_timestamps()
         created_host_ids = []
 
         host_count = 3
         for _ in range(host_count):
-            host_data = minimal_db_host(stale_timestamp=staleness_timestamps["culled"], reporter="puptoo")
+            host_data = minimal_db_host(reporter="puptoo")
             created_host = db_create_host(host=host_data)
             created_host_ids.append(created_host.id)
 
-        created_hosts = db_get_hosts(created_host_ids)
-        assert created_hosts.count() == host_count
+    created_hosts = db_get_hosts(created_host_ids)
+    assert created_hosts.count() == host_count
 
-        fake_event_producer = mock.Mock()
+    fake_event_producer = mock.Mock()
 
-        threadctx.request_id = None
-        inventory_config.host_delete_chunk_size = 1
+    threadctx.request_id = None
+    inventory_config.host_delete_chunk_size = 1
 
-        host_reaper_run(
-            inventory_config,
-            mock.Mock(),
-            db.session,
-            fake_event_producer,
-            notification_event_producer_mock,
-            shutdown_handler=mock.Mock(**{"shut_down.side_effect": (False, False, True)}),
-            application=flask_app,
-        )
+    host_reaper_run(
+        inventory_config,
+        mock.Mock(),
+        db.session,
+        fake_event_producer,
+        notification_event_producer_mock,
+        shutdown_handler=mock.Mock(**{"shut_down.side_effect": (False, False, True)}),
+        application=flask_app,
+    )
 
-        remaining_hosts = db_get_hosts(created_host_ids)
-        assert remaining_hosts.count() == 1
-        assert fake_event_producer.write_event.call_count == 2
+    remaining_hosts = db_get_hosts(created_host_ids)
+    assert remaining_hosts.count() == 1
+    assert fake_event_producer.write_event.call_count == 2
 
 
 @pytest.mark.host_reaper
@@ -272,6 +255,9 @@ def test_unknown_host_is_not_removed(
 
     assert event_producer_mock.event is None
     assert notification_event_producer_mock.event is None
+
+    retrieved_host = db_get_host(created_host.id)
+    assert retrieved_host
 
 
 def assert_system_culling_data(
@@ -312,40 +298,42 @@ def test_reaper_stops_after_kafka_producer_error(
 
         event_producer._kafka_producer.produce.side_effect = produce_side_effects
 
-        staleness_timestamps = get_staleness_timestamps()
-
         host_count = 3
-        created_hosts = db_create_multiple_hosts(
-            how_many=host_count, extra_data={"stale_timestamp": staleness_timestamps["culled"], "reporter": "puptoo"}
-        )
+        created_hosts = db_create_multiple_hosts(how_many=host_count, extra_data={"reporter": "puptoo"})
         created_host_ids = [str(host.id) for host in created_hosts]
 
-        hosts = db_get_hosts(created_host_ids)
-        assert hosts.count() == host_count
+    hosts = db_get_hosts(created_host_ids)
+    assert hosts.count() == host_count
 
-        threadctx.request_id = None
-        inventory_config.host_delete_chunk_size = 1
+    threadctx.request_id = None
+    inventory_config.host_delete_chunk_size = 1
 
-        with pytest.raises(KafkaException):
-            host_reaper_run(
-                inventory_config,
-                mock.Mock(),
-                db.session,
-                event_producer,
-                notification_event_producer,
-                shutdown_handler=mock.Mock(**{"shut_down.return_value": False}),
-                application=flask_app,
-            )
+    with pytest.raises(KafkaException):
+        host_reaper_run(
+            inventory_config,
+            mock.Mock(),
+            db.session,
+            event_producer,
+            notification_event_producer,
+            shutdown_handler=mock.Mock(**{"shut_down.return_value": False}),
+            application=flask_app,
+        )
 
-        remaining_hosts = db_get_hosts(created_host_ids)
-        assert remaining_hosts.count() == 2
-        assert event_producer._kafka_producer.produce.call_count == 2
-        assert notification_event_producer._kafka_producer.produce.call_count == 1
+    remaining_hosts = db_get_hosts(created_host_ids)
+    assert remaining_hosts.count() == 2
+    assert event_producer._kafka_producer.produce.call_count == 2
+    assert notification_event_producer._kafka_producer.produce.call_count == 1
 
 
 @pytest.mark.usefixtures("event_producer_mock", "notification_event_producer_mock")
 def test_host_with_rhsm_conduit_and_other_reporters_can_be_culled(
     db_create_host: Callable[..., Host],
+    db_get_host: Callable[[UUID | str], Host | None],
+    event_producer_mock: MockEventProducer,
+    notification_event_producer_mock: MockEventProducer,
+    event_datetime_mock: datetime,
+    inventory_config: Config,
+    flask_app: FlaskApp,
 ) -> None:
     """Test that hosts with rhsm-system-profile-bridge AND other reporters can still be culled normally."""
 
@@ -353,12 +341,13 @@ def test_host_with_rhsm_conduit_and_other_reporters_can_be_culled(
     past_time = datetime.now(UTC) - timedelta(days=30)
 
     mixed_reporter_host = Host(
-        canonical_facts={"subscription_manager_id": generate_uuid()},
+        canonical_facts={"subscription_manager_id": generate_uuid(), "insights_id": generate_uuid()},
         display_name="mixed-reporter-host",
         reporter="rhsm-system-profile-bridge",
-        stale_timestamp=past_time,
         org_id=USER_IDENTITY["org_id"],
     )
+    mixed_reporter_host.last_check_in = past_time
+    mixed_reporter_host.stale_timestamp = past_time
     mixed_reporter_host.stale_warning_timestamp = past_time
     mixed_reporter_host.deletion_timestamp = past_time
     mixed_reporter_host.per_reporter_staleness = {
@@ -382,6 +371,28 @@ def test_host_with_rhsm_conduit_and_other_reporters_can_be_culled(
 
     # This host should be eligible for deletion since it has multiple reporters
     assert should_host_stay_fresh_forever(created_mixed_host) is False
+
+    threadctx.request_id = None
+    host_reaper_run(
+        inventory_config,
+        mock.Mock(),
+        db.session,
+        event_producer_mock,
+        notification_event_producer_mock,
+        shutdown_handler=mock.Mock(**{"shut_down.return_value": False}),
+        application=flask_app,
+    )
+
+    assert not db_get_host(created_mixed_host.id)
+    assert_delete_event_is_valid(
+        event_producer=event_producer_mock,
+        host=created_mixed_host,
+        timestamp=event_datetime_mock,
+        identity=None,
+    )
+    assert_delete_notification_is_valid(
+        notification_event_producer=notification_event_producer_mock, host=created_mixed_host
+    )
 
 
 @pytest.mark.parametrize(
@@ -486,3 +497,49 @@ def test_no_hosts_to_delete(
     )
     assert len(db_get_hosts(immutable_hosts).all()) == 2
     assert len(db_get_hosts(conventional_hosts).all()) == 2
+
+
+def test_host_reaper_doesnt_use_updated_timestamp(
+    flask_app: FlaskApp,
+    event_producer_mock: MockEventProducer,
+    event_datetime_mock: datetime,
+    notification_event_producer_mock: MockEventProducer,
+    inventory_config: Config,
+    db_create_host: Callable[..., Host],
+    db_get_host: Callable[[UUID | str], Host | None],
+) -> None:
+    """Test that host reaper doesn't use 'updated' timestamp to determine if a host should be culled."""
+
+    with patch("app.models.utils.datetime") as mock_datetime:
+        mock_datetime.now.return_value = datetime(year=2023, month=4, day=2, hour=1, minute=1, second=1, tzinfo=UTC)
+        mock_datetime.side_effect = lambda *args, **kw: datetime(*args, **kw)
+
+        host = minimal_db_host(reporter="puptoo")
+        host.updated = datetime.now(UTC)
+        created_host = db_create_host(host=host)
+
+    retrieved_host = db_get_host(created_host.id)
+    assert retrieved_host
+    assert retrieved_host.updated >= datetime.now(UTC) - timedelta(seconds=1)
+
+    threadctx.request_id = None
+    host_reaper_run(
+        inventory_config,
+        mock.Mock(),
+        db.session,
+        event_producer=event_producer_mock,
+        notification_event_producer=notification_event_producer_mock,
+        shutdown_handler=mock.Mock(**{"shut_down.return_value": False}),
+        application=flask_app,
+    )
+
+    assert not db_get_host(created_host.id)
+    assert_delete_event_is_valid(
+        event_producer=event_producer_mock,
+        host=created_host,
+        timestamp=event_datetime_mock,
+        identity=None,
+    )
+    assert_delete_notification_is_valid(
+        notification_event_producer=notification_event_producer_mock, host=created_host
+    )
