fc98d4963da2669f1e7d1c03a9d11862e19ef83a
Asa Price <aprice@redhat.com>
2025-12-09 17:50:53 -0500

    fix(RHINENG-22191): Use clowder config for Kessel endpoint (#3264)
    * Use clowder config for Kessel endpoint
    
    * replace kessel-inventory-api with kessel-inventory
    
    * Update clowder config
    
    * Use endpoints rather than dependencyEndpoints
    
    * Update dependencies usage
    
    * Remove http from non-clowder endpoint config
    
    * Set blank default values
    
    * Remove kessel-inventory dependency
    
    * Use DependencyEndpoints config
    
    * Update config name

--- app/config.py
+++ app/config.py
@@ -6,6 +6,8 @@ import tempfile
 from datetime import timedelta
 from enum import Enum
 
+from app_common_python import DependencyEndpoints
+
 from app.common import get_build_version
 from app.culling import CONVENTIONAL_TIME_TO_DELETE_SECONDS
 from app.culling import CONVENTIONAL_TIME_TO_STALE_SECONDS
@@ -41,6 +43,8 @@ class Config:
         import app_common_python
 
         cfg = app_common_python.LoadedConfig
+        kessel_relations_hostname = DependencyEndpoints["kessel-relations"]["api"].hostname
+        self.kessel_relations_api_endpoint = f"{kessel_relations_hostname}:9000"
 
         self.is_clowder = True
         self.metrics_port = cfg.metricsPort
@@ -156,6 +160,7 @@ class Config:
         self._db_port = os.getenv("INVENTORY_DB_PORT", 5432)
         self._db_name = os.getenv("INVENTORY_DB_NAME", "insights")
         self.rbac_endpoint = os.environ.get("RBAC_ENDPOINT", "http://localhost:8111")
+        self.kessel_relations_api_endpoint = os.environ.get("KESSEL_RELATIONS_API_ENDPOINT", "localhost:9000")
         self.export_service_endpoint = os.environ.get("EXPORT_SERVICE_ENDPOINT", "http://localhost:10010")
         self.host_ingress_topic = os.environ.get("KAFKA_HOST_INGRESS_TOPIC", "platform.inventory.host-ingress")
         self.additional_validation_topic = os.environ.get(
@@ -327,7 +332,6 @@ class Config:
         self.sp_authorized_users = os.getenv("SP_AUTHORIZED_USERS", "tuser@redhat.com").split()
         self.mq_db_batch_max_messages = int(os.getenv("MQ_DB_BATCH_MAX_MESSAGES", "1"))
         self.mq_db_batch_max_seconds = float(os.getenv("MQ_DB_BATCH_MAX_SECONDS", "0.5"))
-        self.kessel_target_url = os.getenv("KESSEL_TARGET_URL", "localhost:9000")
 
         self.s3_access_key_id = os.getenv("S3_AWS_ACCESS_KEY_ID")
         self.s3_secret_access_key = os.getenv("S3_AWS_SECRET_ACCESS_KEY")
