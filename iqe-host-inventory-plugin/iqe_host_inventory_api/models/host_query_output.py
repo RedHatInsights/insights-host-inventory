2d568d33fde0ea4786e18fded0bde4630209f9ef
Asa Price <aprice@redhat.com>
2025-12-19 13:06:38 -0500

    linting changes

--- iqe-host-inventory-plugin/iqe_host_inventory_api/models/host_query_output.py
+++ iqe-host-inventory-plugin/iqe_host_inventory_api/models/host_query_output.py
@@ -208,9 +208,9 @@ class HostQueryOutput:
             elif isinstance(value, dict):
                 result[attr] = dict(
                     map(
-                        lambda item: (item[0], item[1].to_dict())
-                        if hasattr(item[1], "to_dict")
-                        else item,
+                        lambda item: (
+                            (item[0], item[1].to_dict()) if hasattr(item[1], "to_dict") else item
+                        ),
                         value.items(),
                     )
                 )
