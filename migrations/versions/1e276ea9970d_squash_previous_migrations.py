283ff43b2a88cae06a30f98e0e03ebecf582b61e
Joao Paulo Ramos <jramos@redhat.com>
2026-01-15 19:02:50 -0300

    chore: remove unncessary logical replication files

--- migrations/versions/1e276ea9970d_squash_previous_migrations.py
+++ migrations/versions/1e276ea9970d_squash_previous_migrations.py
@@ -6,6 +6,8 @@ Create Date: 2024-11-26 11:20:10.948151
 
 """
 
+import os
+
 from alembic import op
 
 # revision identifiers, used by Alembic.
@@ -16,7 +18,11 @@ depends_on = None
 
 
 def upgrade():
-    with open("app_migrations/hbi_schema_2024-12-03.sql") as f:
+    # Get the path to the schema file relative to this migration script
+    migration_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    schema_file_path = os.path.join(migration_dir, "hbi_schema_2024-12-03.sql")
+
+    with open(schema_file_path) as f:
         # Don't execute the last item, as it's just an empty comment
         # (i.e. "PostgreSQL database dump complete")
         for stmt in f.read().split(";\n")[:-1]:
