9666f59a645baf884beadc47dfa83a710019dbcc
Muhammad Arif <aarif@redhat.com>
2026-02-18 17:55:42 -0500

    Fix one statement left behind

--- app/serialization.py
+++ app/serialization.py
@@ -310,7 +310,7 @@ def serialize_group_without_host_count(group: Group) -> dict:
         "org_id": group.org_id,
         "account": group.account,
         "name": group.name,
-        "ungrouped": group.ungrouped,
+        "ungrouped": bool(group.ungrouped),
         "created": _serialize_datetime(group.created_on),
         "updated": _serialize_datetime(group.modified_on),
     }
