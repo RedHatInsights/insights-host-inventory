6fb9c63e821e53c3b33c2eab7c43e413d4b819f7
Fabricia Diniz <agomesdi@redhat.com>
2026-04-28 13:43:40 -0300

    refactor(RHINENG-25844): Make staleness_timestamp nullable

--- tests/test_models.py
+++ tests/test_models.py
@@ -431,6 +431,37 @@ def test_host_model_default_timestamps(db_create_host):
     assert host.modified_on < after_commit
 
 
+def test_host_stale_timestamp_nullable_on_insert(db_create_host):
+    """The hosts table allows inserting a row with stale_timestamp NULL."""
+    host = Host(
+        account=USER_IDENTITY["account_number"],
+        subscription_manager_id=generate_uuid(),
+        reporter="yupana",
+        org_id=USER_IDENTITY["org_id"],
+    )
+    host.stale_timestamp = None
+    db_create_host(host=host)
+    db.session.refresh(host)
+    assert host.stale_timestamp is None
+
+
+def test_host_stale_timestamp_nullable_update_to_null(db_create_host):
+    """An existing row may update stale_timestamp from a value to NULL."""
+    host = Host(
+        account=USER_IDENTITY["account_number"],
+        subscription_manager_id=generate_uuid(),
+        reporter="yupana",
+        org_id=USER_IDENTITY["org_id"],
+    )
+    db_create_host(host=host)
+    assert host.stale_timestamp is not None
+
+    host.stale_timestamp = None
+    db.session.commit()
+    db.session.refresh(host)
+    assert host.stale_timestamp is None
+
+
 def test_host_model_updated_timestamp(db_create_host):
     host = Host(
         account=USER_IDENTITY["account_number"],
